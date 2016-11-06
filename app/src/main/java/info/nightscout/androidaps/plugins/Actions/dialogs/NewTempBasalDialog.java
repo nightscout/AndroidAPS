95ffb03a266f19b99212d84763b5590882a1d313
Milos Kozak <m.kozak@sysop.cz>
2016-11-06 12:10:24 +0100

    fix resources crash

--- app/src/main/java/info/nightscout/androidaps/plugins/Actions/dialogs/NewTempBasalDialog.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/Actions/dialogs/NewTempBasalDialog.java
@@ -151,9 +151,9 @@ public class NewTempBasalDialog extends DialogFragment implements View.OnClickLi
                                     }
                                     if (!result.success) {
                                         AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                                        builder.setTitle(getContext().getString(R.string.treatmentdeliveryerror));
+                                        builder.setTitle(MainApp.sResources.getString(R.string.treatmentdeliveryerror));
                                         builder.setMessage(result.comment);
-                                        builder.setPositiveButton(getContext().getString(R.string.ok), null);
+                                        builder.setPositiveButton(MainApp.sResources.getString(R.string.ok), null);
                                         builder.show();
                                     }
                                 }
