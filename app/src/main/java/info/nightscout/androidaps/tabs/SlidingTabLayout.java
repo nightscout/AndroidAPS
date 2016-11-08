a4a9efd812d0adfa8dbdcaf54917f6aa1e908282
Milos Kozak <m.kozak@sysop.cz>
2016-11-08 14:24:02 +0100

    TAB_VIEW_PADDING_DIPS = 9

--- app/src/main/java/info/nightscout/androidaps/tabs/SlidingTabLayout.java
+++ app/src/main/java/info/nightscout/androidaps/tabs/SlidingTabLayout.java
@@ -60,7 +60,7 @@ public class SlidingTabLayout extends HorizontalScrollView {
     }
 
     private static final int TITLE_OFFSET_DIPS = 24;
-    private static final int TAB_VIEW_PADDING_DIPS = 16;
+    private static final int TAB_VIEW_PADDING_DIPS = 9;
     private static final int TAB_VIEW_TEXT_SIZE_SP = 12;
 
     private int mTitleOffset;
