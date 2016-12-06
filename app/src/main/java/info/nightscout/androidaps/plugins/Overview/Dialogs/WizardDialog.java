86755599dfa314030ef7866b530396b373988689
Milos Kozak <m.kozak@sysop.cz>
2016-12-06 23:00:57 +0100

    show IC and ICF with 1 decimal

--- app/src/main/java/info/nightscout/androidaps/plugins/Overview/Dialogs/WizardDialog.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/Overview/Dialogs/WizardDialog.java
@@ -295,7 +295,7 @@ public class WizardDialog extends DialogFragment implements OnClickListener {
                 bgDiff = lastBgValue - targetBGHigh;
             }
 
-            bg.setText(lastBg.valueToUnitsToString(units) + " ISF: " + DecimalFormatter.to0Decimal(sens));
+            bg.setText(lastBg.valueToUnitsToString(units) + " ISF: " + DecimalFormatter.to1Decimal(sens));
             bgInsulin.setText(DecimalFormatter.to2Decimal(bgDiff / sens) + "U");
             bgInput.removeTextChangedListener(textWatcher);
             //bgInput.setText(lastBg.valueToUnitsToString(units));
@@ -371,7 +371,7 @@ public class WizardDialog extends DialogFragment implements OnClickListener {
         bg.setText(c_bg + " ISF: " + DecimalFormatter.to0Decimal(wizard.sens));
         bgInsulin.setText(DecimalFormatter.to2Decimal(wizard.insulinFromBG) + "U");
 
-        carbs.setText(DecimalFormatter.to0Decimal(c_carbs) + "g IC: " + DecimalFormatter.to0Decimal(wizard.ic));
+        carbs.setText(DecimalFormatter.to0Decimal(c_carbs) + "g IC: " + DecimalFormatter.to1Decimal(wizard.ic));
         carbsInsulin.setText(DecimalFormatter.to2Decimal(wizard.insulinFromCarbs) + "U");
 
         bolusIobInsulin.setText(DecimalFormatter.to2Decimal(wizard.insulingFromBolusIOB) + "U");
