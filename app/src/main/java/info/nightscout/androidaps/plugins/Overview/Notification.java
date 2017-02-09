7f123e4208154943548bce4daa95b76c914d685a
Milos Kozak <m.kozak@sysop.cz>
2017-02-09 16:14:09 +0100

    warn on wrong entered sms phone number

--- app/src/main/java/info/nightscout/androidaps/plugins/Overview/Notification.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/Overview/Notification.java
@@ -21,6 +21,7 @@ public class Notification {
     public static final int FAILED_UDPATE_PROFILE = 6;
     public static final int BASAL_VALUE_BELOW_MINIMUM = 7;
     public static final int OLD_NSCLIENT = 8;
+    public static final int INVALID_PHONE_NUMBER = 9;
 
     public int id;
     public Date date;
