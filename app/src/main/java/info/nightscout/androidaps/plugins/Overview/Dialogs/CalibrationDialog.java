83667be8cc87f2a90b843a083365c9656489bc89
Milos Kozak <m.kozak@sysop.cz>
2017-02-10 17:25:32 +0100

    extract sending intent to a function

--- app/src/main/java/info/nightscout/androidaps/plugins/Overview/Dialogs/CalibrationDialog.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/Overview/Dialogs/CalibrationDialog.java
@@ -2,12 +2,8 @@ package info.nightscout.androidaps.plugins.Overview.Dialogs;
 
 
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,19 +16,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.DecimalFormat;
-import java.util.Date;
-import java.util.List;
 
-import info.nightscout.androidaps.Config;
 import info.nightscout.androidaps.Constants;
 import info.nightscout.androidaps.MainApp;
 import info.nightscout.androidaps.R;
-import info.nightscout.androidaps.Services.Intents;
 import info.nightscout.androidaps.data.GlucoseStatus;
 import info.nightscout.client.data.NSProfile;
 import info.nightscout.utils.PlusMinusEditText;
-import info.nightscout.utils.SafeParse;
-import info.nightscout.utils.ToastUtils;
+import info.nightscout.utils.XripCalibrations;
 
 public class CalibrationDialog extends DialogFragment implements View.OnClickListener {
     private static Logger log = LoggerFactory.getLogger(CalibrationDialog.class);
@@ -79,42 +70,9 @@ public class CalibrationDialog extends DialogFragment implements View.OnClickLis
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.overview_calibration_okbutton:
-                if (parentContext != null) {
-                    final NSProfile profile = MainApp.getConfigBuilder().getActiveProfile().getProfile();
-
-                    final Double bg = bgText.getValue();
-
-                    String confirmMessage = String.format(MainApp.sResources.getString(R.string.send_calibration), bg);
-
-                    AlertDialog.Builder builder = new AlertDialog.Builder(parentContext);
-                    builder.setTitle(MainApp.sResources.getString(R.string.confirmation));
-                    builder.setMessage(confirmMessage);
-                    builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            Context context = MainApp.instance().getApplicationContext();
-                            Bundle bundle = new Bundle();
-                            bundle.putDouble("glucose_number", bg);
-                            bundle.putString("units", profile.getUnits().equals(Constants.MGDL) ? "mgdl" : "mmol");
-                            bundle.putLong("timestamp", new Date().getTime());
-                            Intent intent = new Intent(Intents.ACTION_REMOTE_CALIBRATION);
-                            intent.putExtras(bundle);
-                            intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-                            context.sendBroadcast(intent);
-                            List<ResolveInfo> q = MainApp.instance().getApplicationContext().getPackageManager().queryBroadcastReceivers(intent, 0);
-                            if (q.size() < 1) {
-                                ToastUtils.showToastInUiThread(MainApp.instance().getApplicationContext(),MainApp.sResources.getString(R.string.xdripnotinstalled));
-                                log.debug(MainApp.sResources.getString(R.string.xdripnotinstalled));
-                            } else {
-                                ToastUtils.showToastInUiThread(MainApp.instance().getApplicationContext(),MainApp.sResources.getString(R.string.calibrationsent));
-                            }
-                        }
-                    });
-                    builder.setNegativeButton(getString(R.string.cancel), null);
-                    builder.show();
-                    dismiss();
-                } else {
-                    log.error("parentContext == null");
-                }
+                final Double bg = bgText.getValue();
+                XripCalibrations.confirmAndSendCalibration(bg, parentContext);
+                dismiss();
                 break;
         }
     }
