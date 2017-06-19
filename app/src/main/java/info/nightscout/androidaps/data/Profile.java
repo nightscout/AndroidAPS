6293e89be2156b9838c55da7321b92370d3672a1
PoweRGbg <roumen_georgiev@yahoo.com>
2017-06-19 13:10:32 +0300

    fixed getICList() returning bg units/U to g/hr

--- app/src/main/java/info/nightscout/androidaps/data/Profile.java
+++ app/src/main/java/info/nightscout/androidaps/data/Profile.java
@@ -247,7 +247,7 @@ public class Profile {
     }
 
     public String getIcList() {
-        return getValuesList(ic, null, new DecimalFormat("0.0"), getUnits() + "/U");
+        return getValuesList(ic, null, new DecimalFormat("0.0"), " g/hr");
     }
 
     public Double getBasal() {
