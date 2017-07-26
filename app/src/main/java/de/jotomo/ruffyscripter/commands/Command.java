e1b63a4a74cf11f1de0c3f792529ff298555d050
Johannes Mockenhaupt <git@jotomo.de>
2017-07-26 15:50:07 +0200

    Command doc.

--- app/src/main/java/de/jotomo/ruffyscripter/commands/Command.java
+++ app/src/main/java/de/jotomo/ruffyscripter/commands/Command.java
@@ -5,6 +5,13 @@ import java.util.List;
 import de.jotomo.ruffyscripter.PumpState;
 import de.jotomo.ruffyscripter.RuffyScripter;
 
+/**
+ * Interface for all commands to be executed by the pump.
+ *
+ * Note on cammond methods and timing: a method shall wait before and after executing
+ * as necessary to not cause timing issues, so the caller can just call methods in
+ * sequence, letting the methods take care of waits.
+ */
 public interface Command {
     CommandResult execute(RuffyScripter ruffyScripter, PumpState initialPumpState);
     List<String> validateArguments();
