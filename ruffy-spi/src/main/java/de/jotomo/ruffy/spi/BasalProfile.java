75afa4e0a18601ef3578ed3c3bfa880aeafd8881
Johannes Mockenhaupt <git@jotomo.de>
2017-11-11 16:19:34 +0100

    Remove basal rate profile number, only using one.

--- ruffy-spi/src/main/java/de/jotomo/ruffy/spi/BasalProfile.java
+++ ruffy-spi/src/main/java/de/jotomo/ruffy/spi/BasalProfile.java
@@ -1,13 +1,9 @@
 package de.jotomo.ruffy.spi;
 
 public class BasalProfile {
-    public final int number;
     public final double[] hourlyRates;
 
-    public BasalProfile(int number, double[] hourlyRates) {
-        this.number = number;
-        if (hourlyRates.length != 24)
-            throw new IllegalArgumentException("Profile must have 24 hourly rates");
+    public BasalProfile( double[] hourlyRates) {
         this.hourlyRates = hourlyRates;
     }
 }
