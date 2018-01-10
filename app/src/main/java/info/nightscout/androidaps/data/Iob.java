d4365a235216d8e93cd07637e343df287ac0f143
Markus M. May <triplem@javafreedom.org>
2018-01-10 22:11:20 +0100

    Add a unit test for Insulin Plugin

--- app/src/main/java/info/nightscout/androidaps/data/Iob.java
+++ app/src/main/java/info/nightscout/androidaps/data/Iob.java
@@ -12,4 +12,26 @@ public class Iob {
         activityContrib += iob.activityContrib;
         return this;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Iob iob = (Iob) o;
+
+        if (Double.compare(iob.iobContrib, iobContrib) != 0) return false;
+        return Double.compare(iob.activityContrib, activityContrib) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(iobContrib);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(activityContrib);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
