4587a47840bd1be50a7fe4e2dcbdbb4830194bf1
AdrianLxM <adrian.m@inbox.com>
2018-02-10 14:53:43 +0100

    add links to about

--- app/src/main/java/info/nightscout/androidaps/MainActivity.java
+++ app/src/main/java/info/nightscout/androidaps/MainActivity.java
@@ -1,6 +1,7 @@
 package info.nightscout.androidaps;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
@@ -19,6 +20,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -27,6 +31,7 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
@@ -392,10 +397,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                     builder.setIcon(R.mipmap.blueowl);
                                 String message = "Build: " + BuildConfig.BUILDVERSION + "\n";
                                 message += MainApp.sResources.getString(R.string.configbuilder_nightscoutversion_label) + " " + ConfigBuilderPlugin.nightscoutVersionName;
-                                builder.setMessage(message);
+                                message += getString(R.string.about_link_urls);
+                                final SpannableString messageSpanned =  new SpannableString(message);
+                                Linkify.addLinks(messageSpanned, Linkify.WEB_URLS);
+                                builder.setMessage(messageSpanned);
                                 builder.setPositiveButton(MainApp.sResources.getString(R.string.ok), null);
                                 AlertDialog alertDialog = builder.create();
                                 alertDialog.show();
+                                ((TextView)alertDialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
                                 break;
                             case R.id.nav_exit:
                                 log.debug("Exiting");
