30d12622f7369d4de6d0223ccccf4447c3f54c35
Milos Kozak <m.kozak@sysop.cz>
2018-03-12 17:59:42 +0100

    update maxIOB hard limits

--- app/src/main/java/info/nightscout/utils/HardLimits.java
+++ app/src/main/java/info/nightscout/utils/HardLimits.java
@@ -39,8 +39,8 @@ public class HardLimits {
     public static final double MINISF = 2; // mgdl
     public static final double MAXISF = 720; // mgdl
 
-    public static final double[] MAXIOB_AMA = {3, 5, 7, 7};
-    public static final double[] MAXIOB_SMB = {3, 6, 10, 12};
+    public static final double[] MAXIOB_AMA = {3, 5, 7, 12};
+    public static final double[] MAXIOB_SMB = {3, 7, 12, 22};
 
     public static final double[] MAXBASAL = {2, 5, 10, 12};
 
