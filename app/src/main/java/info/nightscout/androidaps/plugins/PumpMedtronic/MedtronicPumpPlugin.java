99544e1f2b6fe0c2f381fff334368a380f40d2fb
Andy Rozman <andy.rozman@gmail.com>
2018-04-23 19:47:51 +0100

    Pre start for Medtronic plugin

---
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpMedtronic/MedtronicPumpPlugin.java
@@ -0,0 +1,8 @@
+package info.nightscout.androidaps.plugins.PumpMedtronic;
+
+/**
+ * Created by andy on 23.04.18.
+ */
+
+public class MedtronicPumpPlugin {
+}
---
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpPluginAbstract.java
@@ -0,0 +1,8 @@
+package info.nightscout.androidaps.plugins;
+
+/**
+ * Created by andy on 23.04.18.
+ */
+
+public class PumpPluginAbstract {
+}
