2f846bc4171dfad0c220e7ea9d2fd234d392a6f8
Johannes Mockenhaupt <git@jotomo.de>
2018-04-26 01:04:27 +0200

    WatchUpdaterService: use IobCob.getCobInfo().

--- app/src/main/java/info/nightscout/androidaps/plugins/Wear/wearintegration/WatchUpdaterService.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/Wear/wearintegration/WatchUpdaterService.java
@@ -37,6 +37,7 @@ import info.nightscout.androidaps.data.Profile;
 import info.nightscout.androidaps.db.BgReading;
 import info.nightscout.androidaps.db.DatabaseHelper;
 import info.nightscout.androidaps.db.TemporaryBasal;
+import info.nightscout.androidaps.plugins.IobCobCalculator.CobInfo;
 import info.nightscout.androidaps.plugins.Treatments.Treatment;
 import info.nightscout.androidaps.interfaces.PluginType;
 import info.nightscout.androidaps.interfaces.TreatmentsInterface;
@@ -718,9 +719,9 @@ public class WatchUpdaterService extends WearableListenerService implements
     private String generateCOBString() {
 
         String cobStringResult = "--";
-        AutosensData autosensData = IobCobCalculatorPlugin.getPlugin().getLastAutosensData("WatcherUpdaterService");
-        if (autosensData != null) {
-            cobStringResult = (int) autosensData.cob + "g";
+        CobInfo cobInfo = IobCobCalculatorPlugin.getPlugin().getCobInfo(false, "WatcherUpdaterService");
+        if (cobInfo.displayCob != null) {
+            cobStringResult = DecimalFormatter.to0Decimal(cobInfo.displayCob) + "g";
         }
         return cobStringResult;
     }
