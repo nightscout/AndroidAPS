43cc3c4908089e9dcad62823cc75cdfc4027ab48
Johannes Mockenhaupt <git@jotomo.de>
2018-05-04 14:18:55 +0200

    Extract CarbsGenerator class.

--- app/src/main/java/info/nightscout/androidaps/plugins/Wear/ActionStringHandler.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/Wear/ActionStringHandler.java
@@ -46,6 +46,7 @@ import info.nightscout.androidaps.plugins.PumpDanaRKorean.DanaRKoreanPlugin;
 import info.nightscout.androidaps.plugins.PumpDanaRS.DanaRSPlugin;
 import info.nightscout.androidaps.plugins.PumpDanaRv2.DanaRv2Plugin;
 import info.nightscout.androidaps.plugins.PumpInsight.InsightPlugin;
+import info.nightscout.androidaps.plugins.Treatments.CarbsGenerator;
 import info.nightscout.androidaps.plugins.Treatments.TreatmentsPlugin;
 import info.nightscout.androidaps.queue.Callback;
 import info.nightscout.utils.BolusWizard;
@@ -633,48 +634,12 @@ public class ActionStringHandler {
     private static void doECarbs(int carbs, long time, int duration) {
         if (carbs > 0) {
             if (duration == 0) {
-                createCarb(carbs, time, "watch");
+                CarbsGenerator.createCarb(carbs, time, "watch");
             } else {
-                long remainingCarbs = carbs;
-                int ticks = (duration * 4); //duration guaranteed to be integer greater zero
-                for (int i = 0; i < ticks; i++){
-                    long carbTime = time + i * 15 * 60 * 1000;
-                    long smallCarbAmount = Math.round((1d * remainingCarbs) / (ticks-i));  //on last iteration (ticks-i) is 1 -> smallCarbAmount == remainingCarbs
-                    remainingCarbs -= smallCarbAmount;
-                    if (smallCarbAmount > 0)
-                        createCarb(smallCarbAmount, carbTime, "watch eCarbs");
-                }
+                CarbsGenerator.generateCarbs(carbs, time, duration, "watch eCarbs");
             }
         }
     }
-
-    private static void createCarb(long carbs, long time, @Nullable String notes) {
-        DetailedBolusInfo carbInfo = new DetailedBolusInfo();
-        carbInfo.date = time;
-        carbInfo.eventType = CareportalEvent.CARBCORRECTION;
-        carbInfo.carbs = carbs;
-        carbInfo.context = MainApp.instance();
-        carbInfo.source = Source.USER;
-        carbInfo.notes = notes;
-        if (ConfigBuilderPlugin.getActivePump().getPumpDescription().storesCarbInfo && carbInfo.date <= now()) {
-            ConfigBuilderPlugin.getCommandQueue().bolus(carbInfo, new Callback() {
-                @Override
-                public void run() {
-                    if (!result.success) {
-                        Intent i = new Intent(MainApp.instance(), ErrorHelperActivity.class);
-                        i.putExtra("soundid", R.raw.boluserror);
-                        i.putExtra("status", result.comment);
-                        i.putExtra("title", MainApp.gs(R.string.treatmentdeliveryerror));
-                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        MainApp.instance().startActivity(i);
-                    }
-                }
-            });
-        } else {
-            TreatmentsPlugin.getPlugin().addToHistoryTreatment(carbInfo);
-        }
-    }
-
 
     private static void setCPP(int timeshift, int percentage) {
 
