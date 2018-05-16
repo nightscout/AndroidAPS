764e035666c266fc4a184c86989ed2e589269d3d
Roumen Georgiev <roumen_georgiev@yahoo.com>
2018-05-16 15:35:17 +0300

    remove wizard from backstack and fix the Exit -> returning to SW

--- app/src/main/java/info/nightscout/androidaps/startupwizard/SetupWizardActivity.java
+++ app/src/main/java/info/nightscout/androidaps/startupwizard/SetupWizardActivity.java
@@ -163,6 +163,7 @@ public class SetupWizardActivity extends AppCompatActivity {
     public void finishSetupWizard(View view) {
         SP.putBoolean(R.string.key_setupwizard_processed, true);
         Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
