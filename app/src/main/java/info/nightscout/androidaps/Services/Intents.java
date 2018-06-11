e7eec8fe69e4ad44a09a7f5ea09e89d57d47c491
Milos Kozak <m.kozak@sysop.cz>
2018-06-11 17:26:31 +0200

    Poctech BG source

--- app/src/main/java/info/nightscout/androidaps/Services/Intents.java
+++ app/src/main/java/info/nightscout/androidaps/Services/Intents.java
@@ -49,4 +49,6 @@ public interface Intents {
     String GLIMP_BG = "it.ct.glicemia.ACTION_GLUCOSE_MEASURED";
 
     String DEXCOMG5_BG = "com.dexcom.cgm.DATA";
+
+    String POCTECH_BG = "com.china.poctech.data";
 }
