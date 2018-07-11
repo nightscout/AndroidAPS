395f2313fd5615cd68ada257a6226d9065bf90fc
harisp <haris.poljo@gmail.com>
2018-07-11 18:17:09 +0200

    Implemented feedback

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpInsight/InsightPlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpInsight/InsightPlugin.java
@@ -914,6 +914,9 @@ public class InsightPlugin extends PluginBase implements PumpInterface, Constrai
     public Constraint<Double> applyBolusConstraints(Constraint<Double> insulin) {
         if (statusResult != null) {
             insulin.setIfSmaller(statusResult.maximumBolusAmount, String.format(MainApp.gs(R.string.limitingbolus), statusResult.maximumBolusAmount, MainApp.gs(R.string.pumplimit)), this);
+
+            if(insulin.value() != 0)
+                insulin.setIfGreater(statusResult.minimumBolusAmount, String.format(MainApp.gs(R.string.limitingbolus), statusResult.maximumBolusAmount, MainApp.gs(R.string.pumplimit)), this);
         }
         return insulin;
     }
