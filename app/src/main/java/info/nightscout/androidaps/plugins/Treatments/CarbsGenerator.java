6403c5d9976f73d4b88b9f3b982f65c080d1650e
Milos Kozak <m.kozak@sysop.cz>
2018-09-21 23:24:50 +0200

    fix carb generator

--- app/src/main/java/info/nightscout/androidaps/plugins/Treatments/CarbsGenerator.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/Treatments/CarbsGenerator.java
@@ -11,6 +11,7 @@ import info.nightscout.androidaps.db.Source;
 import info.nightscout.androidaps.plugins.ConfigBuilder.ConfigBuilderPlugin;
 import info.nightscout.androidaps.plugins.Overview.Dialogs.ErrorHelperActivity;
 import info.nightscout.androidaps.queue.Callback;
+import info.nightscout.utils.DateUtil;
 
 import static info.nightscout.utils.DateUtil.now;
 
@@ -28,8 +29,11 @@ public class CarbsGenerator {
     }
 
     public static void createCarb(int carbs, long time, String eventType, @Nullable String notes) {
+        long now = DateUtil.now();
+        int carbTime = (int) ((time - now) / 1000 / 60);
         DetailedBolusInfo carbInfo = new DetailedBolusInfo();
-        carbInfo.date = time;
+        carbInfo.date = now;
+        carbInfo.carbTime = carbTime;
         carbInfo.eventType = eventType;
         carbInfo.carbs = carbs;
         carbInfo.context = MainApp.instance();
