51315fc942e8c67855d1d158b46afef4aea8f170
Johannes Mockenhaupt <git@jotomo.de>
2018-10-02 18:03:45 +0200

    GH#1480: Improve conversion of incoming NS-PS name.

--- app/src/main/java/info/nightscout/utils/PercentageSplitter.java
+++ app/src/main/java/info/nightscout/utils/PercentageSplitter.java
@@ -9,11 +9,14 @@ import java.util.regex.Pattern;
 
 public class PercentageSplitter {
     // "Profile name (200%,2h)"
-    private static final Pattern percentagePattern = Pattern.compile("(.+)\\(\\d+%,\\d+h\\)");
+    private static final Pattern percentagePattern = Pattern.compile("(.+)\\(\\d+%,-?\\d+h\\)");
     // "Profile name (200%)"
     private static final Pattern percentageShiftPattern = Pattern.compile("(.+)\\(\\d+%\\)");
 
-    /** Removes the suffix for percentage and timeshift from a profile name. */
+    /** Removes the suffix for percentage and timeshift from a profile name. This is the inverse of what
+     * {@link info.nightscout.androidaps.db.ProfileSwitch#getCustomizedName()} does.
+     * Since the customized name is used for the PS upload to NS, this is needed get the original profile name
+     * when retrieving the PS from NS again. */
     public static String pureName(String name) {
         Matcher percentageMatch = percentagePattern.matcher(name);
         if (percentageMatch.find()) {
