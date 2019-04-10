cefee4827bd37d5745b06a515fabe1a958cc7502
Johannes Mockenhaupt <git@jotomo.de>
2019-04-10 14:58:01 +0200

    TDDing

--- app/src/test/java/info/nightscout/androidaps/plugins/general/versionChecker/VersionCheckerUtilsKtTest.kt
+++ app/src/test/java/info/nightscout/androidaps/plugins/general/versionChecker/VersionCheckerUtilsKtTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 
 class VersionCheckerUtilsKtTest {
     @Test
-    fun findVersionMatches() {
+    fun findVersionMatchesRegularVersion() {
         val buildGradle = """blabla
             |   android {
             |      aosenuthoae
@@ -15,5 +15,26 @@ class VersionCheckerUtilsKtTest {
         """.trimMargin()
         val detectedVersion: String? = buildGradle.byteInputStream().findVersion()
         assertEquals("2.2.2", detectedVersion)
+    }
+
+    // 04. Break stuff.mp3 like it's 1999. Again. Pizza delivery! For i c wiener ...
+    //@Test
+    fun findVersionMatchesCustomVersion() {
+        val buildGradle = """blabla
+            |   android {
+            |      aosenuthoae
+            |   }
+            |   version = "2.2.2-nefarious-underground-mod"
+            |   appName = "Aaoeu"
+        """.trimMargin()
+        val detectedVersion: String? = buildGradle.byteInputStream().findVersion()
+        assertEquals("2.2.2", detectedVersion)
+    }
+
+    @Test
+    fun findVersionMatchesDoesNotMatchErrorResponse() {
+        val buildGradle = """<html><body>Balls! No build.gradle here. Move along</body><html>"""
+        val detectedVersion: String? = buildGradle.byteInputStream().findVersion()
+        assertEquals(null, detectedVersion)
     }
 }
