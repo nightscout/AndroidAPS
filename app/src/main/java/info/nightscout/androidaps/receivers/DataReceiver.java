b04641e46616832eccaa7b1ecd579dc80ffcb3e1
Milos Kozak <m.kozak@sysop.cz>
2019-08-26 15:06:34 +0200

    Revert "DataService as JobIntentService"
    This reverts commit e0c2e7d57918c25e8421fb3bb690d51d70120497.

--- app/src/main/java/info/nightscout/androidaps/receivers/DataReceiver.java
+++ app/src/main/java/info/nightscout/androidaps/receivers/DataReceiver.java
@@ -1,9 +1,8 @@
 package info.nightscout.androidaps.receivers;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import androidx.legacy.content.WakefulBroadcastReceiver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,16 +10,15 @@ import org.slf4j.LoggerFactory;
 import info.nightscout.androidaps.logging.L;
 import info.nightscout.androidaps.services.DataService;
 
-public class DataReceiver extends BroadcastReceiver {
+public class DataReceiver extends WakefulBroadcastReceiver {
     private static Logger log = LoggerFactory.getLogger(L.DATASERVICE);
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (L.isEnabled(L.DATASERVICE))
             log.debug("onReceive " + intent);
-        // Explicitly specify that GcmIntentService will handle the intent.
-        ComponentName comp = new ComponentName(context.getPackageName(),
-                DataService.class.getName());
-        DataService.enqueueWork(context, intent.setComponent(comp));
+        startWakefulService(context, new Intent(context, DataService.class)
+                .setAction(intent.getAction())
+                .putExtras(intent));
     }
 }
