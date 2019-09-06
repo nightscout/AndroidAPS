00349c13ac20cb0242ce02633649ab2a7fc1f51c
Milos Kozak <m.kozak@sysop.cz>
2019-09-06 15:02:41 +0200

    more exam tasks

--- app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective2.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective2.java
@@ -1,5 +1,6 @@
 package info.nightscout.androidaps.plugins.constraints.objectives.objectives;
 
+import java.util.Collections;
 import java.util.List;
 
 import info.nightscout.androidaps.R;
@@ -33,5 +34,37 @@ public class Objective2 extends Objective {
                 .option(new Option(R.string.offlineprofile_nsprofile, true))
                 .hint(new Hint(R.string.offlineprofile_hint1))
         );
-     }
+        tasks.add(new ExamTask(R.string.pumpdisconnect, R.string.pumpdisconnect_whattodo,"pumpdisconnect")
+                .option(new Option(R.string.pumpdisconnect_letknow, true))
+                .option(new Option(R.string.pumpdisconnect_dontchnage, false))
+                .hint(new Hint(R.string.pumpdisconnect_hint1))
+        );
+        tasks.add(new ExamTask(R.string.objectives, R.string.objectives_howtosave,"objectives")
+                .option(new Option(R.string.objectives_writetopaper, false))
+                .option(new Option(R.string.objectives_exportsettings, true))
+                .option(new Option(R.string.objectives_storeelsewhere, true))
+                .option(new Option(R.string.objectives_doexportonstart, false))
+                .option(new Option(R.string.objectives_doexportafterchange, true))
+                .option(new Option(R.string.objectives_doexportafterfirtssettings, true))
+                .hint(new Hint(R.string.objectives_hint1))
+        );
+        tasks.add(new ExamTask(R.string.noisycgm, R.string.noisycgm_whattodo,"noisycgm")
+                .option(new Option(R.string.nothing, false))
+                .option(new Option(R.string.disconnectpumpfor1h, false))
+                .option(new Option(R.string.noisycgm_pause, true))
+                .option(new Option(R.string.noisycgm_replacesensor, true))
+                .option(new Option(R.string.noisycgm_turnoffcgmreceiver, false))
+                .option(new Option(R.string.noisycgm_checksmoothing, true))
+                .hint(new Hint(R.string.noisycgm_hint1))
+        );
+
+        for (Task task : tasks)
+            Collections.shuffle(((ExamTask)task).options);
+    }
+
+    @Override
+    public boolean isRevertable() {
+        return true;
+    }
+
 }
