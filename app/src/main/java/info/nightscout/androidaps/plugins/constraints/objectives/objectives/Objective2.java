45638ef5ebbfbeb5658ef425e71ea73c27ce5b64
Milos Kozak <m.kozak@sysop.cz>
2019-09-07 11:56:16 +0200

    more exams

--- app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective2.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/constraints/objectives/objectives/Objective2.java
@@ -56,6 +56,27 @@ public class Objective2 extends Objective {
                 .option(new Option(R.string.noisycgm_checksmoothing, true))
                 .hint(new Hint(R.string.noisycgm_hint1))
         );
+        tasks.add(new ExamTask(R.string.exercise, R.string.exercise_whattodo,"exercise")
+                .option(new Option(R.string.nothing, false))
+                .option(new Option(R.string.exercise_setactivitytt, true))
+                .option(new Option(R.string.exercise_switchprofilebelow100, true))
+                .option(new Option(R.string.exercise_switchprofileabove100, false))
+                .option(new Option(R.string.exercise_stoploop, false))
+                .option(new Option(R.string.exercise_doitbeforestart, true))
+                .option(new Option(R.string.exercise_doitafterstart, false))
+                .hint(new Hint(R.string.exercise_hint1))
+        );
+       tasks.add(new ExamTask(R.string.suspendloop, R.string.suspendloop_doigetinsulin,"suspendloop")
+                .option(new Option(R.string.suspendloop_yes, true))
+                .option(new Option(R.string.no, false))
+                 .hint(new Hint(R.string.exercise_hint1))
+        );
+       tasks.add(new ExamTask(R.string.basaltest, R.string.basaltest_when,"basaltest")
+                .option(new Option(R.string.basaltest_beforeloop, true))
+                .option(new Option(R.string.basaltest_havingregularhypo, true))
+                .option(new Option(R.string.basaltest_havingregularhyper, true))
+                 .hint(new Hint(R.string.basaltest_hint1))
+        );
 
         for (Task task : tasks)
             Collections.shuffle(((ExamTask)task).options);
