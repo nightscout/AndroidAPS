a66b3f70903392512c6ab96bef06d1e6a8c22c98
Milos Kozak <m.kozak@sysop.cz>
2019-12-03 21:07:31 +0100

    allow sorting of automation tasks

--- app/src/main/java/info/nightscout/androidaps/plugins/general/automation/AutomationFragment.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/automation/AutomationFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import info.nightscout.androidaps.R
 import info.nightscout.androidaps.plugins.bus.RxBus
 import info.nightscout.androidaps.plugins.general.automation.dialogs.EditEventDialog
+import info.nightscout.androidaps.plugins.general.automation.dragHelpers.OnStartDragListener
+import info.nightscout.androidaps.plugins.general.automation.dragHelpers.SimpleItemTouchHelperCallback
 import info.nightscout.androidaps.plugins.general.automation.events.EventAutomationDataChanged
 import info.nightscout.androidaps.plugins.general.automation.events.EventAutomationUpdateGui
 import info.nightscout.androidaps.utils.FabricPrivacy
@@ -17,10 +21,13 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.automation_fragment.*
 
-class AutomationFragment : Fragment() {
+
+class AutomationFragment : Fragment(), OnStartDragListener {
 
     private var disposable: CompositeDisposable = CompositeDisposable()
     private var eventListAdapter: EventListAdapter? = null
+
+    private var itemTouchHelper: ItemTouchHelper? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.automation_fragment, container, false)
@@ -29,7 +36,7 @@ class AutomationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        eventListAdapter = EventListAdapter(AutomationPlugin.automationEvents, fragmentManager, activity)
+        eventListAdapter = EventListAdapter(AutomationPlugin.automationEvents, fragmentManager, activity, this)
         automation_eventListView.layoutManager = LinearLayoutManager(context)
         automation_eventListView.adapter = eventListAdapter
 
@@ -41,6 +48,10 @@ class AutomationFragment : Fragment() {
             dialog.arguments = args
             fragmentManager?.let { dialog.show(it, "EditEventDialog") }
         }
+
+        val callback: ItemTouchHelper.Callback = SimpleItemTouchHelperCallback(eventListAdapter!!)
+        itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper?.attachToRecyclerView(automation_eventListView)
 
     }
 
@@ -79,6 +90,10 @@ class AutomationFragment : Fragment() {
         for (l in AutomationPlugin.executionLog.reversed())
             sb.append(l).append("\n")
         automation_logView?.text = sb.toString()
+    }
+
+    override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
+        itemTouchHelper?.startDrag(viewHolder);
     }
 
 }
