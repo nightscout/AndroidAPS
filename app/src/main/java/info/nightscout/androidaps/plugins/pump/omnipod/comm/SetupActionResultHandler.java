8c07e747d4635c1c8ed368dc00cb785de01e6e8d
Bart Sopers <bart@sopers.nl>
2019-12-04 17:21:37 +0100

    Add TODOs

--- app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/comm/SetupActionResultHandler.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/comm/SetupActionResultHandler.java
@@ -1,5 +1,6 @@
 package info.nightscout.androidaps.plugins.pump.omnipod.comm;
 
+// TODO replace with Consumer when our min API level >= 24
 @FunctionalInterface
 public interface SetupActionResultHandler {
     void handle(SetupActionResult result);
--- app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/comm/StatusResponseHandler.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/comm/StatusResponseHandler.java
@@ -2,6 +2,7 @@ package info.nightscout.androidaps.plugins.pump.omnipod.comm;
 
 import info.nightscout.androidaps.plugins.pump.omnipod.comm.message.response.StatusResponse;
 
+// TODO replace with Consumer when our min API level >= 24
 @FunctionalInterface
 public interface StatusResponseHandler {
     void handle(StatusResponse statusResponse);
