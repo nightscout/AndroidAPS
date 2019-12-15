a8f702ddac5f11409ab9c282a8bb6ec2ec5677c1
Bart Sopers <bart@sopers.nl>
2019-12-15 22:57:39 +0100

    Remove inappropriate copyright and remove unused imports

--- app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dialogs/wizard/initpod/FullInitPodWizardModel.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dialogs/wizard/initpod/FullInitPodWizardModel.java
@@ -1,32 +1,12 @@
-/*
- * Copyright 2012 Roman Nurik
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package info.nightscout.androidaps.plugins.pump.omnipod.dialogs.wizard.initpod;
 
 import android.content.Context;
 
-import androidx.fragment.app.Fragment;
-
 import com.atech.android.library.wizardpager.model.DisplayTextPage;
-import com.tech.freak.wizardpager.model.AbstractWizardModel;
 import com.tech.freak.wizardpager.model.PageList;
 
 import info.nightscout.androidaps.R;
 import info.nightscout.androidaps.plugins.pump.omnipod.defs.PodInitActionType;
-import info.nightscout.androidaps.plugins.pump.omnipod.dialogs.wizard.pages.PodInfoFragment;
 
 /**
  * Created by andy on 12/11/2019
