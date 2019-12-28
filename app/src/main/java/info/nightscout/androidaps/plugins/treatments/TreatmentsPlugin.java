244e99f5c9a5457801f03a3ce9369250dbc56f0d
Johannes Mockenhaupt <git@jotomo.de>
2019-12-28 17:03:19 +0100

    Refactor: improve misleading method name.

--- app/src/main/java/info/nightscout/androidaps/plugins/treatments/TreatmentsPlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/treatments/TreatmentsPlugin.java
@@ -361,11 +361,10 @@ public class TreatmentsPlugin extends PluginBase implements TreatmentsInterface 
 
 
     @Override
-    public List<Treatment> getTreatments5MinBackFromHistory(long time) {
+    public List<Treatment> getCarbTreatments5MinBackFromHistory(long time) {
         List<Treatment> in5minback = new ArrayList<>();
         synchronized (treatments) {
-            for (Integer pos = 0; pos < treatments.size(); pos++) {
-                Treatment t = treatments.get(pos);
+            for (Treatment t : treatments) {
                 if (!t.isValid)
                     continue;
                 if (t.date <= time && t.date > time - 5 * 60 * 1000 && t.carbs > 0)
