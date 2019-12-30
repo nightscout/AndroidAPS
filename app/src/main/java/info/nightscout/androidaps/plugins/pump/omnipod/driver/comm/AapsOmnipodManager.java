a17defb817848247d72014db300aed1c81bbf906
Bart Sopers <bart@sopers.nl>
2019-12-30 13:32:06 -0500

    Report implicitly cancelled tbr when deactivating pod or resetting pod state

--- app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/driver/comm/AapsOmnipodManager.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/driver/comm/AapsOmnipodManager.java
@@ -217,13 +217,16 @@ public class AapsOmnipodManager implements OmnipodCommunicationManagerInterface 
         long time = System.currentTimeMillis();
         try {
             delegate.deactivatePod();
-            addSuccessToHistory(time, PodHistoryEntryType.DeactivatePod, null);
         } catch (Exception ex) {
             String comment = handleAndTranslateException(ex);
             podInitReceiver.returnInitTaskStatus(PodInitActionType.DeactivatePodWizardStep, false, comment);
             addFailureToHistory(time, PodHistoryEntryType.DeactivatePod, comment);
             return new PumpEnactResult().success(false).enacted(false).comment(comment);
         }
+
+        addSuccessToHistory(time, PodHistoryEntryType.DeactivatePod, null);
+
+        reportImplicitlyCanceledTbr();
 
         podInitReceiver.returnInitTaskStatus(PodInitActionType.DeactivatePodWizardStep, true, null);
 
@@ -263,6 +266,8 @@ public class AapsOmnipodManager implements OmnipodCommunicationManagerInterface 
     @Override
     public PumpEnactResult resetPodStatus() {
         delegate.resetPodState();
+
+        reportImplicitlyCanceledTbr();
 
         OmnipodUtil.setPodSessionState(null);
 
