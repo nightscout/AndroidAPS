e95d6116a8012b95b046dc09934fb67764ba8fae
Milos Kozak <m.kozak@sysop.cz>
2020-04-24 18:36:39 +0200

    allow null BT name

--- app/src/main/java/info/nightscout/androidaps/events/EventBTChange.kt
+++ app/src/main/java/info/nightscout/androidaps/events/EventBTChange.kt
@@ -1,6 +1,6 @@
 package info.nightscout.androidaps.events
 
-class EventBTChange constructor(val state: Change, val deviceName: String) : Event() {
+class EventBTChange constructor(val state: Change, val deviceName: String?) : Event() {
     enum class Change {
         CONNECT,
         DISCONNECT
