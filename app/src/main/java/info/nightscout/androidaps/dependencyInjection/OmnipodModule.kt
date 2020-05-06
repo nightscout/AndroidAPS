25e0b8b19013528590f45ee2e7a651b13bd6c305
Bart Sopers <bart@sopers.nl>
2020-05-06 23:38:56 +0200

    Add dagger stuff

--- app/src/main/java/info/nightscout/androidaps/dependencyInjection/OmnipodModule.kt
+++ app/src/main/java/info/nightscout/androidaps/dependencyInjection/OmnipodModule.kt
@@ -3,6 +3,7 @@ package info.nightscout.androidaps.dependencyInjection
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import info.nightscout.androidaps.plugins.pump.omnipod.comm.OmnipodCommunicationManager
+import info.nightscout.androidaps.plugins.pump.omnipod.defs.state.PodSessionState
 import info.nightscout.androidaps.plugins.pump.omnipod.dialogs.wizard.pages.InitPodRefreshAction
 import info.nightscout.androidaps.plugins.pump.omnipod.driver.comm.AapsOmnipodManager
 import info.nightscout.androidaps.plugins.pump.omnipod.driver.ui.OmnipodUITask
@@ -15,5 +16,6 @@ abstract class OmnipodModule {
     @ContributesAndroidInjector abstract fun omnipodUITaskProvider(): OmnipodUITask
     @ContributesAndroidInjector abstract fun aapsOmnipodManagerProvider(): AapsOmnipodManager
     @ContributesAndroidInjector abstract fun initPodRefreshAction(): InitPodRefreshAction
+    @ContributesAndroidInjector abstract fun podSessionState(): PodSessionState
 
 }
