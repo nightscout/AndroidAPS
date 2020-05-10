2375b159face4e7afac6eaf79f307d18c104af8f
Milos Kozak <m.kozak@sysop.cz>
2020-05-10 22:31:36 +0200

    resources cleanup

--- app/src/main/java/info/nightscout/androidaps/plugins/general/automation/actions/ActionSendSMS.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/automation/actions/ActionSendSMS.kt
@@ -26,7 +26,7 @@ class ActionSendSMS(injector: HasAndroidInjector) : Action(injector) {
 
     override fun doAction(callback: Callback) {
         val result = smsCommunicatorPlugin.sendNotificationToAllNumbers(text.value)
-        callback.result(PumpEnactResult(injector).success(result).comment(if (result) R.string.ok else R.string.danar_error))?.run()
+        callback.result(PumpEnactResult(injector).success(result).comment(if (result) R.string.ok else R.string.error))?.run()
     }
 
     override fun toJSON(): String {
