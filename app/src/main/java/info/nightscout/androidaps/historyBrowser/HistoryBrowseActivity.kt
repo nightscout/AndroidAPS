eeb4ce1686d2e5b2dd427dc276cf4aae8c0c121b
Milos Kozak <m.kozak@sysop.cz>
2020-05-19 12:52:01 +0200

    fix HistoryBrowser injection

--- app/src/main/java/info/nightscout/androidaps/historyBrowser/HistoryBrowseActivity.kt
+++ app/src/main/java/info/nightscout/androidaps/historyBrowser/HistoryBrowseActivity.kt
@@ -49,7 +49,8 @@ class HistoryBrowseActivity : NoSplashAppCompatActivity() {
     @Inject lateinit var resourceHelper: ResourceHelper
     @Inject lateinit var profileFunction: ProfileFunction
     @Inject lateinit var defaultValueHelper: DefaultValueHelper
-    @Inject lateinit var iobCobStaticCalculatorPlugin: IobCobStaticCalculatorPlugin
+    @Inject lateinit var iobCobCalculatorPluginHistory: IobCobCalculatorPluginHistory
+    @Inject lateinit var treatmentsPluginHistory: TreatmentsPluginHistory
     @Inject lateinit var activePlugin: ActivePluginProvider
     @Inject lateinit var buildHelper: BuildHelper
     @Inject lateinit var fabricPrivacy: FabricPrivacy
@@ -148,7 +149,7 @@ class HistoryBrowseActivity : NoSplashAppCompatActivity() {
     public override fun onPause() {
         super.onPause()
         disposable.clear()
-        iobCobStaticCalculatorPlugin.stopCalculation("onPause")
+        iobCobCalculatorPluginHistory.stopCalculation("onPause")
     }
 
     public override fun onResume() {
@@ -157,7 +158,7 @@ class HistoryBrowseActivity : NoSplashAppCompatActivity() {
             .toObservable(EventAutosensCalculationFinished::class.java)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ event: EventAutosensCalculationFinished ->
-                // catch only events from iobCobStaticCalculatorPlugin
+                // catch only events from iobCobCalculatorPluginHistory
                 if (event.cause === eventCustomCalculationFinished) {
                     aapsLogger.debug(LTag.AUTOSENS, "EventAutosensCalculationFinished")
                     updateGUI("EventAutosensCalculationFinished")
@@ -220,10 +221,11 @@ class HistoryBrowseActivity : NoSplashAppCompatActivity() {
     }
 
     private fun runCalculation(from: String) {
+        treatmentsPluginHistory.initializeData(start - T.hours(8).msecs())
         val end = start + T.hours(rangeToDisplay.toLong()).msecs()
-        iobCobStaticCalculatorPlugin.stopCalculation(from)
-        iobCobStaticCalculatorPlugin.clearCache()
-        iobCobStaticCalculatorPlugin.runCalculation(from, end, true, false, eventCustomCalculationFinished)
+        iobCobCalculatorPluginHistory.stopCalculation(from)
+        iobCobCalculatorPluginHistory.clearCache()
+        iobCobCalculatorPluginHistory.runCalculation(from, end, true, false, eventCustomCalculationFinished)
     }
 
     @Synchronized
@@ -242,7 +244,7 @@ class HistoryBrowseActivity : NoSplashAppCompatActivity() {
 
         GlobalScope.launch(Dispatchers.Main) {
             historybrowse_bggraph ?: return@launch
-            val graphData = GraphData(injector, historybrowse_bggraph, iobCobStaticCalculatorPlugin)
+            val graphData = GraphData(injector, historybrowse_bggraph, iobCobCalculatorPluginHistory, treatmentsPluginHistory)
             val secondaryGraphsData: ArrayList<GraphData> = ArrayList()
 
             // do preparation in different thread
@@ -279,7 +281,7 @@ class HistoryBrowseActivity : NoSplashAppCompatActivity() {
 
                 // ------------------ 2nd graph
                 for (g in 0 until secondaryGraphs.size) {
-                    val secondGraphData = GraphData(injector, secondaryGraphs[g], iobCobStaticCalculatorPlugin)
+                    val secondGraphData = GraphData(injector, secondaryGraphs[g], iobCobCalculatorPluginHistory, treatmentsPluginHistory)
                     var useIobForScale = false
                     var useCobForScale = false
                     var useDevForScale = false
