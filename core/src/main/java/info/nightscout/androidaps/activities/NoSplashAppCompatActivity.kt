380ee4aea046808a2003ba299f6755e94951c2e0
osodebailar <andreas.eggen@gmail.com>
2020-05-29 16:42:38 +0200

    WIP: newgui into dev

--- core/src/main/java/info/nightscout/androidaps/activities/NoSplashAppCompatActivity.kt
+++ core/src/main/java/info/nightscout/androidaps/activities/NoSplashAppCompatActivity.kt
@@ -3,6 +3,7 @@ package info.nightscout.androidaps.activities
 import android.content.Context
 import android.os.Bundle
 import dagger.android.support.DaggerAppCompatActivity
+import info.nightscout.androidaps.core.R
 import info.nightscout.androidaps.utils.locale.LocaleHelper
 import info.nightscout.androidaps.utils.sharedPreferences.SP
 import javax.inject.Inject
@@ -11,6 +12,7 @@ import javax.inject.Inject
 open class NoSplashAppCompatActivity : DaggerAppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
     }
 
     public override fun attachBaseContext(newBase: Context) {
