5bd623e3222f2a74dc0389154d4c63c5ab322675
Bart Sopers <bart@sopers.nl>
2020-09-01 22:05:59 +0200

    Remove unresolved Omnipod import after merge

--- app/src/main/java/info/nightscout/androidaps/queue/commands/CommandStartPump.kt
+++ app/src/main/java/info/nightscout/androidaps/queue/commands/CommandStartPump.kt
@@ -3,8 +3,8 @@ package info.nightscout.androidaps.queue.commands
 import dagger.android.HasAndroidInjector
 import info.nightscout.androidaps.interfaces.ActivePluginProvider
 import info.nightscout.androidaps.interfaces.ProfileFunction
+import info.nightscout.androidaps.plugins.pump.common.defs.PumpType
 import info.nightscout.androidaps.plugins.pump.insight.LocalInsightPlugin
-import info.nightscout.androidaps.plugins.pump.omnipod.OmnipodPumpPlugin
 import info.nightscout.androidaps.queue.Callback
 import javax.inject.Inject
 
@@ -21,7 +21,7 @@ class CommandStartPump(
         if (pump is LocalInsightPlugin) {
             val result = pump.startPump()
             callback?.result(result)?.run()
-        } else if (pump is OmnipodPumpPlugin) {
+        } else if (pump.pumpDescription.pumpType == PumpType.Insulet_Omnipod) {
             // When using CommandQueue.setProfile, it refuses to set the profile is the same as the current profile
             // However we need to set the current profile to resume delivery in case the Pod is suspended
             if (profileFunction.getProfile() != null) {
