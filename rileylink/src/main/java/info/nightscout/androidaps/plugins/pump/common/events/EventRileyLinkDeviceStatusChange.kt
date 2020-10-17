f64cc6629849ecda29b6119095219fbc3682a25d
Bart Sopers <bart@sopers.nl>
2020-10-17 19:47:39 +0200

    Improve Setup Wizard for Omnipod

--- rileylink/src/main/java/info/nightscout/androidaps/plugins/pump/common/events/EventRileyLinkDeviceStatusChange.kt
+++ rileylink/src/main/java/info/nightscout/androidaps/plugins/pump/common/events/EventRileyLinkDeviceStatusChange.kt
@@ -1,11 +1,13 @@
 package info.nightscout.androidaps.plugins.pump.common.events
 
-import info.nightscout.androidaps.events.Event
+import info.nightscout.androidaps.events.EventStatus
 import info.nightscout.androidaps.plugins.pump.common.defs.PumpDeviceState
 import info.nightscout.androidaps.plugins.pump.common.hw.rileylink.defs.RileyLinkError
 import info.nightscout.androidaps.plugins.pump.common.hw.rileylink.defs.RileyLinkServiceState
+import info.nightscout.androidaps.plugins.pump.common.hw.rileylink.defs.RileyLinkTargetDevice
+import info.nightscout.androidaps.utils.resources.ResourceHelper
 
-open class EventRileyLinkDeviceStatusChange : Event {
+open class EventRileyLinkDeviceStatusChange : EventStatus {
 
     var rileyLinkServiceState: RileyLinkServiceState? = null
     var rileyLinkError: RileyLinkError? = null
@@ -14,9 +16,7 @@ open class EventRileyLinkDeviceStatusChange : Event {
     var errorDescription: String? = null
 
     constructor() {
-
     }
-
 
     constructor(rileyLinkServiceState: RileyLinkServiceState?, rileyLinkError: RileyLinkError?) {
         this.rileyLinkServiceState = rileyLinkServiceState
@@ -30,5 +30,17 @@ open class EventRileyLinkDeviceStatusChange : Event {
     constructor(pumpDeviceState: PumpDeviceState?, errorDescription: String?) {
         this.pumpDeviceState = pumpDeviceState
         this.errorDescription = errorDescription
+    }
+
+    override fun getStatus(resourceHelper: ResourceHelper): String {
+        val rileyLinkServiceState = this.rileyLinkServiceState ?: return ""
+        val resourceId = rileyLinkServiceState.resourceId
+        val rileyLinkError = this.rileyLinkError
+
+        if (rileyLinkServiceState.isError && rileyLinkError != null) {
+            return resourceHelper.gs(rileyLinkError.getResourceId(RileyLinkTargetDevice.Omnipod))
+        }
+
+        return resourceHelper.gs(resourceId)
     }
 }
