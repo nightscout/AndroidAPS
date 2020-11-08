e89eb67236f28d79e8cdc3bbf47cd21982e6e495
Brian Quinion <git@brian.quinion.co.uk>
2020-11-08 16:31:10 +0000

    Update hard limits based on looping in pregnancy group feedback

--- app/src/main/java/info/nightscout/androidaps/utils/HardLimits.kt
+++ app/src/main/java/info/nightscout/androidaps/utils/HardLimits.kt
@@ -25,7 +25,7 @@ class HardLimits @Inject constructor(
     val ADULT = 2
     val RESISTANTADULT = 3
     val PREGNANT = 4
-    val MAXBOLUS = doubleArrayOf(5.0, 10.0, 17.0, 25.0, 30.0)
+    val MAXBOLUS = doubleArrayOf(5.0, 10.0, 17.0, 25.0, 60.0)
 
     // Very Hard Limits Ranges
     // First value is the Lowest and second value is the Highest a Limit can define
@@ -39,13 +39,13 @@ class HardLimits @Inject constructor(
     val VERY_HARD_LIMIT_TEMP_TARGET_BG = intArrayOf(72, 200)
     val MINDIA = doubleArrayOf(5.0, 5.0, 5.0, 5.0, 5.0)
     val MAXDIA = doubleArrayOf(7.0, 7.0, 7.0, 7.0, 10.0)
-    val MINIC = doubleArrayOf(2.0, 2.0, 2.0, 2.0, 0.5)
-    val MAXIC = doubleArrayOf(100.0, 100.0, 100.0, 100.0)
+    val MINIC = doubleArrayOf(2.0, 2.0, 2.0, 2.0, 0.3)
+    val MAXIC = doubleArrayOf(100.0, 100.0, 100.0, 100.0, 100.0)
     val MINISF = 2.0 // mgdl
     val MAXISF = 720.0 // mgdl
-    val MAXIOB_AMA = doubleArrayOf(3.0, 5.0, 7.0, 12.0, 15.0)
-    val MAXIOB_SMB = doubleArrayOf(3.0, 7.0, 12.0, 25.0, 30.0)
-    val MAXBASAL = doubleArrayOf(2.0, 5.0, 10.0, 12.0, 15.0)
+    val MAXIOB_AMA = doubleArrayOf(3.0, 5.0, 7.0, 12.0, 25.0)
+    val MAXIOB_SMB = doubleArrayOf(3.0, 7.0, 12.0, 25.0, 40.0)
+    val MAXBASAL = doubleArrayOf(2.0, 5.0, 10.0, 12.0, 25.0)
 
     //LGS Hard limits
     //No IOB at all
