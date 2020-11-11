c16fc3a946dfbe9ce3e1a33225830df36a38e734
Bart Sopers <bart@sopers.nl>
2020-11-11 20:12:57 +0100

    Show disconnected icon in overview for Omnipod when Pod is suspended and hide disconnect buttons in overview for Omnipod

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewMenus.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewMenus.kt
@@ -34,6 +34,7 @@ import info.nightscout.androidaps.plugins.aps.loop.LoopPlugin
 import info.nightscout.androidaps.plugins.bus.RxBusWrapper
 import info.nightscout.androidaps.plugins.configBuilder.ConfigBuilderPlugin
 import info.nightscout.androidaps.interfaces.ProfileFunction
+import info.nightscout.androidaps.plugins.pump.common.defs.PumpType
 import info.nightscout.androidaps.queue.Callback
 import info.nightscout.androidaps.utils.DateUtil
 import info.nightscout.androidaps.utils.DefaultValueHelper
@@ -198,7 +199,11 @@ class OverviewMenus @Inject constructor(
                     menu.add(resourceHelper.gs(R.string.enableloop))
                 }
                 if (!loopPlugin.isDisconnected) {
-                    showSuspendPump(menu, pumpDescription)
+                    if(pumpDescription.pumpType != PumpType.Insulet_Omnipod) {
+                        // Don't show the disconnect menu for Omnipod,
+                        // as an Omnipod is attached to your body and cannot be physically disconnected.
+                        showSuspendPump(menu, pumpDescription)
+                    }
                 } else {
                     menu.add(resourceHelper.gs(R.string.reconnect))
                 }
