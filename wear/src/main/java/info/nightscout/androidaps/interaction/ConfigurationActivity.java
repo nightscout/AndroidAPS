34ec0e92db4769f2028e4754a975a7131716bf4c
rICTx-T1D <lolo49lala@gmail.com>
2020-11-13 20:05:33 +0100

    Make watchface configuration available for each watchface...

--- wear/src/main/java/info/nightscout/androidaps/interaction/ConfigurationActivity.java
+++ wear/src/main/java/info/nightscout/androidaps/interaction/ConfigurationActivity.java
@@ -1,19 +1,24 @@
-package info.nightscout.androidaps.interaction.configurationActivities;
+package info.nightscout.androidaps.interaction;
 
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-
 import info.nightscout.androidaps.R;
 import preference.WearPreferenceActivity;
 
-public class DigitalStyleConfigAct extends WearPreferenceActivity {
+public class ConfigurationActivity extends WearPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("watchface");
-        addPreferencesFromResource(R.xml.watch_face_digitalstyle_configuration);
+        setTitle("Watchface");
+        String configFileName=getIntent().getAction();
+        int resXmlId = getResources().getIdentifier(configFileName, "xml", getApplicationContext().getPackageName());
+        Log.d("ConfigurationActivity::onCreate --->> getIntent().getAction()",configFileName);
+        Log.d("ConfigurationActivity::onCreate --->> resXmlId",String.valueOf(resXmlId));
+        addPreferencesFromResource(resXmlId);
         ViewGroup view = (ViewGroup) getWindow().getDecorView();
         removeBackgroundRecursively(view);
         view.setBackground(getResources().getDrawable(R.drawable.settings_background));
