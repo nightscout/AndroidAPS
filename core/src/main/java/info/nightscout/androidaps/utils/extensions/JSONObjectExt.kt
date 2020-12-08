bc60363618eac6aafba2c6b616216a27ab5d252c
Milos Kozak <m.kozak@sysop.cz>
2020-12-08 20:23:12 +0100

    fix storing SP from AAPS

--- core/src/main/java/info/nightscout/androidaps/utils/extensions/JSONObjectExt.kt
+++ core/src/main/java/info/nightscout/androidaps/utils/extensions/JSONObjectExt.kt
@@ -21,27 +21,27 @@ fun JSONObject.putBoolean(@StringRes key: Int, sp: SP, resourceHelper: ResourceH
     if (sp.contains(key)) put(resourceHelper.gs(key), sp.getBoolean(key, false)) else this
 
 fun JSONObject.storeInt(@StringRes key: Int, sp: SP, resourceHelper: ResourceHelper): JSONObject {
-    if (has(resourceHelper.gs(key))) sp.putInt(key, getInt(resourceHelper.gs(key)))
+    if (has(resourceHelper.gs(key))) sp.putString(key, getInt(resourceHelper.gs(key)).toString())
     return this
 }
 
 fun JSONObject.storeLong(@StringRes key: Int, sp: SP, resourceHelper: ResourceHelper): JSONObject {
-    if (has(resourceHelper.gs(key))) sp.putLong(key, getLong(resourceHelper.gs(key)))
+    if (has(resourceHelper.gs(key))) sp.putString(key, getLong(resourceHelper.gs(key)).toString())
     return this
 }
 
 fun JSONObject.storeDouble(@StringRes key: Int, sp: SP, resourceHelper: ResourceHelper): JSONObject {
-    if (has(resourceHelper.gs(key))) sp.putDouble(key, getDouble(resourceHelper.gs(key)))
+    if (has(resourceHelper.gs(key))) sp.putString(key, getDouble(resourceHelper.gs(key)).toString())
     return this
 }
 
 fun JSONObject.storeString(@StringRes key: Int, sp: SP, resourceHelper: ResourceHelper): JSONObject {
-    if (has(resourceHelper.gs(key))) sp.putString(key, getString(resourceHelper.gs(key)))
+    if (has(resourceHelper.gs(key))) sp.putString(key, getString(resourceHelper.gs(key)).toString())
     return this
 }
 
 fun JSONObject.storeBoolean(@StringRes key: Int, sp: SP, resourceHelper: ResourceHelper): JSONObject {
-    if (has(resourceHelper.gs(key))) sp.putBoolean(key, getBoolean(resourceHelper.gs(key)))
+    if (has(resourceHelper.gs(key))) sp.putString(key, getBoolean(resourceHelper.gs(key)).toString())
     return this
 }
 
