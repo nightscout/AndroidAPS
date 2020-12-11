5f7108317f4aa2a347b531dec56a1cafe4fe3f23
Philoul <legoupil.jp@free.fr>
2020-12-11 10:23:04 +0100

    Move Ago below Arrow

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.kt
@@ -582,10 +582,12 @@ class OverviewFragment : DaggerFragment(), View.OnClickListener, OnLongClickList
                 overview_delta_large?.text = Profile.toSignedUnitsString(glucoseStatus.delta, glucoseStatus.delta * Constants.MGDL_TO_MMOLL, units)
                 overview_delta_large?.setTextColor(color)
                 overview_delta?.text = Profile.toSignedUnitsString(glucoseStatus.delta, glucoseStatus.delta * Constants.MGDL_TO_MMOLL, units)
-                overview_avgdelta?.text = "${Profile.toSignedUnitsString(glucoseStatus.short_avgdelta, glucoseStatus.short_avgdelta * Constants.MGDL_TO_MMOLL, units)}\n${Profile.toSignedUnitsString(glucoseStatus.long_avgdelta, glucoseStatus.long_avgdelta * Constants.MGDL_TO_MMOLL, units)}"
+                overview_avgdelta?.text = "${Profile.toSignedUnitsString(glucoseStatus.short_avgdelta, glucoseStatus.short_avgdelta * Constants.MGDL_TO_MMOLL, units)}"
+                overview_long_avgdelta?.text = "${Profile.toSignedUnitsString(glucoseStatus.long_avgdelta, glucoseStatus.long_avgdelta * Constants.MGDL_TO_MMOLL, units)}"
             } else {
                 overview_delta?.text = "Δ " + resourceHelper.gs(R.string.notavailable)
                 overview_avgdelta?.text = ""
+                overview_long_avgdelta?.text = ""
             }
 
             // strike through if BG is old
