4d02587bb79a20de0b928f4f87f4cc55d2a54dd6
Milos Kozak <m.kozak@sysop.cz>
2020-12-14 12:46:38 +0100

    AutomationEvent new options, automation jetpack bindings

--- app/src/main/java/info/nightscout/androidaps/plugins/general/automation/AutomationFragment.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/automation/AutomationFragment.kt
@@ -9,11 +9,8 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,6 +18,8 @@ import androidx.recyclerview.widget.RecyclerView
 import dagger.android.support.DaggerFragment
 import info.nightscout.androidaps.MainApp
 import info.nightscout.androidaps.R
+import info.nightscout.androidaps.databinding.AutomationEventItemBinding
+import info.nightscout.androidaps.databinding.AutomationFragmentBinding
 import info.nightscout.androidaps.plugins.bus.RxBusWrapper
 import info.nightscout.androidaps.plugins.general.automation.dialogs.EditEventDialog
 import info.nightscout.androidaps.plugins.general.automation.dragHelpers.ItemTouchHelperAdapter
@@ -34,39 +33,47 @@ import info.nightscout.androidaps.utils.FabricPrivacy
 import info.nightscout.androidaps.utils.HtmlHelper
 import info.nightscout.androidaps.utils.alertDialogs.OKDialog.showConfirmation
 import info.nightscout.androidaps.utils.extensions.plusAssign
+import info.nightscout.androidaps.utils.extensions.toVisibility
 import info.nightscout.androidaps.utils.resources.ResourceHelper
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.automation_fragment.*
 import java.util.*
 import javax.inject.Inject
 
 class AutomationFragment : DaggerFragment(), OnStartDragListener {
+
     @Inject lateinit var resourceHelper: ResourceHelper
     @Inject lateinit var rxBus: RxBusWrapper
     @Inject lateinit var fabricPrivacy: FabricPrivacy
     @Inject lateinit var automationPlugin: AutomationPlugin
-    @Inject lateinit var mainApp : MainApp
+    @Inject lateinit var mainApp: MainApp
 
     private var disposable: CompositeDisposable = CompositeDisposable()
     private lateinit var eventListAdapter: EventListAdapter
 
     private var itemTouchHelper: ItemTouchHelper? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.automation_fragment, container, false)
+    private var _binding: AutomationFragmentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = AutomationFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         eventListAdapter = EventListAdapter()
-        automation_eventListView.layoutManager = LinearLayoutManager(context)
-        automation_eventListView.adapter = eventListAdapter
+        binding.eventListView.layoutManager = LinearLayoutManager(context)
+        binding.eventListView.adapter = eventListAdapter
 
-        automation_logView.movementMethod = ScrollingMovementMethod()
+        binding.logView.movementMethod = ScrollingMovementMethod()
 
-        automation_fabAddEvent.setOnClickListener {
+        binding.fabAddEvent.setOnClickListener {
             val dialog = EditEventDialog()
             val args = Bundle()
             args.putString("event", AutomationEvent(mainApp).toJSON())
@@ -77,7 +84,7 @@ class AutomationFragment : DaggerFragment(), OnStartDragListener {
 
         val callback: ItemTouchHelper.Callback = SimpleItemTouchHelperCallback(eventListAdapter)
         itemTouchHelper = ItemTouchHelper(callback)
-        itemTouchHelper?.attachToRecyclerView(automation_eventListView)
+        itemTouchHelper?.attachToRecyclerView(binding.eventListView)
 
     }
 
@@ -105,13 +112,18 @@ class AutomationFragment : DaggerFragment(), OnStartDragListener {
         disposable.clear()
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     @Synchronized
     private fun updateGui() {
         eventListAdapter.notifyDataSetChanged()
         val sb = StringBuilder()
         for (l in automationPlugin.executionLog.reversed())
             sb.append(l).append("<br>")
-        automation_logView?.text = HtmlHelper.fromHtml(sb.toString())
+        binding.logView.text = HtmlHelper.fromHtml(sb.toString())
     }
 
     override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
@@ -132,6 +144,7 @@ class AutomationFragment : DaggerFragment(), OnStartDragListener {
     }
 
     inner class EventListAdapter : RecyclerView.Adapter<EventListAdapter.ViewHolder>(), ItemTouchHelperAdapter {
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             val v = LayoutInflater.from(parent.context).inflate(R.layout.automation_event_item, parent, false)
             return ViewHolder(v, parent.context)
@@ -147,45 +160,47 @@ class AutomationFragment : DaggerFragment(), OnStartDragListener {
         @SuppressLint("ClickableViewAccessibility")
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val event = automationPlugin.automationEvents[position]
-            holder.eventTitle.text = event.title
-            holder.enabled.isChecked = event.isEnabled
-            holder.iconLayout.removeAllViews()
+            holder.binding.eventTitle.text = event.title
+            holder.binding.enabled.isChecked = event.isEnabled
+            holder.binding.enabled.isEnabled = !event.readOnly
+            holder.binding.iconLayout.removeAllViews()
             // trigger icons
             val triggerIcons = HashSet<Int>()
             fillIconSet(event.trigger as TriggerConnector, triggerIcons)
             for (res in triggerIcons) {
-                addImage(res, holder.context, holder.iconLayout)
+                addImage(res, holder.context, holder.binding.iconLayout)
             }
             // arrow icon
             val iv = ImageView(holder.context)
             iv.setImageResource(R.drawable.ic_arrow_forward_white_24dp)
             iv.layoutParams = LinearLayout.LayoutParams(resourceHelper.dpToPx(24), resourceHelper.dpToPx(24))
             iv.setPadding(resourceHelper.dpToPx(4), 0, resourceHelper.dpToPx(4), 0)
-            holder.iconLayout.addView(iv)
+            holder.binding.iconLayout.addView(iv)
             // action icons
             val actionIcons = HashSet<Int>()
             for (action in event.actions) {
                 actionIcons.add(action.icon())
             }
             for (res in actionIcons) {
-                addImage(res, holder.context, holder.iconLayout)
+                addImage(res, holder.context, holder.binding.iconLayout)
             }
             // enabled event
-            holder.enabled.setOnClickListener {
-                event.isEnabled = holder.enabled.isChecked
+            holder.binding.enabled.setOnClickListener {
+                event.isEnabled = holder.binding.enabled.isChecked
                 rxBus.send(EventAutomationDataChanged())
             }
             // edit event
-            holder.rootLayout.setOnClickListener {
-                val dialog = EditEventDialog()
-                val args = Bundle()
-                args.putString("event", event.toJSON())
-                args.putInt("position", position)
-                dialog.arguments = args
-                dialog.show(childFragmentManager, "EditEventDialog")
-            }
+            if (!event.readOnly)
+                holder.binding.rootLayout.setOnClickListener {
+                    val dialog = EditEventDialog()
+                    val args = Bundle()
+                    args.putString("event", event.toJSON())
+                    args.putInt("position", position)
+                    dialog.arguments = args
+                    dialog.show(childFragmentManager, "EditEventDialog")
+                }
             // Start a drag whenever the handle view it touched
-            holder.iconSort.setOnTouchListener { v: View, motionEvent: MotionEvent ->
+            holder.binding.iconSort.setOnTouchListener { v: View, motionEvent: MotionEvent ->
                 if (motionEvent.action == MotionEvent.ACTION_DOWN) {
                     this@AutomationFragment.onStartDrag(holder)
                     return@setOnTouchListener true
@@ -193,16 +208,18 @@ class AutomationFragment : DaggerFragment(), OnStartDragListener {
                 v.onTouchEvent(motionEvent)
             }
             // remove event
-            holder.iconTrash.setOnClickListener {
+            holder.binding.iconTrash.setOnClickListener {
                 showConfirmation(requireContext(), resourceHelper.gs(R.string.removerecord) + " " + automationPlugin.automationEvents[position].title,
                     Runnable {
                         automationPlugin.automationEvents.removeAt(position)
                         notifyItemRemoved(position)
                         rxBus.send(EventAutomationDataChanged())
                         rxBus.send(EventAutomationUpdateGui())
-                    }, Runnable { rxBus.send(EventAutomationUpdateGui())
+                    }, Runnable {
+                    rxBus.send(EventAutomationUpdateGui())
                 })
             }
+            holder.binding.iconTrash.visibility = (!event.readOnly).toVisibility()
         }
 
         override fun getItemCount(): Int = automationPlugin.automationEvents.size
@@ -227,12 +244,8 @@ class AutomationFragment : DaggerFragment(), OnStartDragListener {
         }
 
         inner class ViewHolder(view: View, val context: Context) : RecyclerView.ViewHolder(view), ItemTouchHelperViewHolder {
-            val rootLayout: RelativeLayout = view.findViewById(R.id.rootLayout)
-            val iconLayout: LinearLayout = view.findViewById(R.id.iconLayout)
-            val eventTitle: TextView = view.findViewById(R.id.viewEventTitle)
-            val iconTrash: ImageView = view.findViewById(R.id.iconTrash)
-            val iconSort: ImageView = view.findViewById(R.id.iconSort)
-            val enabled: CheckBox = view.findViewById(R.id.automation_enabled)
+
+            val binding = AutomationEventItemBinding.bind(view)
 
             override fun onItemSelected() = itemView.setBackgroundColor(Color.LTGRAY)
 
