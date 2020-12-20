a1a203d4532b430564038d7ce3dfbd1ed3b10dd9
swissalpine <a.may@mailbox.org>
2020-12-20 17:30:38 +0100

    Add BGI > 0 for security reasons

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/openAPSSMB/DetermineBasalResultSMB.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/openAPSSMB/DetermineBasalResultSMB.java
@@ -59,7 +59,7 @@ public class DetermineBasalResultSMB extends APSResult {
                 // Get active BaseBasalRate
                 double baseBasalRate = activePlugin.getActivePump().getBaseBasalRate();
                 // Activate a small TBR
-                if ( sp.getBoolean(R.string.key_keto_protect, false) && sp.getBoolean(R.string.key_variable_keto_protect_strategy, true) && (bolusIob.iob + basalIob.basaliob) < (0 - baseBasalRate)) {
+                if ( sp.getBoolean(R.string.key_keto_protect, false) && sp.getBoolean(R.string.key_variable_keto_protect_strategy, true) && (bolusIob.iob + basalIob.basaliob) < (0 - baseBasalRate) && -(bolusIob.activity + basalIob.activity) > 0) {
                     // Variable strategy
                     double cutoff = baseBasalRate * (sp.getDouble(R.string.keto_protect_basal, 20d) * 0.01);
                     if (rate < cutoff) rate = cutoff;
