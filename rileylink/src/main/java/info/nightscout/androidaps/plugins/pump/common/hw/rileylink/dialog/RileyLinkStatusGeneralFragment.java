f1bb85eafa935a56ecb2f5198bb397cf1c47b8a5
Bart Sopers <bart@sopers.nl>
2021-01-07 02:21:41 +0100

    Distinguish between configured and connected device for Medtronic in RileyLink fragment

--- rileylink/src/main/java/info/nightscout/androidaps/plugins/pump/common/hw/rileylink/dialog/RileyLinkStatusGeneralFragment.java
+++ rileylink/src/main/java/info/nightscout/androidaps/plugins/pump/common/hw/rileylink/dialog/RileyLinkStatusGeneralFragment.java
@@ -45,8 +45,10 @@ public class RileyLinkStatusGeneralFragment extends DaggerFragment implements Re
     private TextView configuredRileyLinkName;
     private TextView batteryLevel;
     private TextView connectionError;
+    private View connectedDeviceDetails;
     private TextView deviceType;
-    private TextView deviceModel;
+    private TextView configuredDeviceModel;
+    private TextView connectedDeviceModel;
     private TextView serialNumber;
     private TextView pumpFrequency;
     private TextView lastUsedFrequency;
@@ -59,16 +61,18 @@ public class RileyLinkStatusGeneralFragment extends DaggerFragment implements Re
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onResume() {
+        super.onResume();
 
         this.connectionStatus = getActivity().findViewById(R.id.rls_t1_connection_status);
         this.configuredRileyLinkAddress = getActivity().findViewById(R.id.rls_t1_configured_riley_link_address);
         this.configuredRileyLinkName = getActivity().findViewById(R.id.rls_t1_configured_riley_link_name);
         this.batteryLevel = getActivity().findViewById(R.id.rls_t1_battery_level);
         this.connectionError = getActivity().findViewById(R.id.rls_t1_connection_error);
+        this.connectedDeviceDetails = getActivity().findViewById(R.id.rls_t1_connected_device_details);
         this.deviceType = getActivity().findViewById(R.id.rls_t1_device_type);
-        this.deviceModel = getActivity().findViewById(R.id.rls_t1_device_model);
+        this.configuredDeviceModel = getActivity().findViewById(R.id.rls_t1_configured_device_model);
+        this.connectedDeviceModel = getActivity().findViewById(R.id.rls_t1_connected_device_model);
         this.serialNumber = getActivity().findViewById(R.id.rls_t1_serial_number);
         this.pumpFrequency = getActivity().findViewById(R.id.rls_t1_pump_frequency);
         this.lastUsedFrequency = getActivity().findViewById(R.id.rls_t1_last_used_frequency);
@@ -98,18 +102,24 @@ public class RileyLinkStatusGeneralFragment extends DaggerFragment implements Re
                     Optional.ofNullable(rileyLinkServiceData.versionBLE113).orElse(PLACEHOLDER), Optional.ofNullable(rileyLinkServiceData.versionCC110).orElse(PLACEHOLDER)));
         }
 
-        RileyLinkPumpDevice pumpPlugin = (RileyLinkPumpDevice) activePlugin.getActivePump();
-        RileyLinkPumpInfo pumpInfo = pumpPlugin.getPumpInfo();
-        this.deviceType.setText(rileyLinkServiceData.targetDevice.getResourceId());
-        this.deviceModel.setText(pumpInfo.getConnectedDeviceModel());
-        this.serialNumber.setText(pumpInfo.getConnectedDeviceSerialNumber());
-        this.pumpFrequency.setText(pumpInfo.getPumpFrequency());
+        RileyLinkPumpDevice rileyLinkPumpDevice = (RileyLinkPumpDevice) activePlugin.getActivePump();
+        RileyLinkPumpInfo rileyLinkPumpInfo = rileyLinkPumpDevice.getPumpInfo();
+        this.deviceType.setText(targetDevice.getResourceId());
+        if (targetDevice == RileyLinkTargetDevice.MedtronicPump) {
+            this.connectedDeviceDetails.setVisibility(View.VISIBLE);
+            this.configuredDeviceModel.setText(activePlugin.getActivePump().getPumpDescription().pumpType.getDescription());
+            this.connectedDeviceModel.setText(rileyLinkPumpInfo.getConnectedDeviceModel());
+        } else {
+            this.connectedDeviceDetails.setVisibility(View.GONE);
+        }
+        this.serialNumber.setText(rileyLinkPumpInfo.getConnectedDeviceSerialNumber());
+        this.pumpFrequency.setText(rileyLinkPumpInfo.getPumpFrequency());
 
         if (rileyLinkServiceData.lastGoodFrequency != null) {
             this.lastUsedFrequency.setText(resourceHelper.gs(R.string.rileylink_pump_frequency_value, rileyLinkServiceData.lastGoodFrequency));
         }
 
-        long lastConnectionTimeMillis = pumpPlugin.getLastConnectionTimeMillis();
+        long lastConnectionTimeMillis = rileyLinkPumpDevice.getLastConnectionTimeMillis();
         if (lastConnectionTimeMillis == 0) {
             this.lastDeviceContact.setText(resourceHelper.gs(R.string.riley_link_ble_config_connected_never));
         } else {
