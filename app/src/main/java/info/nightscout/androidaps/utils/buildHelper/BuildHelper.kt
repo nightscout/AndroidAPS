d9ed3e4a9ee40610843c7f31b5873b7c75ca5f00
osodebailar <a.eggen@gmail.com>
2021-01-12 15:26:19 +0100

    bump 2.8.1.1-newUserInterface

--- app/src/main/java/info/nightscout/androidaps/utils/buildHelper/BuildHelper.kt
+++ app/src/main/java/info/nightscout/androidaps/utils/buildHelper/BuildHelper.kt
@@ -18,7 +18,7 @@ class BuildHelper @Inject constructor(private val config: Config) {
         val engineeringModeSemaphore = File(extFilesDir, "engineering__mode")
 
         engineeringMode = engineeringModeSemaphore.exists() && engineeringModeSemaphore.isFile
-        devBranch = BuildConfig.VERSION.contains("-dev") || BuildConfig.VERSION.matches(Regex(".*[a-zA-Z]+.*"))
+        devBranch = BuildConfig.VERSION.contains("-dev")
     }
 
     fun isEngineeringModeOrRelease(): Boolean =
