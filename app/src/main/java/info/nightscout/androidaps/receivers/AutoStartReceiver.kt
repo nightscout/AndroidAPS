7aa689d331bce00812fec2197e31ecc5ea101c8b
Milos Kozak <m.kozak@sysop.cz>
2021-01-20 15:56:46 +0100

    fix AutoStartReceiver

--- app/src/main/java/info/nightscout/androidaps/receivers/AutoStartReceiver.kt
+++ app/src/main/java/info/nightscout/androidaps/receivers/AutoStartReceiver.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.content.Intent
 import dagger.android.DaggerBroadcastReceiver
 import info.nightscout.androidaps.plugins.general.persistentNotification.DummyServiceHelper
+import javax.inject.Inject
 
-class AutoStartReceiver constructor(
-    private val dummyServiceHelper: DummyServiceHelper
-) : DaggerBroadcastReceiver() {
+class AutoStartReceiver : DaggerBroadcastReceiver() {
+
+    @Inject lateinit var dummyServiceHelper: DummyServiceHelper
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
