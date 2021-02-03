32637b97e018490e8b2fc6a6df1979cce6016d9e
Milos Kozak <m.kozak@sysop.cz>
2021-02-03 23:58:57 +0100

    Omnipod -> jetpack

--- omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/ui/wizard/activation/fragment/PodActivationActionFragmentBase.kt
+++ omnipod/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/ui/wizard/activation/fragment/PodActivationActionFragmentBase.kt
@@ -3,22 +3,28 @@ package info.nightscout.androidaps.plugins.pump.omnipod.ui.wizard.activation.fra
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import info.nightscout.androidaps.plugins.pump.omnipod.R
 import info.nightscout.androidaps.plugins.pump.omnipod.driver.definition.ActivationProgress
 import info.nightscout.androidaps.plugins.pump.omnipod.driver.manager.PodStateManager
 import info.nightscout.androidaps.plugins.pump.omnipod.ui.wizard.common.fragment.ActionFragmentBase
 import info.nightscout.androidaps.plugins.pump.omnipod.ui.wizard.deactivation.PodDeactivationWizardActivity
-import kotlinx.android.synthetic.main.omnipod_wizard_action_page_fragment.*
 import javax.inject.Inject
 
 abstract class PodActivationActionFragmentBase : ActionFragmentBase() {
 
-    @Inject
-    protected lateinit var podStateManager: PodStateManager
+    @Inject protected lateinit var podStateManager: PodStateManager
+
+    private lateinit var buttonDeactivatePod: Button
+    private lateinit var buttonRetry: Button
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        omnipod_wizard_button_deactivate_pod.setOnClickListener {
+        buttonDeactivatePod = view.findViewById(R.id.omnipod_wizard_button_deactivate_pod)
+        buttonRetry = view.findViewById(R.id.omnipod_wizard_button_retry)
+
+        buttonDeactivatePod.setOnClickListener {
             activity?.let {
                 startActivity(Intent(it, PodDeactivationWizardActivity::class.java))
                 it.finish()
@@ -28,8 +34,8 @@ abstract class PodActivationActionFragmentBase : ActionFragmentBase() {
 
     override fun onActionFailure() {
         if ((podStateManager.isPodActivationTimeExceeded && podStateManager.activationProgress.isAtLeast(ActivationProgress.PAIRING_COMPLETED)) || podStateManager.isPodFaulted) {
-            omnipod_wizard_button_retry.visibility = View.GONE
-            omnipod_wizard_button_deactivate_pod.visibility = View.VISIBLE
+            buttonRetry.visibility = View.GONE
+            buttonDeactivatePod.visibility = View.VISIBLE
         }
     }
 }
