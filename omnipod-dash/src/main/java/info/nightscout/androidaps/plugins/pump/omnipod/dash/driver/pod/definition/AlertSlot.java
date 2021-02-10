43c3b429129c6e93a0c39a691cc172bf6d1a02dc
Bart Sopers <bart@sopers.nl>
2021-02-10 22:35:44 +0100

    Add SilenceAlertsCommand (untested)

--- omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/pod/definition/AlertSlot.java
+++ omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/pod/definition/AlertSlot.java
@@ -1,11 +1,15 @@
 package info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.pod.definition;
 
 public enum AlertSlot {
+    AUTO_OFF((byte) 0x00),
+    MULTI_COMMAND((byte) 0x01),
     EXPIRATION_IMMINENT((byte) 0x02),
+    USER_SET_EXPIRATION((byte) 0x03),
     LOW_RESERVOIR((byte) 0x04),
-    USER_POD_EXPIRATION((byte) 0x03),
-    LUMP_OF_COAL_AND_EXPIRATION((byte) 0x07),
-    UNKNOWN((byte) 255);
+    SUSPEND_IN_PROGRESS((byte) 0x05),
+    SUSPEND_ENDED((byte) 0x06),
+    EXPIRATION((byte) 0x07),
+    UNKNOWN((byte) 0xff);
 
     private byte value;
 
---
+++ omnipod-dash/src/test/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/pod/command/SilenceAlertsCommandTest.java
@@ -0,0 +1,5 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.pod.command;
+
+// TODO capture silence alerts command
+public class SilenceAlertsCommandTest {
+}
