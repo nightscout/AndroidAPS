86fb53ce3425f536e5bd2dfcf069f47e66cd55e2
Milos Kozak <m.kozak@sysop.cz>
2021-02-15 13:57:01 +0100

    SetupWizard update

--- app/src/main/java/info/nightscout/androidaps/setupwizard/elements/SWEditNumber.kt
+++ app/src/main/java/info/nightscout/androidaps/setupwizard/elements/SWEditNumber.kt
@@ -7,18 +7,12 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import dagger.android.HasAndroidInjector
-import info.nightscout.androidaps.Constants
-import info.nightscout.androidaps.data.Profile
-import info.nightscout.androidaps.interfaces.ProfileFunction
 import info.nightscout.androidaps.setupwizard.SWNumberValidator
-import info.nightscout.androidaps.utils.ui.NumberPicker
 import info.nightscout.androidaps.utils.SafeParse
+import info.nightscout.androidaps.utils.ui.NumberPicker
 import java.text.DecimalFormat
-import javax.inject.Inject
 
-class SWEditNumberWithUnits(injector: HasAndroidInjector, private val init: Double, private val min: Double, private val max: Double) : SWItem(injector, Type.UNIT_NUMBER) {
-
-    @Inject lateinit var profileFunction: ProfileFunction
+class SWEditNumber(injector: HasAndroidInjector, private val init: Double, private val min: Double, private val max: Double) : SWItem(injector, Type.DECIMAL_NUMBER) {
 
     private val validator: SWNumberValidator = SWNumberValidator { value -> value in min..max }
     private var updateDelay = 0
@@ -40,10 +34,9 @@ class SWEditNumberWithUnits(injector: HasAndroidInjector, private val init: Doub
         label?.let { l.setText(it) }
         l.setTypeface(l.typeface, Typeface.BOLD)
         layout.addView(l)
-        var initValue = sp.getDouble(preferenceId, init)
-        initValue = Profile.toCurrentUnits(profileFunction.getUnits(), initValue)
+        val initValue = sp.getDouble(preferenceId, init)
         val numberPicker = NumberPicker(context)
-        if (profileFunction.getUnits() == Constants.MMOL) numberPicker.setParams(initValue, min, max, 0.1, DecimalFormat("0.0"), false, null, watcher) else numberPicker.setParams(initValue, min * 18, max * 18, 1.0, DecimalFormat("0"), false, null, watcher)
+        numberPicker.setParams(initValue, min, max, 0.1, DecimalFormat("0.0"), false, null, watcher)
 
         layout.addView(numberPicker)
         val c = TextView(context)
@@ -54,12 +47,12 @@ class SWEditNumberWithUnits(injector: HasAndroidInjector, private val init: Doub
         super.generateDialog(layout)
     }
 
-    fun preferenceId(preferenceId: Int): SWEditNumberWithUnits {
+    fun preferenceId(preferenceId: Int): SWEditNumber {
         this.preferenceId = preferenceId
         return this
     }
 
-    fun updateDelay(updateDelay: Int): SWEditNumberWithUnits {
+    fun updateDelay(updateDelay: Int): SWEditNumber {
         this.updateDelay = updateDelay
         return this
     }
