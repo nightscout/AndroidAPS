c2c9f391dd2aef94628419bb5a64cc80db84109d
Bart Sopers <bart@sopers.nl>
2021-02-20 03:31:50 +0100

    Add Omnipod Dash overview

--- omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/OmnipodDashPumpPlugin.java
+++ omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/OmnipodDashPumpPlugin.java
@@ -2,24 +2,56 @@ package info.nightscout.androidaps.plugins.pump.omnipod.dash;
 
 import androidx.annotation.Nullable;
 
-import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
+import java.util.Collections;
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import dagger.android.HasAndroidInjector;
 import info.nightscout.androidaps.data.DetailedBolusInfo;
 import info.nightscout.androidaps.data.Profile;
 import info.nightscout.androidaps.data.PumpEnactResult;
+import info.nightscout.androidaps.interfaces.CommandQueueProvider;
+import info.nightscout.androidaps.interfaces.PluginDescription;
+import info.nightscout.androidaps.interfaces.PluginType;
 import info.nightscout.androidaps.interfaces.PumpDescription;
 import info.nightscout.androidaps.interfaces.PumpInterface;
+import info.nightscout.androidaps.interfaces.PumpPluginBase;
+import info.nightscout.androidaps.logging.AAPSLogger;
+import info.nightscout.androidaps.logging.LTag;
 import info.nightscout.androidaps.plugins.common.ManufacturerType;
 import info.nightscout.androidaps.plugins.general.actions.defs.CustomAction;
 import info.nightscout.androidaps.plugins.general.actions.defs.CustomActionType;
 import info.nightscout.androidaps.plugins.pump.common.defs.PumpType;
+import info.nightscout.androidaps.plugins.pump.omnipod.dash.ui.OmnipodDashOverviewFragment;
 import info.nightscout.androidaps.queue.commands.CustomCommand;
 import info.nightscout.androidaps.utils.TimeChangeType;
+import info.nightscout.androidaps.utils.resources.ResourceHelper;
 
-public class OmnipodDashPumpPlugin implements PumpInterface {
+@Singleton
+public class OmnipodDashPumpPlugin extends PumpPluginBase implements PumpInterface {
+    private final AAPSLogger aapsLogger;
+    private final ResourceHelper resourceHelper;
+    private final CommandQueueProvider commandQueue;
+
+    @Inject
+    public OmnipodDashPumpPlugin(HasAndroidInjector injector, AAPSLogger aapsLogger, ResourceHelper resourceHelper, CommandQueueProvider commandQueue) {
+        super(new PluginDescription() //
+                .mainType(PluginType.PUMP) //
+                .fragmentClass(OmnipodDashOverviewFragment.class.getName()) //
+                .pluginIcon(R.drawable.ic_pod_128)
+                .pluginName(R.string.omnipod_dash_name) //
+                .shortName(R.string.omnipod_dash_name_short) //
+                .preferencesId(R.xml.omnipod_dash_preferences) //
+                .description(R.string.omnipod_dash_pump_description), injector, aapsLogger, resourceHelper, commandQueue);
+        this.aapsLogger = aapsLogger;
+        this.resourceHelper = resourceHelper;
+        this.commandQueue = commandQueue;
+    }
+
     @Override public boolean isInitialized() {
         return false;
     }
@@ -64,7 +96,7 @@ public class OmnipodDashPumpPlugin implements PumpInterface {
 
     }
 
-    @NotNull @Override public PumpEnactResult setNewBasalProfile(Profile profile) {
+    @Override public PumpEnactResult setNewBasalProfile(Profile profile) {
         return null;
     }
 
@@ -88,7 +120,7 @@ public class OmnipodDashPumpPlugin implements PumpInterface {
         return 0;
     }
 
-    @NotNull @Override public PumpEnactResult deliverTreatment(DetailedBolusInfo detailedBolusInfo) {
+    @Override public PumpEnactResult deliverTreatment(DetailedBolusInfo detailedBolusInfo) {
         return null;
     }
 
@@ -96,47 +128,47 @@ public class OmnipodDashPumpPlugin implements PumpInterface {
 
     }
 
-    @NotNull @Override public PumpEnactResult setTempBasalAbsolute(Double absoluteRate, Integer durationInMinutes, Profile profile, boolean enforceNew) {
+    @Override public PumpEnactResult setTempBasalAbsolute(Double absoluteRate, Integer durationInMinutes, Profile profile, boolean enforceNew) {
         return null;
     }
 
-    @NotNull @Override public PumpEnactResult setTempBasalPercent(Integer percent, Integer durationInMinutes, Profile profile, boolean enforceNew) {
+    @Override public PumpEnactResult setTempBasalPercent(Integer percent, Integer durationInMinutes, Profile profile, boolean enforceNew) {
         return null;
     }
 
-    @NotNull @Override public PumpEnactResult setExtendedBolus(Double insulin, Integer durationInMinutes) {
+    @Override public PumpEnactResult setExtendedBolus(Double insulin, Integer durationInMinutes) {
         return null;
     }
 
-    @NotNull @Override public PumpEnactResult cancelTempBasal(boolean enforceNew) {
+    @Override public PumpEnactResult cancelTempBasal(boolean enforceNew) {
         return null;
     }
 
-    @NotNull @Override public PumpEnactResult cancelExtendedBolus() {
+    @Override public PumpEnactResult cancelExtendedBolus() {
         return null;
     }
 
-    @NotNull @Override public JSONObject getJSONStatus(Profile profile, String profileName, String version) {
+    @Override public JSONObject getJSONStatus(Profile profile, String profileName, String version) {
         return null;
     }
 
-    @NotNull @Override public ManufacturerType manufacturer() {
+    @Override public ManufacturerType manufacturer() {
+        return getPumpDescription().pumpType.getManufacturer();
+    }
+
+    @Override public PumpType model() {
+        return getPumpDescription().pumpType;
+    }
+
+    @Override public String serialNumber() {
         return null;
     }
 
-    @NotNull @Override public PumpType model() {
-        return null;
+    @Override public PumpDescription getPumpDescription() {
+        return getPumpDescription();
     }
 
-    @NotNull @Override public String serialNumber() {
-        return null;
-    }
-
-    @NotNull @Override public PumpDescription getPumpDescription() {
-        return null;
-    }
-
-    @NotNull @Override public String shortStatus(boolean veryShort) {
+    @Override public String shortStatus(boolean veryShort) {
         return null;
     }
 
@@ -144,7 +176,7 @@ public class OmnipodDashPumpPlugin implements PumpInterface {
         return false;
     }
 
-    @NotNull @Override public PumpEnactResult loadTDDs() {
+    @Override public PumpEnactResult loadTDDs() {
         return null;
     }
 
@@ -152,12 +184,14 @@ public class OmnipodDashPumpPlugin implements PumpInterface {
         return false;
     }
 
-    @Nullable @Override public List<CustomAction> getCustomActions() {
-        return null;
+    @Override
+    public List<CustomAction> getCustomActions() {
+        return Collections.emptyList();
     }
 
-    @Override public void executeCustomAction(CustomActionType customActionType) {
-
+    @Override
+    public void executeCustomAction(CustomActionType customActionType) {
+        aapsLogger.warn(LTag.PUMP, "Unknown custom action: " + customActionType);
     }
 
     @Nullable @Override public PumpEnactResult executeCustomCommand(CustomCommand customCommand) {
--- omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/ui/OmnipodDashOverviewFragment.kt
+++ omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/ui/OmnipodDashOverviewFragment.kt
@@ -5,15 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dagger.android.support.DaggerFragment
+import info.nightscout.androidaps.plugins.pump.omnipod.dash.databinding.OmnipodDashOverviewBinding
 
 class OmnipodDashOverviewFragment : DaggerFragment() {
 
-    private var _binding: OmnipodOverviewBinding? = null
+    private var _binding: OmnipodDashOverviewBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
-        OmnipodOverviewBinding.inflate(inflater, container, false).also { _binding = it }.root
+        OmnipodDashOverviewBinding.inflate(inflater, container!!).also { _binding = it }.root
 }
