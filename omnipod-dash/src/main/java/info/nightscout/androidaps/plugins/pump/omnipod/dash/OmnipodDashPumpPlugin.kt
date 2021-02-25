bc8eefaffdf750f1d93308902f38edf811af48f9
Bart Sopers <bart@sopers.nl>
2021-02-25 23:55:12 +0100

    Convert OmnipodDashPumpPlugin to Kotlin

---
+++ omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/OmnipodDashPumpPlugin.kt
@@ -0,0 +1,206 @@
+package info.nightscout.androidaps.plugins.pump.omnipod.dash
+
+import dagger.android.HasAndroidInjector
+import info.nightscout.androidaps.data.DetailedBolusInfo
+import info.nightscout.androidaps.data.Profile
+import info.nightscout.androidaps.data.PumpEnactResult
+import info.nightscout.androidaps.interfaces.*
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.logging.LTag
+import info.nightscout.androidaps.plugins.common.ManufacturerType
+import info.nightscout.androidaps.plugins.general.actions.defs.CustomAction
+import info.nightscout.androidaps.plugins.general.actions.defs.CustomActionType
+import info.nightscout.androidaps.plugins.pump.common.defs.PumpType
+import info.nightscout.androidaps.plugins.pump.omnipod.dash.ui.OmnipodDashOverviewFragment
+import info.nightscout.androidaps.queue.commands.CustomCommand
+import info.nightscout.androidaps.utils.TimeChangeType
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+import org.json.JSONObject
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class OmnipodDashPumpPlugin @Inject constructor(
+    injector: HasAndroidInjector,
+    aapsLogger: AAPSLogger,
+    resourceHelper: ResourceHelper,
+    commandQueue: CommandQueueProvider)
+    : PumpPluginBase(pluginDescription, injector, aapsLogger, resourceHelper, commandQueue), PumpInterface {
+
+    companion object {
+
+        private val pluginDescription = PluginDescription() //
+            .mainType(PluginType.PUMP) //
+            .fragmentClass(OmnipodDashOverviewFragment::class.java.name) //
+            .pluginIcon(R.drawable.ic_pod_128)
+            .pluginName(R.string.omnipod_dash_name) //
+            .shortName(R.string.omnipod_dash_name_short) //
+            .preferencesId(R.xml.omnipod_dash_preferences) //
+            .description(R.string.omnipod_dash_pump_description)
+
+        private val pumpDescription = PumpDescription(PumpType.Omnipod_Dash)
+    }
+
+    override fun isInitialized(): Boolean {
+        // TODO
+        return true
+    }
+
+    override fun isSuspended(): Boolean {
+        // TODO
+        return false
+    }
+
+    override fun isBusy(): Boolean {
+        // prevents the queue from executing commands
+        // TODO
+        return true
+    }
+
+    override fun isConnected(): Boolean {
+        // TODO
+        return false
+    }
+
+    override fun isConnecting(): Boolean {
+        // TODO
+        return false
+    }
+
+    override fun isHandshakeInProgress(): Boolean {
+        // TODO
+        return false
+    }
+
+    override fun finishHandshaking() {
+        // TODO
+    }
+
+    override fun connect(reason: String) {
+        // TODO
+    }
+
+    override fun disconnect(reason: String) {
+        // TODO
+    }
+
+    override fun stopConnecting() {
+        // TODO
+    }
+
+    override fun getPumpStatus(reason: String) {
+        // TODO
+    }
+
+    override fun setNewBasalProfile(profile: Profile): PumpEnactResult {
+        // TODO
+        return PumpEnactResult(injector).success(true).enacted(true)
+    }
+
+    override fun isThisProfileSet(profile: Profile): Boolean {
+        // TODO
+        return true
+    }
+
+    override fun lastDataTime(): Long {
+        // TODO
+        return System.currentTimeMillis()
+    }
+
+    override val baseBasalRate: Double
+        get() = 0.0 // TODO
+
+    override val reservoirLevel: Double
+        get() = 0.0 // TODO
+
+    override val batteryLevel: Int
+        get() = 0
+
+    override fun deliverTreatment(detailedBolusInfo: DetailedBolusInfo): PumpEnactResult {
+        // TODO
+        return PumpEnactResult(injector).success(false).enacted(false).comment("TODO")
+    }
+
+    override fun stopBolusDelivering() {
+        // TODO
+    }
+
+    override fun setTempBasalAbsolute(absoluteRate: Double, durationInMinutes: Int, profile: Profile, enforceNew: Boolean): PumpEnactResult {
+        // TODO
+        return PumpEnactResult(injector).success(false).enacted(false).comment("TODO")
+    }
+
+    override fun setTempBasalPercent(percent: Int, durationInMinutes: Int, profile: Profile, enforceNew: Boolean): PumpEnactResult {
+        // TODO i18n
+        return PumpEnactResult(injector).success(false).enacted(false).comment("Omnipod Dash driver does not support percentage temp basals")
+    }
+
+    override fun setExtendedBolus(insulin: Double, durationInMinutes: Int): PumpEnactResult {
+        // TODO i18n
+        return PumpEnactResult(injector).success(false).enacted(false).comment("Omnipod Dash driver does not support extended boluses")
+    }
+
+    override fun cancelTempBasal(enforceNew: Boolean): PumpEnactResult {
+        // TODO
+        return PumpEnactResult(injector).success(false).enacted(false).comment("TODO")
+    }
+
+    override fun cancelExtendedBolus(): PumpEnactResult {
+        // TODO i18n
+        return PumpEnactResult(injector).success(false).enacted(false).comment("Omnipod Dash driver does not support extended boluses")
+    }
+
+    override fun getJSONStatus(profile: Profile, profileName: String, version: String): JSONObject {
+        // TODO
+        return JSONObject()
+    }
+
+    override val pumpDescription: PumpDescription = Companion.pumpDescription
+
+    override fun manufacturer(): ManufacturerType {
+        return pumpDescription.pumpType.manufacturer
+    }
+
+    override fun model(): PumpType {
+        return pumpDescription.pumpType
+    }
+
+    override fun serialNumber(): String {
+        // TODO
+        return "TODO"
+    }
+
+    override fun shortStatus(veryShort: Boolean): String {
+        // TODO
+        return "TODO"
+    }
+
+    override val isFakingTempsByExtendedBoluses: Boolean
+        get() = false
+
+    override fun loadTDDs(): PumpEnactResult {
+        // TODO i18n
+        return PumpEnactResult(injector).success(false).enacted(false).comment("Omnipod Dash driver does not support TDD")
+    }
+
+    override fun canHandleDST(): Boolean {
+        return false
+    }
+
+    override fun getCustomActions(): List<CustomAction> {
+        return emptyList()
+    }
+
+    override fun executeCustomAction(customActionType: CustomActionType) {
+        aapsLogger.warn(LTag.PUMP, "Unsupported custom action: $customActionType")
+    }
+
+    override fun executeCustomCommand(customCommand: CustomCommand): PumpEnactResult? {
+        // TODO
+        return null
+    }
+
+    override fun timezoneOrDSTChanged(timeChangeType: TimeChangeType) {
+        // TODO
+    }
+}
