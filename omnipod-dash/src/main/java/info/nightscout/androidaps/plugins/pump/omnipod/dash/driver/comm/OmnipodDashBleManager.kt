a23c3f2cd860d1192359f945a62e5cf3155f7006
Andrei Vereha <avereha@gmail.com>
2021-02-26 13:33:13 +0100

    Define OmnipodDashBleManager interface

--- omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/comm/OmnipodDashBleManager.kt
+++ omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/comm/OmnipodDashBleManager.kt
@@ -1,7 +1,16 @@
 package info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.comm
 
+import info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.comm.status.ConnectionStatus
+import info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.pod.command.base.Command
+import info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.pod.response.Response
+
 interface OmnipodDashBleManager {
 
-    // TODO should we keep this method?
-    fun activateNewPod()
+    fun sendCommand(cmd: Command): Response;
+
+    fun getStatus(): ConnectionStatus;
+
+    fun connect()
+
+    fun disconnect()
 }
