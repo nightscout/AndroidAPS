2c5725717cd817f1555f6ba57a6260ddba3904e9
AdrianLxM <adrian.m@inbox.com>
2021-03-25 03:25:57 +0100

    remove test file

--- database/src/main/java/info/nightscout/androidaps/database/entities/XXXValueWithUnit.kt
+++ database/src/main/java/info/nightscout/androidaps/database/entities/XXXValueWithUnit.kt
@@ -46,7 +46,8 @@ sealed class XXXValueWithUnit {
     }
 }
 
-fun List<XXXValueWithUnit>.toPresentationString(translator: Translator) = concat
+fun List<XXXValueWithUnit>.toPresentationString(translator: Translator) =
+    joinToString(separator = " ") { it.toPresentationString(translator) }
 
 // TODO Move to destination module, then uncomment
 fun XXXValueWithUnit.toPresentationString(translator: Translator) : String = when(this){
@@ -90,49 +91,17 @@ fun XXXValueWithUnit.toPresentationString(translator: Translator) : String = whe
  * [x] new sealed classes
  * [x] use entry type directly, not String
  * [ ] database
- * [ ] generate presentation string
+ * [x] generate presentation string
  * [ ] update fragment
  * [ ] generate csv
  *
  */
 
-// just do develop here
+// just do develop in this file. Remove when done.
 interface Translator {
 
-    fun translate(action: UserEntry.Action): String
     fun translate(units: UserEntry.Units): String
     fun translate(meterType: TherapyEvent.MeterType): String
     fun translate(type: TherapyEvent.Type): String
     fun translate(reason: TemporaryTarget.Reason): String
 }
-
-/*
-var valuesWithUnitString = ""
-var rStringParam = 0
-val separator = "  "
-for(v in current.values) {
-    if (rStringParam >0)
-        rStringParam--
-    else
-        when (v.unit) {
-            UserEntry.Units.Timestamp    -> valuesWithUnitString += dateUtil.dateAndTimeAndSecondsString(v.lValue) + separator
-            UserEntry.Units.TherapyEvent -> valuesWithUnitString += translator.translate(v.sValue) + separator
-            UserEntry.Units.R_String     -> {
-                rStringParam = v.lValue.toInt()
-                when (rStringParam) {   //
-                    0 -> valuesWithUnitString += resourceHelper.gs(v.iValue) + separator
-                    1 -> valuesWithUnitString += resourceHelper.gs(v.iValue, current.values[current.values.indexOf(v)+1].value()) + separator
-                    2 -> valuesWithUnitString += resourceHelper.gs(v.iValue, current.values[current.values.indexOf(v)+1].value(), current.values[current.values.indexOf(v)+2].value()) + separator
-                    3 -> valuesWithUnitString += resourceHelper.gs(v.iValue, current.values[current.values.indexOf(v)+1].value(), current.values[current.values.indexOf(v)+2].value(), current.values[current.values.indexOf(v)+3].value()) + separator
-                    4 -> rStringParam = 0
-                }
-            }
-            UserEntry.Units.Mg_Dl        -> valuesWithUnitString += if (profileFunction.getUnits()==Constants.MGDL) DecimalFormatter.to0Decimal(v.dValue) + translator.translate(UserEntry.Units.Mg_Dl) + separator else DecimalFormatter.to1Decimal(v.dValue/Constants.MMOLL_TO_MGDL) + translator.translate(UserEntry.Units.Mmol_L) + separator
-            UserEntry.Units.Mmol_L       -> valuesWithUnitString += if (profileFunction.getUnits()==Constants.MGDL) DecimalFormatter.to0Decimal(v.dValue*Constants.MMOLL_TO_MGDL) + translator.translate(UserEntry.Units.Mg_Dl) + separator else DecimalFormatter.to1Decimal(v.dValue) + translator.translate(UserEntry.Units.Mmol_L) + separator
-            UserEntry.Units.U_H, UserEntry.Units.U
-                                         -> valuesWithUnitString += DecimalFormatter.to2Decimal(v.dValue) + translator.translate(v.unit) + separator
-            UserEntry.Units.G, UserEntry.Units.M, UserEntry.Units.H, UserEntry.Units.Percent
-                                         -> valuesWithUnitString += v.iValue.toString() + translator.translate(v.unit) + separator
-            else                         -> valuesWithUnitString += if (v.iValue != 0 || v.sValue != "") { v.value().toString() + separator } else ""
-        }
-}*/
