19906eae62ad12e346d5923fd52495aa29c62b90
Philoul <legoupil.jp@free.fr>
2021-05-16 17:45:16 +0200

    CRC to kotlin

--- insight/src/main/java/info/nightscout/androidaps/plugins/pump/insight/utils/crypto/CRC.kt
+++ insight/src/main/java/info/nightscout/androidaps/plugins/pump/insight/utils/crypto/CRC.kt
@@ -1,13 +1,13 @@
-package info.nightscout.androidaps.plugins.pump.insight.utils.crypto;
+package info.nightscout.androidaps.plugins.pump.insight.utils.crypto
 
 /*
  * Based on: http://introcs.cs.princeton.edu/java/61data/CRC16.java
  * Table generated using: http://pycrc.org/
  * Command used: python pycrc.py --width 16 --poly 0x1021 --reflect-in True --reflect-out True --xor-in 0xffff --xor-out=0x0000 --generate TABLE
  */
-class CRC {
+object CRC {
 
-    protected static final int[] TABLE = {
+    @JvmField val TABLE = intArrayOf(
         0x0000, 0x1189, 0x2312, 0x329b, 0x4624, 0x57ad, 0x6536, 0x74bf,
         0x8c48, 0x9dc1, 0xaf5a, 0xbed3, 0xca6c, 0xdbe5, 0xe97e, 0xf8f7,
         0x1081, 0x0108, 0x3393, 0x221a, 0x56a5, 0x472c, 0x75b7, 0x643e,
@@ -40,5 +40,5 @@ class CRC {
         0x6b46, 0x7acf, 0x4854, 0x59dd, 0x2d62, 0x3ceb, 0x0e70, 0x1ff9,
         0xf78f, 0xe606, 0xd49d, 0xc514, 0xb1ab, 0xa022, 0x92b9, 0x8330,
         0x7bc7, 0x6a4e, 0x58d5, 0x495c, 0x3de3, 0x2c6a, 0x1ef1, 0x0f78
-    };
+    )
 }
