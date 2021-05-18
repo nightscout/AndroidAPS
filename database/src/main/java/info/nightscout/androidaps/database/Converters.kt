2251a20094811b2481e8dfc6e5827e67c4462585
Milos Kozak <m.kozak@sysop.cz>
2021-05-18 22:06:52 +0200

    rename

--- database/src/main/java/info/nightscout/androidaps/database/Converters.kt
+++ database/src/main/java/info/nightscout/androidaps/database/Converters.kt
@@ -27,11 +27,11 @@ class Converters {
     fun toSource(source: String?) = source?.let { Sources.fromString(it) }
 
     @TypeConverter
-    fun fromListOfXXXValueWithUnit(values: List<ValueWithUnit>): String = values.map(::ValueWithUnitWrapper)
+    fun fromListOfValueWithUnit(values: List<ValueWithUnit>): String = values.map(::ValueWithUnitWrapper)
         .let(SealedClassHelper.gson::toJson)
 
     @TypeConverter
-    fun toMutableListOfXXXValueWithUnit(string: String): List<ValueWithUnit> = SealedClassHelper.gson
+    fun toMutableListOfValueWithUnit(string: String): List<ValueWithUnit> = SealedClassHelper.gson
         .fromJson<List<ValueWithUnitWrapper>>(string).map { it.wrapped }
 
     private class ValueWithUnitWrapper(val wrapped: ValueWithUnit)
