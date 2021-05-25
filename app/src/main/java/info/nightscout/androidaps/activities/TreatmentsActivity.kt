c50da5beca69821998b7196c7b1da919f3247bc4
osodebailar <a.eggen@gmail.com>
2021-05-25 09:44:29 +0200

    fix merge

--- app/src/main/java/info/nightscout/androidaps/activities/TreatmentsActivity.kt
+++ app/src/main/java/info/nightscout/androidaps/activities/TreatmentsActivity.kt
@@ -67,14 +67,14 @@ class TreatmentsActivity : NoSplashAppCompatActivity() {
     }
 
     private fun setBackgroundColorOnSelected(selected: View) {
-        binding.treatments.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.defaultBackground))
-        binding.extendedBoluses.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.defaultBackground))
-        binding.tempBasals.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.defaultBackground))
-        binding.tempTargets.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.defaultBackground))
-        binding.profileSwitches.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.defaultBackground))
-        binding.careportal.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.defaultBackground))
-        binding.userentry.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.defaultBackground))
-        selected.setBackgroundColor(resourceHelper.getAttributeColor(context, R.attr.tabBgColorSelected))
+        binding.treatments.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.defaultBackground))
+        binding.extendedBoluses.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.defaultBackground))
+        binding.tempBasals.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.defaultBackground))
+        binding.tempTargets.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.defaultBackground))
+        binding.profileSwitches.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.defaultBackground))
+        binding.careportal.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.defaultBackground))
+        binding.userentry.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.defaultBackground))
+        selected.setBackgroundColor(resourceHelper.getAttributeColor( selected.context, R.attr.tabBgColorSelected))
     }
 
 }
