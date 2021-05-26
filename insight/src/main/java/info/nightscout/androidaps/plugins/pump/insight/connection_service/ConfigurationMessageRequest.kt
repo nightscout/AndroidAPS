41115500f139e3326f785cb3d741db89a3e1a6eb
Philoul <legoupil.jp@free.fr>
2021-05-26 22:53:31 +0200

    Convert ConfigurationMessageRequest to Kotlin

--- insight/src/main/java/info/nightscout/androidaps/plugins/pump/insight/connection_service/ConfigurationMessageRequest.kt
+++ insight/src/main/java/info/nightscout/androidaps/plugins/pump/insight/connection_service/ConfigurationMessageRequest.kt
@@ -1,33 +1,22 @@
-package info.nightscout.androidaps.plugins.pump.insight.connection_service;
+package info.nightscout.androidaps.plugins.pump.insight.connection_service
 
-import info.nightscout.androidaps.plugins.pump.insight.app_layer.AppLayerMessage;
-import info.nightscout.androidaps.plugins.pump.insight.app_layer.configuration.CloseConfigurationWriteSessionMessage;
-import info.nightscout.androidaps.plugins.pump.insight.app_layer.configuration.OpenConfigurationWriteSessionMessage;
+import info.nightscout.androidaps.plugins.pump.insight.app_layer.AppLayerMessage
+import info.nightscout.androidaps.plugins.pump.insight.app_layer.configuration.CloseConfigurationWriteSessionMessage
+import info.nightscout.androidaps.plugins.pump.insight.app_layer.configuration.OpenConfigurationWriteSessionMessage
 
-public class ConfigurationMessageRequest<T extends AppLayerMessage> extends MessageRequest<T> {
+class ConfigurationMessageRequest<T : AppLayerMessage?>(request: T, private val openRequest: MessageRequest<OpenConfigurationWriteSessionMessage>, private val closeRequest: MessageRequest<CloseConfigurationWriteSessionMessage>) : MessageRequest<T>(request) {
 
-    private final MessageRequest<OpenConfigurationWriteSessionMessage> openRequest;
-    private final MessageRequest<CloseConfigurationWriteSessionMessage> closeRequest;
-
-    public ConfigurationMessageRequest(T request, MessageRequest<OpenConfigurationWriteSessionMessage> openRequest, MessageRequest<CloseConfigurationWriteSessionMessage> closeRequest) {
-        super(request);
-        this.openRequest = openRequest;
-        this.closeRequest = closeRequest;
+    @Throws(Exception::class) override fun await(): T {
+        openRequest.await()
+        val response = super.await()
+        closeRequest.await()
+        return response
     }
 
-    @Override
-    public T await() throws Exception {
-        openRequest.await();
-        T response = super.await();
-        closeRequest.await();
-        return response;
-    }
-
-    @Override
-    public T await(long timeout) throws Exception {
-        openRequest.await(timeout);
-        T response = super.await(timeout);
-        closeRequest.await(timeout);
-        return response;
+    @Throws(Exception::class) override fun await(timeout: Long): T {
+        openRequest.await(timeout)
+        val response = super.await(timeout)
+        closeRequest.await(timeout)
+        return response
     }
 }
