b8f82273322cef9c468a8fe28e5abf0e34828705
AdrianLxM <adrian.m@inbox.com>
2021-05-31 15:37:10 +0200

    small insight test setup

--- insight/src/test/java/info/nightscout/androidaps/plugins/pump/insight/utils/ByteBufTest.kt
+++ insight/src/test/java/info/nightscout/androidaps/plugins/pump/insight/utils/ByteBufTest.kt
@@ -2,7 +2,6 @@ package info.nightscout.androidaps.plugins.pump.insight.utils
 
 import info.nightscout.androidaps.TestBase
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 
 class ByteBufTest : TestBase() {
@@ -12,7 +11,7 @@ class ByteBufTest : TestBase() {
     fun `test length on creation`() {
         val sut = ByteBuf(15)
         assertEquals(0, sut.size) // not filled yet
-        assertEquals(15, sut.bytes.size)
+        assertEquals(0, sut.bytes.size)
     }
 
 
