fa27d409ce7f06135f978a7ba8791d5e61ac1e4b
AdrianLxM <adrian.m@inbox.com>
2021-07-31 13:25:20 +0200

    refactor build helper

--- app/src/test/java/info/nightscout/androidaps/queue/QueueThreadTest.kt
+++ app/src/test/java/info/nightscout/androidaps/queue/QueueThreadTest.kt
@@ -12,7 +12,6 @@ import info.nightscout.androidaps.interfaces.Constraint
 import info.nightscout.androidaps.interfaces.PumpDescription
 import info.nightscout.androidaps.interfaces.PumpSync
 import info.nightscout.androidaps.plugins.configBuilder.ConstraintChecker
-import info.nightscout.androidaps.plugins.general.maintenance.LoggerUtils
 import info.nightscout.androidaps.plugins.general.maintenance.PrefFileListProvider
 import info.nightscout.androidaps.plugins.pump.virtual.VirtualPumpPlugin
 import info.nightscout.androidaps.queue.commands.Command
