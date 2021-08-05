471f077320e90fdb0c381edbc7d8af3e42a8175b
Andrei Vereha <avereha@gmail.com>
2021-08-05 21:29:21 +0200

    convert to primary constructor

--- omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/comm/scan/DiscoveredInvalidPodException.kt
+++ omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/comm/scan/DiscoveredInvalidPodException.kt
@@ -2,6 +2,5 @@ package info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.comm.scan
 
 import android.os.ParcelUuid
 
-class DiscoveredInvalidPodException : Exception {
-    constructor(message: String, serviceUUIds: List<ParcelUuid?>) : super("$message service UUIDs: $serviceUUIds")
-}
+class DiscoveredInvalidPodException(message: String, serviceUUIds: List<ParcelUuid?>) :
+    Exception("$message service UUIDs: $serviceUUIds")
