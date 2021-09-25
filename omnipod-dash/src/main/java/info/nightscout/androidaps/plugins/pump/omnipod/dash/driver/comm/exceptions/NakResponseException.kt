950c745ab7136a09c86b4644ea9f61a628a9febd
Andrei Vereha <avereha@gmail.com>
2021-09-25 18:51:44 +0200

    cleanup before merging to dev

--- omnipod-dash/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/dash/driver/comm/exceptions/NakResponseException.kt
+++
@@ -1,6 +0,0 @@
-package info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.comm.exceptions
-
-import info.nightscout.androidaps.plugins.pump.omnipod.dash.driver.pod.response.NakResponse
-
-class NakResponseException(val response: NakResponse) :
-    Exception("Received NAK response: ${response.nakErrorType.value} ${response.nakErrorType.name}")
