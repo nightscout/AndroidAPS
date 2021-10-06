bfaa392eb074d92e9db36b07bc70a308c1ca7e31
Milos Kozak <m.kozak@sysop.cz>
2021-10-06 15:00:37 +0200

    fix Nan error

--- core/src/main/java/info/nightscout/androidaps/extensions/TotalDailyDoseExtension.kt
+++ core/src/main/java/info/nightscout/androidaps/extensions/TotalDailyDoseExtension.kt
@@ -9,9 +9,12 @@ import java.util.*
 val TotalDailyDose.total
     get() = if (totalAmount > 0) totalAmount else basalAmount + bolusAmount
 
+val TotalDailyDose.basalPct: Double
+    get() = if (total > 0) basalAmount / total * 100 else 0.0
+
 fun TotalDailyDose.toText(resourceHelper: ResourceHelper, dateUtil: DateUtil, includeCarbs: Boolean): String =
-    if (includeCarbs) resourceHelper.gs(R.string.tddwithcarbsformat, dateUtil.dateStringShort(timestamp), total, bolusAmount, basalAmount, basalAmount / total * 100, carbs)
-    else resourceHelper.gs(R.string.tddformat, dateUtil.dateStringShort(timestamp), total, bolusAmount, basalAmount, basalAmount / total * 100)
+    if (includeCarbs) resourceHelper.gs(R.string.tddwithcarbsformat, dateUtil.dateStringShort(timestamp), total, bolusAmount, basalAmount, basalPct, carbs)
+    else resourceHelper.gs(R.string.tddformat, dateUtil.dateStringShort(timestamp), total, bolusAmount, basalAmount, basalPct)
 
 fun TotalDailyDose.toText(resourceHelper: ResourceHelper, days: Int, includeCarbs: Boolean): String =
     if (includeCarbs) resourceHelper.gs(R.string.tddwithcarbsformat, String.format(Locale.getDefault(), "%d ", days) + resourceHelper.gs(R.string.days), total, bolusAmount, basalAmount, basalAmount / total * 100, carbs)
