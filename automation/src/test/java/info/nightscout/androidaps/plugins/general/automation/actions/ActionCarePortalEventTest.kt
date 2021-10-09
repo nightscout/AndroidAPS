3acd1f8a9c01129be4479a92a5e94f9d812f1e77
Milos Kozak <m.kozak@sysop.cz>
2021-10-09 10:10:26 +0200

    fix tests

--- automation/src/test/java/info/nightscout/androidaps/plugins/general/automation/actions/ActionCarePortalEventTest.kt
+++ automation/src/test/java/info/nightscout/androidaps/plugins/general/automation/actions/ActionCarePortalEventTest.kt
@@ -1,53 +1,23 @@
 package info.nightscout.androidaps.plugins.general.automation.actions
 
-import dagger.android.AndroidInjector
-import dagger.android.HasAndroidInjector
-import info.nightscout.androidaps.TestBase
 import info.nightscout.androidaps.automation.R
-import info.nightscout.androidaps.database.AppRepository
 import info.nightscout.androidaps.database.transactions.InsertIfNewByTimestampTherapyEventTransaction
 import info.nightscout.androidaps.database.transactions.Transaction
 import info.nightscout.androidaps.interfaces.GlucoseUnit
-import info.nightscout.androidaps.interfaces.ProfileFunction
 import info.nightscout.androidaps.plugins.general.automation.elements.InputCarePortalMenu
 import info.nightscout.androidaps.plugins.general.automation.elements.InputDuration
 import info.nightscout.androidaps.plugins.general.automation.elements.InputString
 import info.nightscout.androidaps.queue.Callback
-import info.nightscout.androidaps.utils.DateUtil
-import info.nightscout.androidaps.utils.resources.ResourceHelper
-import info.nightscout.androidaps.utils.sharedPreferences.SP
-import io.reactivex.Completable
 import io.reactivex.Single
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers
-import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.any
-import org.mockito.Mockito.doNothing
 
-class ActionCarePortalEventTest : TestBase() {
-
-    @Mock lateinit var resourceHelper: ResourceHelper
-    @Mock lateinit var repository: AppRepository
-    @Mock lateinit var sp: SP
-    @Mock lateinit var dateUtil: DateUtil
-    @Mock lateinit var profileFunction: ProfileFunction
+class ActionCarePortalEventTest : ActionsTestBase() {
 
     private lateinit var sut: ActionCarePortalEvent
-    var injector: HasAndroidInjector = HasAndroidInjector {
-        AndroidInjector {
-            if (it is ActionCarePortalEvent) {
-                it.resourceHelper = resourceHelper
-                it.repository = repository
-                it.sp = sp
-                it.dateUtil = dateUtil
-                it.profileFunction = profileFunction
-            }
-        }
-    }
 
     @Before
     fun setup() {
@@ -60,8 +30,9 @@ class ActionCarePortalEventTest : TestBase() {
         ).thenReturn("Note : %s")
         `when`(dateUtil.now()).thenReturn(0)
         `when`(profileFunction.getUnits()).thenReturn(GlucoseUnit.MGDL)
-        `when`(repository.runTransaction(anyObject<Transaction<InsertIfNewByTimestampTherapyEventTransaction.TransactionResult>>()))
-            .thenReturn(Completable.fromAction {})
+        `when`(repository.runTransactionForResult(anyObject<Transaction<InsertIfNewByTimestampTherapyEventTransaction.TransactionResult>>()))
+            .thenReturn(Single.just(InsertIfNewByTimestampTherapyEventTransaction.TransactionResult().apply {
+            }))
         sut = ActionCarePortalEvent(injector)
         sut.cpEvent = InputCarePortalMenu(resourceHelper)
         sut.cpEvent.value = InputCarePortalMenu.EventType.NOTE
@@ -80,7 +51,7 @@ class ActionCarePortalEventTest : TestBase() {
     @Test fun iconTest() {
         Assert.assertEquals(R.drawable.ic_cp_note, sut.icon())
     }
-/*
+
     @Test fun doActionTest() {
         sut.doAction(object : Callback() {
             override fun run() {
@@ -88,7 +59,7 @@ class ActionCarePortalEventTest : TestBase() {
             }
         })
     }
-*/
+
     @Test fun hasDialogTest() {
         Assert.assertTrue(sut.hasDialog())
     }
