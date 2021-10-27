c95d1d6cf25693773481331152623497637b01e7
damianou <anapodi84@gmail.com>
2021-10-27 13:19:28 +0300

    glunovo

---
+++ app/src/main/java/info/nightscout/androidaps/plugins/source/GlunovoPluginService.kt
@@ -0,0 +1,12 @@
+package info.nightscout.androidaps.plugins.source
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class GlunovoPluginService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
