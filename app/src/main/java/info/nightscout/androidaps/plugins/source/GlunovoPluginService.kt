5be96e7a52c07d424371c5d4fd6ecf5a3277e416
damianou <anapodi84@gmail.com>
2021-11-04 13:38:15 +0200

    glunovo

--- app/src/main/java/info/nightscout/androidaps/plugins/source/GlunovoPluginService.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/source/GlunovoPluginService.kt
@@ -24,35 +24,25 @@ class GlunovoPluginService : Service() {
     private val mgetValue: Runnable = object : Runnable {
         override fun run() {
             val cr = contentResolver.query(CONTENT_URI, null, null, null, null)
-            val stringbuilder = StringBuilder()
+            val crfirst = contentResolver.query(CONTENT_URI, null, null, null, null)
             cr!!.moveToLast()
+            crfirst!!.moveToFirst()
             var i: Int =1
-            while (i<=90) {
+            while ((i<=90) && (cr != crfirst)) {
                 cr!!.moveToPrevious()
                 i = i + 1
             }
-            var time = cr.getLong(0)
-            var value = cr.getDouble(1) * 18.018 //value in mmol/l... transformed in mg/dl if value *18.018
-            stringbuilder.append("$time   $value\n")
-
+            var time : Long
+            var value : Double
             var intent = Intent()
-            intent.action = "com.glunovoservice.BgEstimate"
-            intent.putExtra("Time", time)
-            intent.putExtra("BgEstimate", value)
-            intent.setPackage("info.nightscout.androidaps")
-            var bundle = Bundle()
-            bundle.putLong("Time",time);
-            bundle.putDouble("BgEstimate",value);
-            //bundle.putDouble("Current",readingCurrent);
-            intent.putExtra("bundle", bundle);
-            sendBroadcast(intent)
+            var bundle :Bundle
+            cr!!.moveToPrevious()
 
             while (i>1)
             {
                 cr!!.moveToNext()
                 time = cr.getLong(0)
                 value = cr.getDouble(1) * 18.018 //value in mmol/l... transformed in mg/dl if value *18.018
-                stringbuilder.append("$time   $value\n")
                 intent = Intent()
                 intent.action = "com.glunovoservice.BgEstimate"
                 intent.putExtra("Time", time)
@@ -61,7 +51,6 @@ class GlunovoPluginService : Service() {
                 bundle = Bundle()
                 bundle.putLong("Time",time);
                 bundle.putDouble("BgEstimate",value);
-                //bundle.putDouble("Current",readingCurrent);
                 intent.putExtra("bundle", bundle);
                 sendBroadcast(intent)
                 i = i - 1
