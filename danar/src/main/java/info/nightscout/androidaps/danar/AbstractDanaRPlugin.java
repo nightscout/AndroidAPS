81d500477f0f883b753c7a6de9ea3456296f2c31
Philoul <legoupil.jp@free.fr>
2021-11-21 00:14:23 +0100

    Add missing string and Fix Crash

--- danar/src/main/java/info/nightscout/androidaps/danar/AbstractDanaRPlugin.java
+++ danar/src/main/java/info/nightscout/androidaps/danar/AbstractDanaRPlugin.java
@@ -62,7 +62,6 @@ public abstract class AbstractDanaRPlugin extends PumpPluginBase implements Pump
     protected DateUtil dateUtil;
     protected AapsSchedulers aapsSchedulers;
     protected PumpSync pumpSync;
-    protected ResourceHelper rh;
 
     protected AbstractDanaRPlugin(
             HasAndroidInjector injector,
@@ -112,7 +111,7 @@ public abstract class AbstractDanaRPlugin extends PumpPluginBase implements Pump
                     if (event.isChanged(getRh(), R.string.key_danar_bt_name)) {
                         danaPump.reset();
                         pumpSync.connectNewPump();
-                        getCommandQueue().readStatus(rh.gs(R.string.device_changed), null);
+                        getCommandQueue().readStatus(getRh().gs(R.string.device_changed), null);
                     }
                 })
         );
