38738018ef712923478443bff9ece92d3912ff0c
Milos Kozak <m.kozak@sysop.cz>
2021-11-23 23:15:51 +0100

    AutosensData -> kt

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewData.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewData.kt
@@ -646,9 +646,9 @@ class OverviewData @Inject constructor(
                     maxCobValueFound = max(maxCobValueFound, cob.toDouble())
                     lastCob = cob
                 }
-                if (autosensData.failoverToMinAbsorbtionRate) {
-                    autosensData.setScale(cobScale)
-                    autosensData.setChartTime(time)
+                if (autosensData.failOverToMinAbsorptionRate) {
+                    autosensData.scale = cobScale
+                    autosensData.chartTime = time
                     minFailOverActiveList.add(autosensData)
                 }
             }
