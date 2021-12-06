9145a11563b53ac2e57ff35865e904ee550a0d85
Philoul <legoupil.jp@free.fr>
2021-12-07 00:02:58 +0100

    Simplify code

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.kt
@@ -557,10 +557,10 @@ class OverviewFragment : DaggerFragment(), View.OnClickListener, OnLongClickList
         val xDripIsBgSource = xdripPlugin.isEnabled()
         val dexcomIsSource = dexcomPlugin.isEnabled()
         binding.buttonsLayout.calibrationButton.visibility = ((xDripIsBgSource || dexcomIsSource) && actualBG != null && sp.getBoolean(R.string.key_show_calibration_button, true)).toVisibility()
-        if (dexcomPlugin.isEnabled()) {
+        if (dexcomIsSource) {
             binding.buttonsLayout.cgmButton.setCompoundDrawablesWithIntrinsicBounds(null, rh.gd(R.drawable.ic_byoda), null, null)
             binding.buttonsLayout.cgmButton.setTextColor(rh.gc(R.color.white))
-        } else if (xdripPlugin.isEnabled()) {
+        } else if (xDripIsBgSource) {
             binding.buttonsLayout.cgmButton.setCompoundDrawablesWithIntrinsicBounds(null, rh.gd(R.drawable.ic_xdrip), null, null)
             binding.buttonsLayout.cgmButton.setTextColor(rh.gc(R.color.colorCalibrationButton))
         }
