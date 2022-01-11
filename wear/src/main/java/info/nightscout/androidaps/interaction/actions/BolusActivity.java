6a846021e03c79fe9eb44f5f0c5c17187e579c03
Andries Smit <mr.andries.smit@gmail.com>
2022-01-11 23:19:20 +0100

    Wear: Add bolus activity for tile

--- wear/src/main/java/info/nightscout/androidaps/interaction/actions/BolusActivity.java
+++ wear/src/main/java/info/nightscout/androidaps/interaction/actions/BolusActivity.java
@@ -14,13 +14,8 @@ import info.nightscout.androidaps.data.ListenerService;
 import info.nightscout.androidaps.interaction.utils.PlusMinusEditText;
 import info.nightscout.shared.SafeParse;
 
-/**
- * Created by adrian on 09/02/17.
- */
+public class BolusActivity extends ViewSelectorActivity {
 
-public class TreatmentActivity extends ViewSelectorActivity {
-
-    PlusMinusEditText editCarbs;
     PlusMinusEditText editInsulin;
 
     @Override
@@ -35,11 +30,10 @@ public class TreatmentActivity extends ViewSelectorActivity {
         finish();
     }
 
-
     private class MyGridViewPagerAdapter extends GridPagerAdapter {
         @Override
         public int getColumnCount(int arg0) {
-            return 3;
+            return 2;
         }
 
         @Override
@@ -61,28 +55,15 @@ public class TreatmentActivity extends ViewSelectorActivity {
                 container.addView(view);
                 view.requestFocus();
                 return view;
-            } else if (col == 1) {
-                final View view = getInflatedPlusMinusView(container);
-                double def = 0;
-                if (editCarbs != null) {
-                    def = SafeParse.stringToDouble(editCarbs.editText.getText().toString());
-                }
-                editCarbs = new PlusMinusEditText(view, R.id.amountfield, R.id.plusbutton, R.id.minusbutton, def, 0d, 150d, 1d, new DecimalFormat("0"), false);
-                setLabelToPlusMinusView(view, getString(R.string.action_carbs));
-                container.addView(view);
-                return view;
             } else {
-
                 final View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.action_send_item, container, false);
                 final ImageView confirmbutton = view.findViewById(R.id.confirmbutton);
                 confirmbutton.setOnClickListener((View v) -> {
-                        //check if it can happen that the fagment is never created that hold data?
-                        // (you have to swipe past them anyways - but still)
-                        String actionstring = "bolus " + SafeParse.stringToDouble(editInsulin.editText.getText().toString())
-                                + " " + SafeParse.stringToInt(editCarbs.editText.getText().toString());
-                        ListenerService.initiateAction(TreatmentActivity.this, actionstring);
-                        confirmAction(TreatmentActivity.this, R.string.action_treatment_confirmation);
-                        finishAffinity();
+                    String actionstring = "bolus " + SafeParse.stringToDouble(editInsulin.editText.getText().toString())
+                            + " 0"; // Zero carbs
+                    ListenerService.initiateAction(BolusActivity.this, actionstring);
+                    confirmAction(BolusActivity.this, R.string.action_bolus_confirmation);
+                    finishAffinity();
                 });
                 container.addView(view);
                 return view;
@@ -91,8 +72,6 @@ public class TreatmentActivity extends ViewSelectorActivity {
 
         @Override
         public void destroyItem(ViewGroup container, int row, int col, Object view) {
-            // Handle this to get the data before the view is destroyed?
-            // Object should still be kept by this, just setup for reinit?
             container.removeView((View) view);
         }
 
