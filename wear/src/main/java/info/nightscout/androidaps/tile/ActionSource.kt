73eb236291cb14ae67380fa34338bb90cf2691f1
Andries Smit <mr.andries.smit@gmail.com>
2022-01-12 12:22:53 +0100

    Wear: tile code cleanup

--- wear/src/main/java/info/nightscout/androidaps/tile/ActionSource.kt
+++ wear/src/main/java/info/nightscout/androidaps/tile/ActionSource.kt
@@ -13,55 +13,45 @@ object ActionSource : TileSource {
         return listOf(
             Action(
                 id = 0,
-                settingName = "wizzard",
+                settingName = "wizard",
                 nameRes = R.string.menu_wizard_short,
                 iconRes = R.drawable.ic_calculator_green,
-                activityClass = WizardActivity::class.java.getName(),
-                background = false,
-                actionString = "",
+                activityClass = WizardActivity::class.java.name,
             ),
             Action(
                 id = 1,
                 settingName = "treatment",
                 nameRes = R.string.menu_treatment_short,
                 iconRes = R.drawable.ic_bolus_carbs,
-                activityClass = TreatmentActivity::class.java.getName(),
-                background = false,
-                actionString = "",
+                activityClass = TreatmentActivity::class.java.name,
             ),
             Action(
                 id = 2,
                 settingName = "bolus",
                 nameRes = R.string.action_insulin,
                 iconRes = R.drawable.ic_bolus,
-                activityClass = BolusActivity::class.java.getName(),
-                background = false,
-                actionString = "",
+                activityClass = BolusActivity::class.java.name,
             ),
             Action(
                 id = 3,
                 settingName = "carbs",
                 nameRes = R.string.action_carbs,
                 iconRes = R.drawable.ic_carbs_orange,
-                activityClass = ECarbActivity::class.java.getName(),
-                background = false,
-                actionString = "",
+                activityClass = ECarbActivity::class.java.name,
             ),
             Action(
                 id = 4,
                 settingName = "temp_target",
                 nameRes = R.string.menu_tempt,
                 iconRes = R.drawable.ic_temptarget_flat,
-                activityClass = TempTargetActivity::class.java.getName(),
-                background = false,
-                actionString = "",
+                activityClass = TempTargetActivity::class.java.name,
             )
         )
     }
 
     override fun getDefaultConfig(): Map<String, String> {
         return mapOf(
-            "tile_action_1" to "wizzard",
+            "tile_action_1" to "wizard",
             "tile_action_2" to "treatment",
             "tile_action_3" to "carbs",
             "tile_action_4" to "temp_target"
