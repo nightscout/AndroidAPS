d04c5df1e3ab20937da6c05d679e6312328e43dd
Andries Smit <mr.andries.smit@gmail.com>
2022-01-13 11:16:26 +0100

    Wear: remove tile debug log

--- wear/src/main/java/info/nightscout/androidaps/interaction/actions/TempTargetActivity.java
+++ wear/src/main/java/info/nightscout/androidaps/interaction/actions/TempTargetActivity.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.wearable.view.GridPagerAdapter;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,7 +47,6 @@ public class TempTargetActivity extends ViewSelectorActivity {
             String actionString = extras.getString("actionString", "");
             boolean inBackground = extras.getBoolean("inBackground", false);
             if (inBackground) {
-                Log.i("ASTAG", "TempTargetActivity.onCreate: " + actionString);
                 ListenerService.initiateAction(this, actionString);
                 confirmAction(this, R.string.action_tempt_confirmation);
                 finishAffinity();
