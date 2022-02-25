bd61e53bc8a6d8e081cc8dc47dc24c62936fc84e
Milos Kozak <m.kozak@sysop.cz>
2022-02-25 09:16:51 +0100

    SMS: fix crash on Android<12

--- core/src/main/java/info/nightscout/androidaps/di/CoreModule.kt
+++ core/src/main/java/info/nightscout/androidaps/di/CoreModule.kt
@@ -21,5 +21,5 @@ open class CoreModule {
     fun provideResources(context: Context, fabricPrivacy: FabricPrivacy): ResourceHelper = ResourceHelperImplementation(context, fabricPrivacy)
 
     @Provides
-    fun smsManager(context: Context) : SmsManager = context.getSystemService(SmsManager::class.java)
+    fun smsManager(context: Context) : SmsManager? = context.getSystemService(SmsManager::class.java)
 }
