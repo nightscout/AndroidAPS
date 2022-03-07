90d787f6f71a670e05f0a55c7fb23ec9b4692770
Dave2526 <36053025+Dave2526@users.noreply.github.com>
2022-03-07 15:18:07 +0100

    Update XdripPlugin.kt

--- app/src/main/java/info/nightscout/androidaps/plugins/source/XdripPlugin.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/source/XdripPlugin.kt
@@ -40,8 +40,10 @@ class XdripPlugin @Inject constructor(
 
     override fun shouldUploadToNs(glucoseValue: GlucoseValue): Boolean  = false
 
+    //override fun advancedFilteringSupported(): Boolean {
+    //    return advancedFiltering
     override fun advancedFilteringSupported(): Boolean {
-        return advancedFiltering
+        return true
     }
 
     private fun detectSource(glucoseValue: GlucoseValue) {
