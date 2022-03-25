1ce8e298dc03017457c5a2f927e7bee25799c25c
Andries Smit <mr.andries.smit@gmail.com>
2022-03-25 09:52:03 +0100

    fix: bg color dialogs

--- core/src/main/java/info/nightscout/androidaps/utils/alertDialogs/AlertDialogHelper.kt
+++ core/src/main/java/info/nightscout/androidaps/utils/alertDialogs/AlertDialogHelper.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.annotation.LayoutRes
 import androidx.annotation.StyleRes
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.ContextThemeWrapper
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import info.nightscout.androidaps.core.R
@@ -22,7 +21,7 @@ object AlertDialogHelper {
     fun buildCustomTitle(context: Context, title: String,
                          @DrawableRes iconResource: Int = R.drawable.ic_check_while_48dp,
                          @StyleRes themeResId: Int = R.style.AppTheme,
-                         @LayoutRes layoutResource: Int = R.layout.dialog_alert_custom): View? {
+                         @LayoutRes layoutResource: Int = R.layout.dialog_alert_custom_title): View? {
         val titleLayout = LayoutInflater.from(ContextThemeWrapper(context, themeResId)).inflate(layoutResource, null)
         (titleLayout.findViewById<View>(R.id.alertdialog_title) as TextView).text = title
         (titleLayout.findViewById<View>(R.id.alertdialog_icon) as ImageView).setImageResource(iconResource)
