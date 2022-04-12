0d50220784eb609eb03574f7da35d75e7e5c0d0d
Philoul <legoupil.jp@free.fr>
2022-04-12 19:52:53 +0200

    Update Autotune interface

--- core/src/main/java/info/nightscout/androidaps/interfaces/Autotune.kt
+++ core/src/main/java/info/nightscout/androidaps/interfaces/Autotune.kt
@@ -9,13 +9,14 @@ interface Autotune {
     fun updateProfile(newProfile: ATProfile?)
     fun atLog(message: String)
 
-    var pumpProfile: ATProfile?
+    var pumpProfile: ATProfile
     var tunedProfile: ATProfile?
     var result: String
     var selectedProfile: String
     var calculationRunning: Boolean
     var lastRun: Long
     var lastNbDays: String
+    var compareButtonVisibility: Int
     var copyButtonVisibility: Int
     var updateButtonVisibility: Int
     var profileSwitchButtonVisibility: Int
