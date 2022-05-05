4473418edb2519926393965fc26cde0f07f1a383
Philoul <legoupil.jp@free.fr>
2022-05-06 00:10:10 +0200

    Add dev and engineeering mode in Automation

--- automation/src/main/java/info/nightscout/androidaps/plugins/general/automation/actions/ActionRunAutotune.kt
+++ automation/src/main/java/info/nightscout/androidaps/plugins/general/automation/actions/ActionRunAutotune.kt
@@ -40,18 +40,21 @@ class ActionRunAutotune(injector: HasAndroidInjector) : Action(injector) {
     @DrawableRes override fun icon(): Int = R.drawable.ic_actions_profileswitch
 
     override fun doAction(callback: Callback) {
-        val autoSwitch = sp.getBoolean(R.string.key_autotune_auto, false)
+        val autoSwitch = sp.getBoolean(R.string.key_autotune_auto, false) && buildHelper.isEngineeringMode()
         val profileName = if (inputProfileName.value == rh.gs(R.string.active)) "" else inputProfileName.value
         var message = if (autoSwitch) R.string.autotune_run_with_autoswitch else R.string.autotune_run_without_autoswitch
         Thread {
+            if (buildHelper.isDev()) {
                 autotunePlugin.atLog("[Automation] Run Autotune $profileName, ${daysBack.value} days, Autoswitch $autoSwitch")
                 autotunePlugin.aapsAutotune(daysBack.value, autoSwitch, profileName)
                 if (!autotunePlugin.lastRunSuccess) {
                     message = R.string.autotune_run_with_error
                     aapsLogger.error(LTag.AUTOMATION, "Error during Autotune Run")
                 }
-                callback.result(PumpEnactResult(injector).success(autotunePlugin.lastRunSuccess).comment(message))?.run()
-            }.start()
+            } else
+                message = R.string.autotune_dev_warning
+            callback.result(PumpEnactResult(injector).success(autotunePlugin.lastRunSuccess).comment(message))?.run()
+        }.start()
         return
     }
 
