329bc1e1c6af9a64b4fb101419745d89e04fe547
Tim Street <street.tj@gmail.com>
2022-06-06 09:18:40 +0100

    Version increment updated following bug fixes.

--- app/src/main/assets/OpenAPSSMBDynamicISF/determine-basal.js
+++ app/src/main/assets/OpenAPSSMBDynamicISF/determine-basal.js
@@ -218,7 +218,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         //*********************************************************************************
 
             console.error("---------------------------------------------------------");
-            console.error( " Dynamic ISF version Beta 1.6.2 ");
+            console.error( " Dynamic ISF version Beta 1.6.3 ");
             console.error("---------------------------------------------------------");
 
 
