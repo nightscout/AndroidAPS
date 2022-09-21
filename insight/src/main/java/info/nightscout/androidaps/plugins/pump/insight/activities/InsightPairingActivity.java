ad152120ec9ae81b32b68069abf2ec77d643dc10
Milos Kozak <m.kozak@sysop.cz>
2022-09-21 14:24:20 +0200

    BluetoothAdapter safeEnable,safeDisable

--- insight/src/main/java/info/nightscout/androidaps/plugins/pump/insight/activities/InsightPairingActivity.java
+++ insight/src/main/java/info/nightscout/androidaps/plugins/pump/insight/activities/InsightPairingActivity.java
@@ -1,5 +1,6 @@
 package info.nightscout.androidaps.plugins.pump.insight.activities;
 
+import info.nightscout.androidaps.extensions.BluetoothAdapterExtensionKt;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -33,13 +34,13 @@ import java.util.List;
 import javax.inject.Inject;
 
 import info.nightscout.androidaps.activities.NoSplashAppCompatActivity;
+import info.nightscout.androidaps.extensions.IntentExtensionKt;
 import info.nightscout.androidaps.insight.R;
 import info.nightscout.androidaps.interfaces.PumpSync;
 import info.nightscout.androidaps.plugins.pump.common.ble.BlePreCheck;
 import info.nightscout.androidaps.plugins.pump.insight.connection_service.InsightConnectionService;
 import info.nightscout.androidaps.plugins.pump.insight.descriptors.InsightState;
 import info.nightscout.androidaps.plugins.pump.insight.utils.ExceptionTranslator;
-import info.nightscout.androidaps.extensions.IntentExtensionKt;
 
 public class InsightPairingActivity extends NoSplashAppCompatActivity implements InsightConnectionService.StateCallback, View.OnClickListener, InsightConnectionService.ExceptionCallback {
 
@@ -190,7 +191,7 @@ public class InsightPairingActivity extends NoSplashAppCompatActivity implements
         if (!scanning) {
             BluetoothAdapter bluetoothAdapter = ((BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
             if (bluetoothAdapter != null) {
-                if (!bluetoothAdapter.isEnabled()) bluetoothAdapter.enable();
+                BluetoothAdapterExtensionKt.safeEnable(bluetoothAdapter, 0, null);
                 IntentFilter intentFilter = new IntentFilter();
                 intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
                 intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
