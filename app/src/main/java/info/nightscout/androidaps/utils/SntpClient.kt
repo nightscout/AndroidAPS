648985a11c2dcec0feb8f079e2fbca8854dceb8c
Jianwei Li <lijianwei0320@gmail.com>
2022-10-19 14:27:23 +0800

    Adapt to GFW

--- app/src/main/java/info/nightscout/androidaps/utils/SntpClient.kt
+++ app/src/main/java/info/nightscout/androidaps/utils/SntpClient.kt
@@ -101,7 +101,7 @@ class SntpClient @Inject constructor(
 
     fun doNtpTime(callback: Callback) {
         aapsLogger.debug("Time detection started")
-        callback.success = requestTime("time.google.com", 5000)
+        callback.success = requestTime("ntp1.aliyun.com", 5000)
         callback.time = ntpTime + SystemClock.elapsedRealtime() - ntpTimeReference
         aapsLogger.debug("Time detection ended: " + callback.success + " " + dateUtil.dateAndTimeString(ntpTime))
         callback.run()
