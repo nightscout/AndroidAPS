d60282e0d01f52cafaad2ab34c65b322ad8763f5
Selcuk Kekec <khskekec@gmail.com>
2022-11-03 21:30:57 +0100

    Dexcom G7 integration for dev

--- core/src/main/java/info/nightscout/androidaps/receivers/Intents.kt
+++ core/src/main/java/info/nightscout/androidaps/receivers/Intents.kt
@@ -27,6 +27,7 @@ interface Intents {
         const val ACTION_REMOTE_CALIBRATION = "com.eveningoutpost.dexdrip.NewCalibration"
         const val GLIMP_BG = "it.ct.glicemia.ACTION_GLUCOSE_MEASURED"
         const val DEXCOM_BG = "com.dexcom.cgm.EXTERNAL_BROADCAST"
+        const val DEXCOM_G7_BG = "com.dexcom.g7.EXTERNAL_BROADCAST"
         const val EVERSENSE_BG = "com.senseonics.AndroidAPSEventSubscriber.BROADCAST"
         const val POCTECH_BG = "com.china.poctech.data"
         const val TOMATO_BG = "com.fanqies.tomatofn.BgEstimate"
