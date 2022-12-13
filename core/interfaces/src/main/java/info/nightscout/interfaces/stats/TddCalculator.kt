0e1caa3c9c9fad5fd6adb03da620cdbcfe981d09
Milos Kozak <m.kozak@sysop.cz>
2022-12-13 12:51:32 +0100

    DynISF: resolve situation where TDD is not available

--- core/interfaces/src/main/java/info/nightscout/interfaces/stats/TddCalculator.kt
+++ core/interfaces/src/main/java/info/nightscout/interfaces/stats/TddCalculator.kt
@@ -7,10 +7,10 @@ import info.nightscout.database.entities.TotalDailyDose
 
 interface TddCalculator {
 
-    fun calculate(days: Long): LongSparseArray<TotalDailyDose>
-    fun calculateToday(): TotalDailyDose
-    fun calculateDaily(startHours: Long, endHours: Long): TotalDailyDose
-    fun calculate(startTime: Long, endTime: Long): TotalDailyDose
-    fun averageTDD(tdds: LongSparseArray<TotalDailyDose>): TotalDailyDose?
+    fun calculate(days: Long, allowMissingDays: Boolean): LongSparseArray<TotalDailyDose>?
+    fun calculateToday(): TotalDailyDose?
+    fun calculateDaily(startHours: Long, endHours: Long): TotalDailyDose?
+    fun calculate(startTime: Long, endTime: Long, allowMissingData: Boolean): TotalDailyDose?
+    fun averageTDD(tdds: LongSparseArray<TotalDailyDose>?): TotalDailyDose?
     fun stats(context: Context): TableLayout
 }
