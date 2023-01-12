4ac5305f7aa8f52e010aea1b3282b11bf655aed1
Milos Kozak <m.kozak@sysop.cz>
2023-01-12 10:59:50 +0100

    enable NSClient v3 for public testing

--- app/src/main/java/info/nightscout/androidaps/di/PluginsListModule.kt
+++ app/src/main/java/info/nightscout/androidaps/di/PluginsListModule.kt
@@ -324,7 +324,7 @@ abstract class PluginsListModule {
     abstract fun bindTidepoolPlugin(plugin: TidepoolPlugin): PluginBase
 
     @Binds
-    @Unfinished
+    @AllConfigs
     @IntoMap
     @IntKey(362)
     abstract fun bindNSClientV3Plugin(plugin: NSClientV3Plugin): PluginBase
