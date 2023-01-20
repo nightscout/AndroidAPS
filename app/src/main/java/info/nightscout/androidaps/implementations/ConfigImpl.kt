aa9684f621dba47691d3ea8c10877756ce9b245b
Milos Kozak <m.kozak@sysop.cz>
2023-01-20 17:06:38 +0100

    set min supported NS to 15.0.0

--- app/src/main/java/info/nightscout/androidaps/implementations/ConfigImpl.kt
+++ app/src/main/java/info/nightscout/androidaps/implementations/ConfigImpl.kt
@@ -15,7 +15,7 @@ class ConfigImpl @Inject constructor(
     fileListProvider: PrefFileListProvider
 ) : Config {
 
-    override val SUPPORTED_NS_VERSION = 140206 // 14.2.6
+    override val SUPPORTED_NS_VERSION = 150000 // 15.0.0
     override val APS = BuildConfig.FLAVOR == "full"
     override val NSCLIENT = BuildConfig.FLAVOR == "aapsclient" || BuildConfig.FLAVOR == "aapsclient2"
     override val PUMPCONTROL = BuildConfig.FLAVOR == "pumpcontrol"
