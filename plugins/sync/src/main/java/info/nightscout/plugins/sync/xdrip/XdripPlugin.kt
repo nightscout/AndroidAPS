8ce12d61c0c9074a4bb8264d907c708d7adae630
Milos Kozak <m.kozak@sysop.cz>
2023-03-22 22:09:43 +0100

    Xdrip: prevent NPE

--- plugins/sync/src/main/java/info/nightscout/plugins/sync/xdrip/XdripPlugin.kt
+++ plugins/sync/src/main/java/info/nightscout/plugins/sync/xdrip/XdripPlugin.kt
@@ -362,10 +362,8 @@ class XdripPlugin @Inject constructor(
 
     private fun sendEntries(dataPairs: List<DataSyncSelector.DataPair>, progress: String) {
         val array = JSONArray()
-        for (dataPair in dataPairs) {
-            val data = (dataPair as DataSyncSelector.PairGlucoseValue).value.toXdripJson()
-            array.put(data)
-        }
+        for (dataPair in dataPairs)
+            (dataPair as DataSyncSelector.PairGlucoseValue?)?.value?.toXdripJson()?.also { gv -> array.put(gv) }
         rxBus.send(EventXdripNewLog("SENDING", "Sent ${array.length()} BGs ($progress)"))
         broadcast(
             Intent(Intents.ACTION_NEW_SGV)
