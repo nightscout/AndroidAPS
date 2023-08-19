67d5b2a7eede07629442e78eac3848866d0e8983
Milos Kozak <m.kozak@sysop.cz>
2023-08-19 10:19:21 +0200

    LogSettingActivity: toolbar

--- plugins/configuration/src/main/java/info/nightscout/configuration/maintenance/activities/LogSettingActivity.kt
+++ plugins/configuration/src/main/java/info/nightscout/configuration/maintenance/activities/LogSettingActivity.kt
@@ -1,20 +1,26 @@
 package info.nightscout.configuration.maintenance.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.CheckBox
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.MenuProvider
 import info.nightscout.configuration.R
 import info.nightscout.configuration.databinding.ActivityLogsettingBinding
 import info.nightscout.core.ui.activities.TranslatedDaggerAppCompatActivity
 import info.nightscout.rx.interfaces.L
 import info.nightscout.rx.interfaces.LogElement
+import info.nightscout.shared.interfaces.ResourceHelper
 import javax.inject.Inject
 
 class LogSettingActivity : TranslatedDaggerAppCompatActivity() {
 
     @Inject lateinit var l: L
+    @Inject lateinit var rh: ResourceHelper
 
     private lateinit var binding: ActivityLogsettingBinding
 
@@ -23,13 +29,30 @@ class LogSettingActivity : TranslatedDaggerAppCompatActivity() {
         binding = ActivityLogsettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        title = rh.gs(R.string.nav_logsettings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         createViewsForSettings()
 
         binding.reset.setOnClickListener {
             l.resetToDefaults()
             createViewsForSettings()
         }
-        binding.ok.setOnClickListener { finish() }
+        // Add menu items without overriding methods in the Activity
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {}
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+                when (menuItem.itemId) {
+                    android.R.id.home -> {
+                        onBackPressedDispatcher.onBackPressed()
+                        true
+                    }
+
+                    else              -> false
+                }
+        })
     }
 
     private fun createViewsForSettings() {
