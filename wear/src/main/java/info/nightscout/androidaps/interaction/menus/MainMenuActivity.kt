d9bbb830b31584fba3496998f4e593b864c922e7
Philoul <legoupil.jp@free.fr>
2023-09-06 23:35:04 +0200

    Wear Add preferenceMenuActivity

--- wear/src/main/java/info/nightscout/androidaps/interaction/menus/MainMenuActivity.kt
+++ wear/src/main/java/info/nightscout/androidaps/interaction/menus/MainMenuActivity.kt
@@ -42,7 +42,7 @@ class MainMenuActivity : MenuListActivity() {
 
     override fun doAction(position: String) {
         when (position) {
-            getString(R.string.menu_settings)         -> startActivity(Intent(this, WatchfaceConfigurationActivity::class.java).apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) })
+            getString(R.string.menu_settings)         -> startActivity(Intent(this, PreferenceMenuActivity::class.java).apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) })
             getString(R.string.menu_resync)           -> rxBus.send(EventWearToMobile(ActionResendData("Re-Sync")))
             getString(R.string.status_profile_switch) -> rxBus.send(EventWearToMobile(EventData.ActionProfileSwitchSendInitialData(System.currentTimeMillis())))
             getString(R.string.menu_tempt)            -> startActivity(Intent(this, TempTargetActivity::class.java).apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) })
