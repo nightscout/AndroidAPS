a46e396815e45804affb4c3191efa7f4d391ed1c
Ryan Haining <haining.cpp@gmail.com>
2023-09-20 22:16:25 -0700

    Rewrites MealDataTest with matchers
    Issue #2745

--- core/main/src/test/java/info/nightscout/core/data/MealDataTest.kt
+++ core/main/src/test/java/info/nightscout/core/data/MealDataTest.kt
@@ -1,13 +1,13 @@
 package info.nightscout.core.data
 
+import com.google.common.truth.Truth.assertThat
 import info.nightscout.interfaces.iob.MealData
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class MealDataTest {
 
     @Test fun canCreateObject() {
         val md = MealData()
-        Assertions.assertEquals(0.0, md.carbs, 0.01)
+        assertThat(md.carbs).isWithin(0.01).of(0.0)
     }
 }
