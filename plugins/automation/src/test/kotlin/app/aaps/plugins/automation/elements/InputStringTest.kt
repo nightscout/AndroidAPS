214dae1b2482b9b61b8e49af90a659d3f757b6a5
Ryan Haining <haining.cpp@gmail.com>
2023-10-04 20:52:51 -0700

    Rewrites InputStringTest with matchers
    Issue #2745

--- plugins/automation/src/test/kotlin/app/aaps/plugins/automation/elements/InputStringTest.kt
+++ plugins/automation/src/test/kotlin/app/aaps/plugins/automation/elements/InputStringTest.kt
@@ -1,7 +1,7 @@
 package app.aaps.plugins.automation.elements
 
 import app.aaps.plugins.automation.triggers.TriggerTestBase
-import org.junit.jupiter.api.Assertions
+import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
 class InputStringTest : TriggerTestBase() {
@@ -9,6 +9,6 @@ class InputStringTest : TriggerTestBase() {
     @Test fun setValueTest() {
         val i = InputString()
         i.value = "asd"
-        Assertions.assertEquals("asd", i.value)
+        assertThat(i.value).isEqualTo("asd")
     }
 }
