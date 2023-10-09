499a3cc3102212077ba705b39fa34e3d69e4437b
Urban Kravos <urban.kravos@gmail.com>
2023-10-09 18:53:42 +0200

    QuickWizard Edit Dialog Refactor draft v3 Percentage set with SeekBar. Only 30 option(every 5%) to select from 0 to 150%.

--- ui/src/main/kotlin/app/aaps/ui/activities/QuickWizardListActivity.kt
+++ ui/src/main/kotlin/app/aaps/ui/activities/QuickWizardListActivity.kt
@@ -74,7 +74,12 @@ class QuickWizardListActivity : TranslatedDaggerAppCompatActivity(), OnStartDrag
             holder.binding.from.text = dateUtil.timeString(entry.validFromDate())
             holder.binding.to.text = dateUtil.timeString(entry.validToDate())
             holder.binding.buttonText.text = entry.buttonText()
-            holder.binding.carbs.text = rh.gs(app.aaps.core.main.R.string.format_carbs, entry.carbs())
+            var bindingCarbsTextFull = rh.gs(app.aaps.core.main.R.string.format_carbs, entry.carbs())
+            if (entry.useEcarbs() == QuickWizardEntry.YES) {
+                bindingCarbsTextFull += " +" + rh.gs(app.aaps.core.main.R.string.format_carbs, entry.carbs2())
+                bindingCarbsTextFull += "/" + entry.duration() + "h -> " + entry.time() + "min"
+            }
+            holder.binding.carbs.text = bindingCarbsTextFull
             if (entry.device() == QuickWizardEntry.DEVICE_ALL) {
                 holder.binding.device.visibility = View.GONE
             } else {
