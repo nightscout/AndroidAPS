d242dd31296545f81996420f0b8bb6fa43fb4a2e
olorinmaia <alfeinarj@gmail.com>
2023-10-11 17:37:36 +0200

    Screen forced ON while activating or deactivating Omnipod Dash

--- pump/omnipod-common/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/common/ui/wizard/deactivation/PodDeactivationWizardActivity.kt
+++ pump/omnipod-common/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/common/ui/wizard/deactivation/PodDeactivationWizardActivity.kt
@@ -1,6 +1,7 @@
 package info.nightscout.androidaps.plugins.pump.omnipod.common.ui.wizard.deactivation
 
 import android.os.Bundle
+import android.view.WindowManager
 import info.nightscout.androidaps.plugins.pump.omnipod.common.R
 import info.nightscout.androidaps.plugins.pump.omnipod.common.ui.wizard.common.activity.OmnipodWizardActivityBase
 
@@ -8,6 +9,7 @@ abstract class PodDeactivationWizardActivity : OmnipodWizardActivityBase() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         setContentView(R.layout.omnipod_common_pod_deactivation_wizard_activity)
 
