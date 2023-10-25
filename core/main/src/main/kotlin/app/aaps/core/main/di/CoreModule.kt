0dfa133fa5f792e2210dfbdc81b22622052f4b25
Milos Kozak <m.kozak@sysop.cz>
2023-10-25 17:25:45 +0200

    simplify :core:main

--- core/main/src/main/kotlin/app/aaps/core/main/di/CoreModule.kt
+++ core/main/src/main/kotlin/app/aaps/core/main/di/CoreModule.kt
@@ -3,16 +3,25 @@ package app.aaps.core.main.di
 import android.content.Context
 import android.os.Build
 import android.telephony.SmsManager
+import app.aaps.core.main.wizard.BolusWizard
+import app.aaps.core.main.wizard.QuickWizardEntry
 import dagger.Module
 import dagger.Provides
+import dagger.android.ContributesAndroidInjector
 
 @Module(
     includes = [
-        CoreDataClassesModule::class,
-        PreferencesModule::class
+        CoreModule.CoreDataClassesModule::class,
     ]
 )
 open class CoreModule {
+    @Module
+    abstract class CoreDataClassesModule {
+
+        @ContributesAndroidInjector abstract fun bolusWizardInjector(): BolusWizard
+        @ContributesAndroidInjector abstract fun quickWizardEntryInjector(): QuickWizardEntry
+    }
+
 
     @Suppress("DEPRECATION")
     @Provides
