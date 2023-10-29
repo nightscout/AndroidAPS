d9d667a17f0f966eb0444e328b0fb38f25d2ed0a
Philoul <legoupil.jp@free.fr>
2023-10-29 09:44:17 +0100

    Insight Fix InsightFragment Crash

--- pump/insight/src/main/kotlin/app/aaps/pump/insight/InsightFragment.kt
+++ pump/insight/src/main/kotlin/app/aaps/pump/insight/InsightFragment.kt
@@ -16,6 +16,7 @@ import app.aaps.core.interfaces.rx.bus.RxBus
 import app.aaps.core.interfaces.utils.DateUtil
 import app.aaps.core.interfaces.utils.DecimalFormatter
 import app.aaps.core.interfaces.utils.fabric.FabricPrivacy
+import app.aaps.core.ui.extensions.toVisibility
 import app.aaps.pump.insight.descriptors.*
 import app.aaps.pump.insight.events.EventLocalInsightUpdateGUI
 import dagger.android.support.DaggerFragment
@@ -24,6 +25,7 @@ import info.nightscout.androidaps.insight.databinding.LocalInsightFragmentBindin
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import javax.inject.Inject
 
+@SuppressLint("SetTextI18n")
 class InsightFragment : DaggerFragment(), View.OnClickListener {
 
     @Inject lateinit var insightPlugin: InsightPlugin
@@ -49,7 +51,7 @@ class InsightFragment : DaggerFragment(), View.OnClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = LocalInsightFragmentBinding.inflate(inflater, container, false)
         binding.tbrOverNotification.setOnClickListener(this)
-        binding.operatingMode.setOnClickListener(this)
+        binding.operatingModeButton.setOnClickListener(this)
         binding.refresh.setOnClickListener(this)
         return binding.root
     }
@@ -80,9 +82,9 @@ class InsightFragment : DaggerFragment(), View.OnClickListener {
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.operating_mode        -> {
+            R.id.operating_mode_button -> {
                 if (insightPlugin.operatingMode != null) {
-                    binding.operatingMode.isEnabled = false
+                    binding.operatingModeButton.isEnabled = false
                     operatingModeCallback = object : Callback() {
                         override fun run() {
                             Handler(Looper.getMainLooper()).post {
@@ -134,7 +136,7 @@ class InsightFragment : DaggerFragment(), View.OnClickListener {
         _binding ?: return
         binding.statusItemContainer.removeAllViews()
         if (!insightPlugin.isInitialized()) {
-            binding.operatingMode.visibility = View.GONE
+            binding.operatingModeButton.visibility = View.GONE
             binding.tbrOverNotification.visibility = View.GONE
             binding.refresh.visibility = View.GONE
             return
@@ -145,31 +147,19 @@ class InsightFragment : DaggerFragment(), View.OnClickListener {
         binding.tbrOverNotification.visibility = if (notificationBlock == null) View.GONE else View.VISIBLE
         if (notificationBlock != null) binding.tbrOverNotification.setText(if (notificationBlock.isEnabled) R.string.disable_tbr_over_notification else R.string.enable_tbr_over_notification)
         binding.tbrOverNotification.isEnabled = tbrOverNotificationCallback == null
-        val statusItems: MutableList<View> = ArrayList()
-        getConnectionStatusItem(statusItems)
-        getLastConnectedItem(statusItems)
-        getOperatingModeItem(statusItems)
-        getBatteryStatusItem(statusItems)
-        getCartridgeStatusItem(statusItems)
-        getTDDItems(statusItems)
-        getBaseBasalRateItem(statusItems)
-        getTBRItem(statusItems)
-        getLastBolusItem(statusItems)
-        getBolusItems(statusItems)
-        for (i in statusItems.indices) {
-            binding.statusItemContainer.addView(statusItems[i])
-            if (i != statusItems.size - 1) layoutInflater.inflate(R.layout.local_insight_status_delimitter, binding.statusItemContainer)
-        }
+        getConnectionStatusItem()
+        getLastConnectedItem()
+        getOperatingModeItem()
+        getBatteryStatusItem()
+        getCartridgeStatusItem()
+        getTDDItems()
+        getBaseBasalRateItem()
+        getTBRItem()
+        getLastBolusItem()
+        getBolusItems()
     }
 
-    private fun getStatusItem(label: String, value: String): View {
-        @SuppressLint("InflateParams") val statusItem = layoutInflater.inflate(R.layout.local_insight_status_item, null)
-        (statusItem.findViewById<View>(R.id.label) as TextView).text = label
-        (statusItem.findViewById<View>(R.id.value) as TextView).text = value
-        return statusItem
-    }
-
-    private fun getConnectionStatusItem(statusItems: MutableList<View>) {
+    private fun getConnectionStatusItem() {
         insightPlugin.connectionService?.let {
             val string = when (it.state) {
                 InsightState.NOT_PAIRED                 -> R.string.not_paired
@@ -191,17 +181,22 @@ class InsightFragment : DaggerFragment(), View.OnClickListener {
                 InsightState.CONNECTED                  -> app.aaps.core.interfaces.R.string.connected
                 InsightState.RECOVERING                 -> R.string.recovering
             }
-            statusItems.add(getStatusItem(rh.gs(R.string.insight_status), rh.gs(string)))
+            binding.statusLine.visibility = View.VISIBLE
+            binding.status.text = rh.gs(string)
+            binding.recoveryDurationLine.visibility = (it.state == InsightState.RECOVERING).toVisibility()
             if (it.state == InsightState.RECOVERING) {
-                statusItems.add(getStatusItem(rh.gs(R.string.recovery_duration), (it.recoveryDuration / 1000).toString() + "s"))
+                binding.recoveryDuration.text = "${(it.recoveryDuration / 1000)}s"           // todo replace by second short string
             }
         }
     }
 
-    private fun getLastConnectedItem(statusItems: MutableList<View>) {
+    private fun getLastConnectedItem() {
         insightPlugin.connectionService?.let {
             when (it.state) {
-                InsightState.CONNECTED, InsightState.NOT_PAIRED -> return
+                InsightState.CONNECTED, InsightState.NOT_PAIRED -> {
+                    binding.lastConnectedLine.visibility = View.GONE
+                    return
+                }
 
                 else                                            -> {
                     val lastConnection = it.lastConnected
@@ -213,73 +208,104 @@ class InsightFragment : DaggerFragment(), View.OnClickListener {
                     } else {
                         dateUtil.hourAgo(lastConnection, rh)
                     }
-                    statusItems.add(getStatusItem(rh.gs(R.string.last_connected), dateUtil.timeString(lastConnection) + " (" + ago + ")")
-                    )
+                    binding.lastConnectedLine.visibility = View.VISIBLE
+                    binding.lastConnected.text = "${dateUtil.timeString(lastConnection)} ($ago)"
                 }
             }
 
         }
     }
 
-    private fun getOperatingModeItem(statusItems: MutableList<View>) {
+    private fun getOperatingModeItem() {
         if (insightPlugin.operatingMode == null) {
-            binding.operatingMode.visibility = View.GONE
+            binding.operatingModeButton.visibility = View.GONE
+            binding.operatingModeLine.visibility = View.GONE
             return
         }
         var string = 0
-        if (ENABLE_OPERATING_MODE_BUTTON) binding.operatingMode.visibility = View.VISIBLE
-        binding.operatingMode.isEnabled = operatingModeCallback == null
+        if (ENABLE_OPERATING_MODE_BUTTON) binding.operatingModeButton.visibility = View.VISIBLE
+        binding.operatingModeButton.isEnabled = operatingModeCallback == null
         when (insightPlugin.operatingMode) {
             OperatingMode.STARTED -> {
-                binding.operatingMode.setText(R.string.stop_pump)
+                binding.operatingModeButton.setText(R.string.stop_pump)
                 string = R.string.started
             }
 
             OperatingMode.STOPPED -> {
-                binding.operatingMode.setText(R.string.start_pump)
+                binding.operatingModeButton.setText(R.string.start_pump)
                 string = R.string.stopped
             }
 
             OperatingMode.PAUSED  -> {
-                binding.operatingMode.setText(R.string.start_pump)
+                binding.operatingModeButton.setText(R.string.start_pump)
                 string = app.aaps.core.ui.R.string.paused
             }
 
             null                  -> Unit
         }
-        statusItems.add(getStatusItem(rh.gs(R.string.operating_mode), rh.gs(string)))
+        binding.operatingModeLine.visibility = View.VISIBLE
+        binding.operatingMode.text = rh.gs(string)
     }
 
-    private fun getBatteryStatusItem(statusItems: MutableList<View>) {
-        val batteryStatus = insightPlugin.batteryStatus ?: return
-        statusItems.add(getStatusItem(rh.gs(app.aaps.core.ui.R.string.battery_label), batteryStatus.batteryAmount.toString() + "%"))
+    private fun getBatteryStatusItem() {
+        insightPlugin.batteryStatus?.let { batteryStatus ->
+            binding.batteryLevelLine.visibility = View.VISIBLE
+            binding.batteryLevel.text = rh.gs(app.aaps.core.ui.R.string.format_percent, batteryStatus.batteryAmount)
+        } ?:apply {
+            binding.batteryLevelLine.visibility = View.GONE
+        }
     }
 
-    private fun getCartridgeStatusItem(statusItems: MutableList<View>) {
-        val cartridgeStatus = insightPlugin.cartridgeStatus ?: return
-        val status: String = if (cartridgeStatus.isInserted) decimalFormatter.to2Decimal(cartridgeStatus.remainingAmount) + "U" else rh.gs(R.string.not_inserted)
-        statusItems.add(getStatusItem(rh.gs(app.aaps.core.ui.R.string.reservoir_label), status))
+    private fun getCartridgeStatusItem() {
+        insightPlugin.cartridgeStatus?.let { cartridgeStatus ->
+            binding.reservoirLevelLine.visibility = View.VISIBLE
+            val status: String = if (cartridgeStatus.isInserted) rh.gs( app.aaps.core.ui.R.string.format_insulin_units, cartridgeStatus.remainingAmount)  else rh.gs(R.string.not_inserted)
+            binding.reservoirLevel.text = status
+        } ?: apply {
+            binding.reservoirLevelLine.visibility = View.GONE
+        }
     }
 
-    private fun getTDDItems(statusItems: MutableList<View>) {
-        val tdd = insightPlugin.totalDailyDose ?: return
-        statusItems.add(getStatusItem(rh.gs(R.string.tdd_bolus), decimalFormatter.to2Decimal(tdd.bolus)))
-        statusItems.add(getStatusItem(rh.gs(R.string.tdd_basal), decimalFormatter.to2Decimal(tdd.basal)))
-        statusItems.add(getStatusItem(rh.gs(app.aaps.core.ui.R.string.tdd_total), decimalFormatter.to2Decimal(tdd.bolusAndBasal)))
+    private fun getTDDItems() {
+        insightPlugin.totalDailyDose?.let { tdd ->
+            binding.tddBolusLine.visibility = View.VISIBLE
+            binding.tddBolus.text = decimalFormatter.to2Decimal(tdd.bolus)
+            binding.tddBasalLine.visibility = View.VISIBLE
+            binding.tddBasal.text = decimalFormatter.to2Decimal(tdd.basal)
+            binding.tddTotalLine.visibility = View.VISIBLE
+            binding.tddTotal.text = decimalFormatter.to2Decimal(tdd.bolusAndBasal)
+        }  ?: apply {
+            binding.tddBolusLine.visibility = View.GONE
+            binding.tddBasalLine.visibility = View.GONE
+            binding.tddTotalLine.visibility = View.GONE
+        }
     }
 
-    private fun getBaseBasalRateItem(statusItems: MutableList<View>) {
-        val activeBasalRate = insightPlugin.activeBasalRate ?: return
-        statusItems.add(getStatusItem(rh.gs(app.aaps.core.ui.R.string.base_basal_rate_label),decimalFormatter.to2Decimal(activeBasalRate.activeBasalRate) + " U/h (" + activeBasalRate.activeBasalProfileName + ")"))
+
+    private fun getBaseBasalRateItem() {
+        insightPlugin.activeBasalRate?.let { activeBasalRate ->
+            binding.activeBasalRateLine.visibility = View.VISIBLE
+            binding.activeBasalRate.text = rh.gs(app.aaps.core.ui.R.string.pump_base_basal_rate, activeBasalRate.activeBasalRate) + " (${activeBasalRate.activeBasalProfileName})"
+        } ?: apply {
+            binding.activeBasalRateLine.visibility = View.GONE
+        }
     }
 
-    private fun getTBRItem(statusItems: MutableList<View>) {
-        val activeTBR = insightPlugin.activeTBR ?: return
-        statusItems.add(getStatusItem(rh.gs(app.aaps.core.ui.R.string.tempbasal_label),rh.gs(R.string.tbr_formatter, activeTBR.percentage, activeTBR.initialDuration - activeTBR.remainingDuration, activeTBR.initialDuration)))
+    private fun getTBRItem() {
+        insightPlugin.activeTBR?.let { activeTBR ->
+            binding.activeTbrLine.visibility = View.VISIBLE
+            binding.activeTbr.text = rh.gs(R.string.tbr_formatter, activeTBR.percentage, activeTBR.initialDuration - activeTBR.remainingDuration, activeTBR.initialDuration)
+        } ?: apply {
+            binding.activeTbrLine.visibility = View.GONE
+        }
     }
 
-    private fun getLastBolusItem(statusItems: MutableList<View>) {
-        if (insightPlugin.lastBolusAmount.equals(0.0) || insightPlugin.lastBolusTimestamp.equals(0L)) return
+    private fun getLastBolusItem() {
+        if (insightPlugin.lastBolusAmount.equals(0.0) || insightPlugin.lastBolusTimestamp.equals(0L)) {
+            binding.lastBolusLine.visibility = View.GONE
+            return
+        }
+        binding.lastBolusLine.visibility = View.VISIBLE
         val agoMsc = System.currentTimeMillis() - insightPlugin.lastBolusTimestamp
         val bolusMinAgo = agoMsc / 60.0 / 1000.0
         val unit = rh.gs(app.aaps.core.ui.R.string.insulin_unit_shortname)
@@ -289,25 +315,31 @@ class InsightFragment : DaggerFragment(), View.OnClickListener {
         } else {
             dateUtil.hourAgo(insightPlugin.lastBolusTimestamp, rh)
         }
-        statusItems.add(
-            getStatusItem(
-                rh.gs(R.string.insight_last_bolus),
-                rh.gs(R.string.insight_last_bolus_formater, insightPlugin.lastBolusAmount, unit, ago)
-            )
-        )
+        binding.lastBolus.text = rh.gs(R.string.insight_last_bolus_formater, insightPlugin.lastBolusAmount, unit, ago)
     }
 
-    private fun getBolusItems(statusItems: MutableList<View>) {
-        insightPlugin.activeBoluses?.forEach { activeBolus ->
-            val label: String?
-            label = when (activeBolus.bolusType) {
-                BolusType.MULTIWAVE -> rh.gs(R.string.multiwave_bolus)
-                BolusType.EXTENDED  -> rh.gs(app.aaps.core.ui.R.string.extended_bolus)
-                else                -> null
-            }
-            label?.let {
-                statusItems.add(getStatusItem(it, rh.gs(R.string.eb_formatter, activeBolus.remainingAmount, activeBolus.initialAmount, activeBolus.remainingDuration)))
-            }
+    private fun getBolusItems() {
+        insightPlugin.activeBoluses?.let { activeBoluses ->
+            binding.extendedBolus1Line.visibility = (activeBoluses.size > 0).toVisibility()
+            if (activeBoluses.size > 0)
+                updateBolusItemView(binding.extendedBolus1, binding.extendedBolus1Label, activeBoluses[0])
+            binding.extendedBolus2Line.visibility = (activeBoluses.size > 1).toVisibility()
+            if (activeBoluses.size > 1)
+                updateBolusItemView(binding.extendedBolus2, binding.extendedBolus2Label, activeBoluses[1])
+        } ?: apply {
+            binding.extendedBolus1Line.visibility = View.GONE
+            binding.extendedBolus2Line.visibility = View.GONE
+        }
+    }
+
+    private fun updateBolusItemView(view: TextView, labelView: TextView, activeBolus: ActiveBolus) {
+        when (activeBolus.bolusType) {
+            BolusType.MULTIWAVE -> rh.gs(R.string.multiwave_bolus)
+            BolusType.EXTENDED  -> rh.gs(app.aaps.core.ui.R.string.extended_bolus)
+            else                -> null
+        }?.let { label ->
+            labelView.text = label
+            view.text = rh.gs(R.string.eb_formatter, activeBolus.remainingAmount, activeBolus.initialAmount, activeBolus.remainingDuration)
         }
     }
 
