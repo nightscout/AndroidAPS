47758b6b27c6e8562d598e5c63ba0ffde6593cb3
Milos Kozak <m.kozak@sysop.cz>
2023-11-03 14:03:57 +0100

    calculated day's difference over DST correctly

--- implementation/src/main/kotlin/app/aaps/implementation/stats/TirCalculatorImpl.kt
+++ implementation/src/main/kotlin/app/aaps/implementation/stats/TirCalculatorImpl.kt
@@ -15,7 +15,6 @@ import app.aaps.core.interfaces.stats.TIR
 import app.aaps.core.interfaces.stats.TirCalculator
 import app.aaps.core.interfaces.utils.DateUtil
 import app.aaps.core.interfaces.utils.MidnightTime
-import app.aaps.core.interfaces.utils.T
 import app.aaps.database.impl.AppRepository
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -31,7 +30,7 @@ class TirCalculatorImpl @Inject constructor(
     override fun calculate(days: Long, lowMgdl: Double, highMgdl: Double): LongSparseArray<TIR> {
         if (lowMgdl < 39) throw RuntimeException("Low below 39")
         if (lowMgdl > highMgdl) throw RuntimeException("Low > High")
-        val startTime = MidnightTime.calc(dateUtil.now() - T.days(days).msecs())
+        val startTime = MidnightTime.calcDaysBack(days)
         val endTime = MidnightTime.calc(dateUtil.now())
 
         val bgReadings = repository.compatGetBgReadingsDataFromTime(startTime, endTime, true).blockingGet()
