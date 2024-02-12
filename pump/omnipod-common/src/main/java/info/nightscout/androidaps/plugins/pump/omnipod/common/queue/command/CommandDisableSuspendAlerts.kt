0c243c650ac82621b8600768e91eb93b0ef7451a
olorinmaia <alfeinarj@gmail.com>
2024-02-13 00:30:57 +0100

    Add string for notification shown when changing profile with dash/eros.

--- pump/omnipod-common/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/common/queue/command/CommandDisableSuspendAlerts.kt
+++ pump/omnipod-common/src/main/java/info/nightscout/androidaps/plugins/pump/omnipod/common/queue/command/CommandDisableSuspendAlerts.kt
@@ -1,9 +1,11 @@
 package info.nightscout.androidaps.plugins.pump.omnipod.common.queue.command
 
 import app.aaps.core.interfaces.queue.CustomCommand
+import app.aaps.core.interfaces.resources.ResourceHelper
+import info.nightscout.androidaps.plugins.pump.omnipod.common.R
 
-class CommandDisableSuspendAlerts : CustomCommand {
+class CommandDisableSuspendAlerts(private val rh: ResourceHelper) : CustomCommand {
 
     override val statusDescription: String
-        get() = "DISABLE SUSPEND ALERTS"
+        get() = rh.gs(R.string.omnipod_common_disable_suspend_alerts)
 }
