9c327b4e4c3ce069ad36e4261995b22f8dddf4fa
Milos Kozak <m.kozak@sysop.cz>
2024-02-17 11:14:50 +0100

    SMB: fix preferences

--- core/keys/src/main/kotlin/app/aaps/core/keys/BooleanKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/BooleanKey.kt
@@ -31,14 +31,14 @@ enum class BooleanKey(
     DexcomCreateSensorChange(R.string.key_dexcom_log_ns_sensor_change, true, defaultedBySM = true),
     ApsUseAutosens(R.string.key_openaps_use_autosens, true, defaultedBySM = true, negativeDependency = R.string.key_use_dynamic_sensitivity), // change from default false
     ApsUseSmb(R.string.key_openaps_use_smb, true, defaultedBySM = true), // change from default false
-    ApsUseSmbWithHighTt(R.string.key_openaps_allow_smb_with_high_temp_target, false, defaultedBySM = true),
-    ApsUseSmbAlways(R.string.key_openaps_enable_smb_always, true, defaultedBySM = true), // change from default false
-    ApsUseSmbWithCob(R.string.key_openaps_allow_smb_with_COB, true, defaultedBySM = true), // change from default false
-    ApsUseSmbWithLowTt(R.string.key_openaps_allow_smb_with_low_temp_target, true, defaultedBySM = true), // change from default false
-    ApsUseSmbAfterCarbs(R.string.key_openaps_enable_smb_after_carbs, true, defaultedBySM = true), // change from default false
+    ApsUseSmbWithHighTt(R.string.key_openaps_allow_smb_with_high_temp_target, false, defaultedBySM = true, dependency = R.string.key_openaps_use_smb),
+    ApsUseSmbAlways(R.string.key_openaps_enable_smb_always, true, defaultedBySM = true, dependency = R.string.key_openaps_use_smb), // change from default false
+    ApsUseSmbWithCob(R.string.key_openaps_allow_smb_with_COB, true, defaultedBySM = true, dependency = R.string.key_openaps_use_smb), // change from default false
+    ApsUseSmbWithLowTt(R.string.key_openaps_allow_smb_with_low_temp_target, true, defaultedBySM = true, dependency = R.string.key_openaps_use_smb), // change from default false
+    ApsUseSmbAfterCarbs(R.string.key_openaps_enable_smb_after_carbs, true, defaultedBySM = true, dependency = R.string.key_openaps_use_smb), // change from default false
     ApsUseUam(R.string.key_openaps_use_uam, true, defaultedBySM = true), // change from default false
-    ApsSensitivityRaisesTarget(R.string.key_openaps_sensitivity_raises_target, true, defaultedBySM = true),
-    ApsResistanceLowersTarget(R.string.key_openaps_resistance_lowers_target, true, defaultedBySM = true), // change from default false
+    ApsSensitivityRaisesTarget(R.string.key_openaps_sensitivity_raises_target, true, defaultedBySM = true, dependency = R.string.key_dynamic_isf_adjust_sensitivity),
+    ApsResistanceLowersTarget(R.string.key_openaps_resistance_lowers_target, true, defaultedBySM = true, dependency = R.string.key_dynamic_isf_adjust_sensitivity), // change from default false
     ApsAlwaysUseShortDeltas(R.string.key_openaps_always_use_short_deltas, false, defaultedBySM = true, hideParentScreenIfHidden = true),
     ApsDynIsfAdjustSensitivity(R.string.key_dynamic_isf_adjust_sensitivity, false, defaultedBySM = true, dependency = R.string.key_use_dynamic_sensitivity), // change from default false
     ApsAmaAutosensAdjustTargets(R.string.key_openaps_ama_autosens_adjust_targets, true, defaultedBySM = true),
