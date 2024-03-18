0119b2790bc20bbbcc466ec07ddf2926ccbf7097
Milos Kozak <m.kozak@sysop.cz>
2024-03-18 14:53:09 +0100

    Revert "libs update"
    This reverts commit 1beb6ea2e5345621c22b5bfb8fa7c88b059fd59d.

--- buildSrc/src/main/kotlin/jacoco-app-dependencies.gradle.kts
+++ buildSrc/src/main/kotlin/jacoco-app-dependencies.gradle.kts
@@ -117,11 +117,11 @@ fun Project.registerCodeCoverageTask(
         description = "Generate Jacoco coverage reports on the ${sourceName.replaceFirstChar(Char::titlecase)} build."
 
         val javaDirectories = fileTree(
-            "${project.layout.buildDirectory}/intermediates/classes/${sourcePath}"
+            "${project.buildDir}/intermediates/classes/${sourcePath}"
         ) { exclude(excludedFiles) }
 
         val kotlinDirectories = fileTree(
-            "${project.layout.buildDirectory}/tmp/kotlin-classes/${sourcePath}"
+            "${project.buildDir}/tmp/kotlin-classes/${sourcePath}"
         ) { exclude(excludedFiles) }
 
         val coverageSrcDirectories = listOf(
@@ -137,7 +137,7 @@ fun Project.registerCodeCoverageTask(
         additionalClassDirs.setFrom(files(coverageSrcDirectories))
         sourceDirectories.setFrom(files(coverageSrcDirectories))
         executionData.setFrom(
-            files("${project.layout.buildDirectory}/jacoco/${testTaskName}.exec")
+            files("${project.buildDir}/jacoco/${testTaskName}.exec")
         )
 
         reports {
