72ad9094ddf916951d3fb405bc802b30255e5d61
Theo van Elsberg <van.elsberg@outlook.com>
2024-10-18 13:45:42 +0200

    Added Pod Activation Plugin

--- plugins/automation/src/main/kotlin/app/aaps/plugins/automation/AutomationPlugin.kt
+++ plugins/automation/src/main/kotlin/app/aaps/plugins/automation/AutomationPlugin.kt
@@ -58,6 +58,7 @@ import app.aaps.plugins.automation.triggers.TriggerBTDevice
 import app.aaps.plugins.automation.triggers.TriggerBg
 import app.aaps.plugins.automation.triggers.TriggerBolusAgo
 import app.aaps.plugins.automation.triggers.TriggerCOB
+import app.aaps.plugins.automation.triggers.TriggerPodChange
 import app.aaps.plugins.automation.triggers.TriggerCannulaAge
 import app.aaps.plugins.automation.triggers.TriggerConnector
 import app.aaps.plugins.automation.triggers.TriggerDelta
@@ -415,10 +416,14 @@ class AutomationPlugin @Inject constructor(
             TriggerSensorAge(injector),
             TriggerCannulaAge(injector),
             TriggerReservoirLevel(injector)
-        )
+            )
 
         val pump = activePlugin.activePump
-        if (!pump.pumpDescription.isPatchPump) {
+
+        if (pump.pumpDescription.isPatchPump) {
+            triggers.add(TriggerPodChange(injector))
+        }
+        else {
             triggers.add(TriggerInsulinAge(injector))
         }
         if (pump.pumpDescription.isBatteryReplaceable || pump.isBatteryChangeLoggingEnabled()) {
