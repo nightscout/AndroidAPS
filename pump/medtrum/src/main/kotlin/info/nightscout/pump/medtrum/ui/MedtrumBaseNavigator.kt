5618586913cb183263d3c6b53112e427ce1ca7b3
Milos Kozak <m.kozak@sysop.cz>
2024-12-11 21:26:47 +0100

    Medtrum: optimize preferences

---
+++ pump/medtrum/src/main/kotlin/info/nightscout/pump/medtrum/ui/MedtrumBaseNavigator.kt
@@ -0,0 +1,8 @@
+package info.nightscout.pump.medtrum.ui
+
+interface MedtrumBaseNavigator {
+
+    fun back()
+
+    fun finish(finishAffinity: Boolean = false)
+}
---
+++ pump/medtrum/src/main/kotlin/info/nightscout/pump/medtrum/ui/event/UIEvent.kt
@@ -0,0 +1,8 @@
+package info.nightscout.pump.medtrum.ui.event
+
+open class UIEvent<out T>(private val content: T) {
+
+    var value: Any? = null
+    fun peekContent(): T = content
+}
+
