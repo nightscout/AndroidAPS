a51fcb187047fa8ee79871cbc5b068fad4b05858
Milos Kozak <m.kozak@sysop.cz>
2024-12-29 12:38:51 +0100

    Prepare AllowedVersionsTest for 3.3.0

--- plugins/constraints/src/test/kotlin/app/aaps/plugins/constraints/versionChecker/AllowedVersionsTest.kt
+++ plugins/constraints/src/test/kotlin/app/aaps/plugins/constraints/versionChecker/AllowedVersionsTest.kt
@@ -29,8 +29,13 @@ class AllowedVersionsTest {
             })
             .put(JSONObject().apply {
                 put("minAndroid", 28) // 9.0
+                put("maxAndroid", 29) // 10
+                put("supported", "3.2.0.4")
+            })
+            .put(JSONObject().apply {
+                put("minAndroid", 30) // 11.0
                 put("maxAndroid", 99)
-                put("supported", "2.8.2")
+                put("supported", "3.3.0")
             })
             // Version time limitation
             .put(JSONObject().apply {
@@ -53,14 +58,16 @@ class AllowedVersionsTest {
         assertThat(AllowedVersions().findByApi(definition, 25)!!.getString("supported")).isEqualTo("2.6.2")
         assertThat(AllowedVersions().findByApi(definition, 26)!!.getString("supported")).isEqualTo("2.8.2")
         assertThat(AllowedVersions().findByApi(definition, 27)!!.getString("supported")).isEqualTo("2.8.2")
-        assertThat(AllowedVersions().findByApi(definition, 28)!!.getString("supported")).isEqualTo("2.8.2")
+        assertThat(AllowedVersions().findByApi(definition, 28)!!.getString("supported")).isEqualTo("3.2.0.4")
+        assertThat(AllowedVersions().findByApi(definition, 29)!!.getString("supported")).isEqualTo("3.2.0.4")
+        assertThat(AllowedVersions().findByApi(definition, 30)!!.getString("supported")).isEqualTo("3.3.0")
     }
 
     @Test
     fun findByVersionTest() {
         //val definition = AllowedVersions().generateSupportedVersions()
         val definition =
-            "[{\"minAndroid\":1,\"maxAndroid\":23},{\"minAndroid\":24,\"maxAndroid\":25,\"supported\":\"2.6.2\"},{\"minAndroid\":26,\"maxAndroid\":27,\"supported\":\"2.8.2\"},{\"minAndroid\":28,\"maxAndroid\":99,\"supported\":\"2.8.2\"},{\"endDate\":\"2021-11-07\",\"version\":\"2.9.0-beta1\"},{\"endDate\":\"2021-11-02\",\"version\":\"3.0-beta1\"},{\"endDate\":\"2021-11-04\",\"version\":\"3.0-beta2\"},{\"endDate\":\"2021-11-10\",\"version\":\"3.0-beta3\"},{\"endDate\":\"2021-11-14\",\"version\":\"3.0-beta4\"}\n" +
+            "[{\"minAndroid\":1,\"maxAndroid\":23},{\"minAndroid\":24,\"maxAndroid\":25,\"supported\":\"2.6.2\"},{\"minAndroid\":26,\"maxAndroid\":27,\"supported\":\"2.8.2\"},{\"minAndroid\":28,\"maxAndroid\":29,\"supported\":\"3.2.0.4\"},{\"minAndroid\":30,\"maxAndroid\":99,\"supported\":\"3.3.0\"},{\"endDate\":\"2021-11-07\",\"version\":\"2.9.0-beta1\"},{\"endDate\":\"2021-11-02\",\"version\":\"3.0-beta1\"},{\"endDate\":\"2021-11-04\",\"version\":\"3.0-beta2\"},{\"endDate\":\"2021-11-10\",\"version\":\"3.0-beta3\"},{\"endDate\":\"2021-11-14\",\"version\":\"3.0-beta4\"}\n" +
                 " ,{\"endDate\":\"2021-11-16\",\"version\":\"3.0-beta5\"}\n" +
                 "]"
         assertThat(AllowedVersions().findByVersion(definition, "2.6.0")).isNull()
