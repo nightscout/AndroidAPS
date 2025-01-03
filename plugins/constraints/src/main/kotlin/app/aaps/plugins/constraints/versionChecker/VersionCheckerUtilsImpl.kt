4344a18d3190c5d27bbcd6f4154868092b62f078
Milos Kozak <m.kozak@sysop.cz>
2025-01-03 10:11:32 +0100

    VersionCheckerUtilsImpl improvement & fix tests

--- plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/versionChecker/VersionCheckerUtilsImpl.kt
+++ plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/versionChecker/VersionCheckerUtilsImpl.kt
@@ -49,14 +49,14 @@ class VersionCheckerUtilsImpl @Inject constructor(
             Thread {
                 try {
                     val definition: String = URL("https://raw.githubusercontent.com/nightscout/AndroidAPS/versions/definition.json").readText()
-                    val version: String? = AllowedVersions().findByApi(definition, Build.VERSION.SDK_INT)?.optString("supported")
-                    val newVersionAvailable = compareWithCurrentVersion(version, config.get().VERSION_NAME)
+                    val version: String? = AllowedVersions.findByApi(definition, Build.VERSION.SDK_INT)?.optString("supported")
+                    val newVersionByApi = compareWithCurrentVersion(version, config.get().VERSION_NAME)
 
                     // App expiration
-                    if (newVersionAvailable) {
+                    if (newVersionByApi || config.get().isDev()) {
                         var endDate = sp.getLong(rh.gs(app.aaps.core.utils.R.string.key_app_expiration) + "_" + config.get().VERSION_NAME, 0)
-                        AllowedVersions().findByVersion(definition, config.get().VERSION_NAME)?.let { expirationJson ->
-                            AllowedVersions().endDateToMilliseconds(expirationJson.getString("endDate"))?.let { ed ->
+                        AllowedVersions.findByVersion(definition, config.get().VERSION_NAME)?.let { expirationJson ->
+                            AllowedVersions.endDateToMilliseconds(expirationJson.getString("endDate"))?.let { ed ->
                                 endDate = ed + T.days(1).msecs()
                                 sp.putLong(rh.gs(app.aaps.core.utils.R.string.key_app_expiration) + "_" + config.get().VERSION_NAME, endDate)
                             }
