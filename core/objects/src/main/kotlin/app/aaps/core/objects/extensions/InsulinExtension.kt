13096f7f26de64d30792aa734064cdfa990dae19
Philoul <legoupil.jp@free.fr>
2025-01-26 22:58:40 +0100

    Add ICfg methods and Extension

---
+++ core/objects/src/main/kotlin/app/aaps/core/objects/extensions/InsulinExtension.kt
@@ -0,0 +1,15 @@
+package app.aaps.core.objects.extensions
+
+import app.aaps.core.data.model.ICfg
+import org.json.JSONObject
+
+fun ICfg.toJson(): JSONObject = JSONObject()
+    .put("insulinLabel", insulinLabel)
+    .put("insulinEndTime", insulinEndTime)
+    .put("peak", insulinPeakTime)
+
+fun ICfg.Companion.fromJson(json: JSONObject): ICfg = ICfg(
+    insulinLabel = json.optString("insulinLabel", ""),
+    insulinEndTime = json.optLong("insulinEndTime", 6 * 3600 * 1000),
+    insulinPeakTime = json.optLong("peak")
+)
