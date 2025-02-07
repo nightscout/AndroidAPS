7bffdb957e08157574a84dae94f7bf93de60a7c8
ga-zelle <ga.zelle@netcologne.de>
2025-02-07 01:21:23 +0100

    no UnitDouble on BG delta item in AutoISF

--- core/keys/src/main/kotlin/app/aaps/core/keys/DoubleKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/DoubleKey.kt
@@ -40,6 +40,7 @@ enum class DoubleKey(
     ApsAutoIsfBgBrakeWeight("bgBrake_ISF_weight", 0.0, 0.0, 1.0, defaultedBySM = true),
     ApsAutoIsfLowBgWeight("lower_ISFrange_weight", 0.0, 0.0, 2.0, defaultedBySM = true),
     ApsAutoIsfHighBgWeight("higher_ISFrange_weight", 0.0, 0.0, 2.0, defaultedBySM = true),
+    ApsAutoIsfSmbDeliveryRatioBgRange("openapsama_smb_delivery_ratio_bg_range", 0.0, 0.0, 100.0, defaultedBySM = true),
     ApsAutoIsfPpWeight("pp_ISF_weight", 0.0, 0.0, 1.0, defaultedBySM = true),
     ApsAutoIsfDuraWeight("dura_ISF_weight", 0.0, 0.0, 3.0, defaultedBySM = true),
     ApsAutoIsfSmbDeliveryRatio("openapsama_smb_delivery_ratio", 0.5, 0.5, 1.0, defaultedBySM = true),
