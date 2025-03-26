75630efc2c9161b4b663d79f50036d2883c8c6b8
Milos Kozak <m.kozak@sysop.cz>
2025-03-26 16:04:50 +0100

    fix tests

--- plugins/sync/src/test/kotlin/app/aaps/plugins/sync/tidepool/comm/UploadChunkTest.kt
+++ plugins/sync/src/test/kotlin/app/aaps/plugins/sync/tidepool/comm/UploadChunkTest.kt
@@ -7,6 +7,7 @@ import app.aaps.core.interfaces.plugin.ActivePlugin
 import app.aaps.core.interfaces.profile.ProfileFunction
 import app.aaps.core.interfaces.profile.ProfileUtil
 import app.aaps.core.interfaces.rx.bus.RxBus
+import app.aaps.core.interfaces.sharedPreferences.SP
 import app.aaps.core.interfaces.utils.DateUtil
 import app.aaps.plugins.sync.tidepool.elements.BolusElement
 import app.aaps.plugins.sync.tidepool.utils.GsonInstance
@@ -24,6 +25,7 @@ import org.mockito.kotlin.any
 @ExtendWith(MockitoExtension::class)
 class UploadChunkTest {
 
+    @Mock lateinit var sp: SP
     @Mock lateinit var rxBus: RxBus
     @Mock lateinit var aapsLogger: AAPSLogger
     @Mock lateinit var profileFunction: ProfileFunction
