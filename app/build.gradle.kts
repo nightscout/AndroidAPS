6f4399cf99623c261bc6fb2b30d02e1af70a6723
MOBXll <97326058@qq.com>
2026-04-13 23:51:06 +0800

    Merge pull request #4660 from nightscout/dev
    3.4.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -219,8 +219,6 @@ dependencies {
     // Remote config
     api(libs.com.google.firebase.config)
     // 动态密码模块
-    implementation("dev.turingcomplete:kotlin-onetimepassword:2.4.1") {
-        exclude(group = "org.json", module = "json")
 }
 
 println("-------------------")
