becc67380bbac1ada31db68a3b3c82b6425696de
olorinmaia <alfeinarj@gmail.com>
2026-07-24 19:28:11 +0200

    Wear: BG graph complication time range setting.

--- wear/src/test/kotlin/app/aaps/wear/complications/BgGraphComplicationTest.kt
+++ wear/src/test/kotlin/app/aaps/wear/complications/BgGraphComplicationTest.kt
@@ -4,6 +4,7 @@ import androidx.wear.watchface.complications.data.ComplicationType
 import androidx.wear.watchface.complications.data.PhotoImageComplicationData
 import androidx.wear.watchface.complications.data.SmallImageComplicationData
 import app.aaps.wear.AAPSLoggerTest
+import app.aaps.wear.R
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -27,7 +28,7 @@ internal class BgGraphComplicationTest {
         Robolectric.buildService(BgGraphComplication::class.java).get().also {
             it.aapsLogger = AAPSLoggerTest()
             it.sp = mock()
-            whenever(it.sp.getString("complication_bg_graph_hours", "3")).thenReturn("3")
+            whenever(it.sp.getString(R.string.key_complication_bg_graph_hours, "3")).thenReturn("3")
         }
 
     @Test
