e1068e77db4f801c046340c8313cd7a2856f4e7c
Milos Kozak <m.kozak@sysop.cz>
2026-08-05 21:02:28 +0200

    :core:keys remove JVM dependency

--- core/keys/src/main/kotlin/app/aaps/core/keys/interfaces/IntComposedNonPreferenceKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/interfaces/IntComposedNonPreferenceKey.kt
@@ -3,14 +3,14 @@ package app.aaps.core.keys.interfaces
 /**
  * Preference key where key is a format string see [String::format]
  *
- * Final key is composed as key + String.format(Locale.ENGLISH, format, *arguments)
+ * Final key is composed as key + format, with %d and %s replaced by the arguments
  */
 interface IntComposedNonPreferenceKey : NonPreferenceKey, ComposedKey {
 
     /**
      * Key is used as prefix for recognizing the preference
      *
-     * Final key is composed as key + String.format(Locale.ENGLISH, format, *arguments)
+     * Final key is composed as key + format, with %d and %s replaced by the arguments
      */
     override val key: String
 
