626176e62d81e1b940b34bf858c4c7c63b5f7097
Prem Nirmal <prnirmal@microsoft.com>
2026-08-25 11:24:41 +0100

    no description

--- core/keys/src/main/kotlin/app/aaps/core/keys/BooleanKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/BooleanKey.kt
@@ -67,10 +67,7 @@ enum class BooleanKey(
     AlertUrgentAsAndroidNotification("raise_urgent_alarms_as_android_notification", true, R.string.pref_title_alert_urgent_as_android_notification),
     AlertIncreaseVolume("gradually_increase_notification_volume", true, R.string.pref_title_alert_increase_volume),
     AlertOverrideDoNotDisturb("alert_override_dnd", true, R.string.pref_title_alert_override_dnd, R.string.pref_summary_alert_override_dnd, defaultedBySM = true),
-    NotificationShowBgOnIcon(
-        "notification_show_bg_on_icon", false, R.string.pref_title_notification_show_bg_on_icon, R.string.pref_summary_notification_show_bg_on_icon
-    ),
-
+    NotificationShowBgOnIcon("notification_show_bg_on_icon", false, R.string.pref_title_notification_show_bg_on_icon),
     BgSourceUploadToNs("dexcomg5_nsupload", true, R.string.pref_title_bg_source_upload_to_ns, defaultedBySM = true, hideParentScreenIfHidden = true),
     BgSourceCreateSensorChange("dexcom_lognssensorchange", true, R.string.pref_title_bg_source_create_sensor_change, R.string.pref_summary_bg_source_create_sensor_change, defaultedBySM = true),
     BgSourceRandomBgRandomize("randombg_randomize", true, R.string.pref_title_random_bg_randomize, R.string.pref_summary_random_bg_randomize, defaultedBySM = true),
