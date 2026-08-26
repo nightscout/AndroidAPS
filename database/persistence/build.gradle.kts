90cee6493ba29d288828989a38696a5002d948f4
Milos Kozak <m.kozak@sysop.cz>
2026-08-27 00:54:56 +0200

    Drop the unused KSP plugin from :database:persistence

--- database/persistence/build.gradle.kts
+++ database/persistence/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.ksp)
     alias(libs.plugins.metro)
     id("android-module-dependencies")
     id("test-module-dependencies")
